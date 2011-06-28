620604fb265b14930e0e195281ad01791d25194d
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-06-28 16:13:35 +0100

    Added Nacny and SisoDb to the project

---
+++ UserGroup_DDD-CQRS/UserGroupManagement.Domain/Locations/ContactName.cs
@@ -0,0 +1,12 @@
+﻿namespace UserGroupManagement.Domain.Locations
+{
+    public class ContactName
+    {
+        public string Name { get; private set; }
+
+        public ContactName(string name)
+        {
+            Name = name;
+        }
+    }
+}
