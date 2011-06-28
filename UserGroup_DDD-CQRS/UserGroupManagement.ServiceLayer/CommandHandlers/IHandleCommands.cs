3ceb21d7c6b29a022b91d31a6412d4407c5aabdd
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-06-28 13:02:06 +0100

    added the service layer

---
+++ UserGroup_DDD-CQRS/UserGroupManagement.ServiceLayer/CommandHandlers/IHandleCommands.cs
@@ -0,0 +1,7 @@
+﻿namespace UserGroupManagement.ServiceLayer.CommandHandlers
+{
+    public interface IHandleCommands<in TCommand> where TCommand : class
+    {
+        void Handle(TCommand command);
+    }
+}
