41527af09e94f712be809167a368f25561d5b69c
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-06-28 18:12:10 +0100

    still not ready for prime time, but getting the structure in place for this model

---
+++ UserGroup_DDD-CQRS/UserGroupManagement.ServiceLayer/CommandProcessor/ChainofResponsibilityBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UserGroupManagement.ServiceLayer.CommandProcessor;
+
+namespace UserGroupManagement.ServiceLayer
+{
+    public class ChainofResponsibilityBuilder
+    {
+        public ChainOfResponsibility Build(Type type)
+        {
+            return new ChainOfResponsibility();
+        }
+    }
+}
