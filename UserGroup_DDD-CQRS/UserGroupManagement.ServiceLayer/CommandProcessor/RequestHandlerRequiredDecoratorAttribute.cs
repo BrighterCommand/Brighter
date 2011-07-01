b858a1032b577611391e8462790bcf535f4dc9fb
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-07-01 14:54:46 +0100

    temporary shelving of code, looking at pre and post handlers in the chain

--- UserGroup_DDD-CQRS/UserGroupManagement.ServiceLayer/CommandProcessor/RequestHandlerRequiredDecoratorAttribute.cs
+++ UserGroup_DDD-CQRS/UserGroupManagement.ServiceLayer/CommandProcessor/RequestHandlerRequiredDecoratorAttribute.cs
@@ -2,19 +2,29 @@
 
 namespace UserGroupManagement.ServiceLayer.CommandProcessor
 {
+    using UserGroupManagement.ServiceLayer.CommandHandlers;
+
     [AttributeUsage(AttributeTargets.Method)]
     public abstract class RequestHandlerAttribute : Attribute
     {
         private readonly int _step;
 
-        protected RequestHandlerAttribute(int step)
+        private readonly HandlerTiming _timing;
+
+        protected RequestHandlerAttribute(int step, HandlerTiming timing = HandlerTiming.Before)
         {
             _step = step;
+            _timing = timing;
         }
 
         public int Step
         {
             get { return _step; }
+        }
+
+        public HandlerTiming Timing
+        {
+            get { return _timing; }
         }
 
         public abstract Type GetHandlerType();
