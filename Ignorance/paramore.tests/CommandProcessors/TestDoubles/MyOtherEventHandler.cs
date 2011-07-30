c54d514f379a1f7a2d58529cd6540c4dea847832
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-07-30 17:26:02 +0100

    oops missed this one

---
+++ Ignorance/paramore.tests/CommandProcessors/TestDoubles/MyOtherEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Paramore.Services.CommandHandlers;
+
+namespace Paramore.Tests.CommandProcessors.TestDoubles
+{
+    internal class MyOtherEventHandler : RequestHandler<MyEvent>
+    {
+        private static MyEvent receivedEvent;
+
+        public MyOtherEventHandler()
+        {
+            receivedEvent = null;
+        }
+
+        public override MyEvent Handle(MyEvent @event)
+        {
+            LogEvent(@event);
+            return @event;
+        }
+
+        private static void LogEvent(MyEvent @event)
+        {
+            receivedEvent = @event;
+        }
+
+        public static bool ShouldRecieve(MyEvent myEvent)
+        {
+            return receivedEvent.Id == myEvent.Id;
+        }
+    }
+}
