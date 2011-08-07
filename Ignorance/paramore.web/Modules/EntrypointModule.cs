ed0a90644e48b334308e492600d19a6564a48686
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-08-07 22:39:26 +0100

    getting started with Nancy

---
+++ Ignorance/paramore.web/Modules/EntrypointModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Paramore.Web.Modules
+{
+    public class EntrypointModule : NancyModule
+    {
+        public EntrypointModule()
+        {
+            Get["/"] = parameters => "<h1>Welcome to Paramore!</h1><p>Nothing to see here at the moment.</p>";
+        }
+    }
+}
---
+++ Ignorance/paramore.web/Modules/ScheduledMeetingsModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+
+namespace Paramore.Web.Modules
+{
+    public class ScheduledMeetingsModule : NancyModule
+    {
+        public ScheduledMeetingsModule() : base("/scheduler")
+        {
+            Get["/{date}"] = parameters =>
+            {
+                return View["Meeting"];
+            };
+            Post["/add"] = parameters =>
+            {
+                return Response.AsRedirect("/scheduler/10-AUG-2011");
+            };
+        }
+    }
+}
