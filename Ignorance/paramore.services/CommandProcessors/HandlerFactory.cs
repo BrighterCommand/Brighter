b34b662c2f5331ac8ba42adcf353d88eee136c41
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2011-10-24 14:29:03 +0100

    Not sure why these were lying about

---
+++ Ignorance/paramore.services/CommandProcessors/HandlerFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Paramore.Services.CommandHandlers;
+using Paramore.Services.Common;
+using TinyIoC;
+
+namespace Paramore.Services.CommandProcessors
+{
+    internal class HandlerFactory<TRequest> where TRequest : class, IRequest
+    {
+        private readonly RequestHandlerAttribute attribute;
+        private readonly TinyIoCContainer container;
+        private readonly Type messageType;
+
+        public HandlerFactory(RequestHandlerAttribute attribute, TinyIoCContainer container)
+        {
+            this.attribute = attribute;
+            this.container = container;
+            messageType = typeof(TRequest);
+        }
+
+        public IHandleRequests<TRequest> CreateRequestHandler()
+        {
+            var handlerType = attribute.GetHandlerType().MakeGenericType(messageType);
+            var parameters = handlerType.GetConstructors()[0].GetParameters().Select(param => container.Resolve(param.ParameterType)).ToArray();
+            return (IHandleRequests<TRequest>)Activator.CreateInstance(handlerType, parameters);
+        }
+    }
+}
---
+++ Ignorance/paramore.services/CommandProcessors/RequestHandlerRequiredDecoratorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Paramore.Services.CommandHandlers;
+
+namespace Paramore.Services.CommandProcessors
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public abstract class RequestHandlerAttribute : Attribute
+    {
+        private readonly int step;
+
+        private readonly HandlerTiming timing;
+
+        protected RequestHandlerAttribute(int step, HandlerTiming timing = HandlerTiming.Before)
+        {
+            this.step = step;
+            this.timing = timing;
+        }
+
+        public int Step
+        {
+            get { return step; }
+        }
+
+        public HandlerTiming Timing
+        {
+            get { return timing; }
+        }
+
+        public abstract Type GetHandlerType();
+    }
+}
