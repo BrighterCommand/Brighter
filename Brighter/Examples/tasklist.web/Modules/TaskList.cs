55a8521721bbe8bde6de7261c3c180b17aecfa42
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2012-01-01 19:58:40 +0000

    Work on integration tests

--- Brighter/Examples/tasklist.web/Modules/TaskList.cs
+++ Brighter/Examples/tasklist.web/Modules/TaskList.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using paramore.commandprocessor;
 using tasklist.web.Commands;
 using tasklist.web.ViewModelRetrievers;
 
@@ -7,15 +8,18 @@ namespace tasklist.web.Modules
     public class TaskListModule : NancyModule
     {
         private readonly ITaskListRetriever taskListRetriever;
+        private readonly IAmACommandProcessor commandProcessor;
 
-        public TaskListModule(ITaskListRetriever taskListRetriever)
+        public TaskListModule(ITaskListRetriever taskListRetriever, IAmACommandProcessor commandProcessor)
         {
             this.taskListRetriever = taskListRetriever;
+            this.commandProcessor = commandProcessor;
             Get["/todo/index"] = _ => TasksView(); 
 
             Post["/todo/index"] = _ =>
             {
                 var cmd = new AddTaskCommand(Request.Form.taskName, Request.Form.taskDecription);
+                commandProcessor.Send(cmd);
                 return TasksView();
             };
 
