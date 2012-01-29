86fdc7c8458b1d3138c7216db9ed80881d58f76b
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2012-01-29 14:45:01 +0000

    Nancy example to allow use of command processor

---
+++ Brighter/Examples/tasklist.web/Bootstrapper/CommandProcessorBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using paramore.commandprocessor;
+using tasklist.web.Commands;
+using tasklist.web.Handlers;
+
+namespace tasklist.web.Bootstrapper
+{
+    public class CommandProcessorBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(TinyIoC.TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+            //try to register the command handlers
+            container.Register<IHandleRequests<AddTaskCommand>, AddTaskCommandHandler>().AsMultiInstance();
+        }
+    }
+}
