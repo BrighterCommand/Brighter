cec80b022ba68bb7a4c70ad1a41cad9e6a1b8c8e
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-02-05 12:11:15 +0000

    Resolve dependencies for OR

---
+++ Rewind/paramore.api/src/paramore.api/Contributors/DependencyPipelineContributor.cs
@@ -0,0 +1,33 @@
+﻿// ReSharper disable RedundantUsingDirective
+using OpenRasta.DI;
+using OpenRasta.Pipeline;
+using OpenRasta.Web;
+// ReSharper restore RedundantUsingDirective
+using Paramore.Adapters.Infrastructure.Repositories;
+
+namespace Paramore.Adapters.Presentation.API.Contributors
+{
+    public class DependencyPipelineContributor : IPipelineContributor
+    {
+        private readonly IDependencyResolver resolver;
+        
+
+        public DependencyPipelineContributor(IDependencyResolver resolver)
+        {
+            this.resolver = resolver;
+        }
+
+        public void Initialize(IPipeline pipelineRunner)
+        {
+            pipelineRunner.Notify(AddDependencies)
+                .Before<KnownStages.IOperationExecution>();
+        }
+
+        private PipelineContinuation AddDependencies(ICommunicationContext arg)
+        {
+            resolver.AddDependencyInstance<IAmAUnitOfWorkFactory>(new UnitOfWorkFactory(), DependencyLifetime.PerRequest);
+            return PipelineContinuation.Continue;
+        }
+
+    }
+}
