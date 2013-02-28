2850cf4f609a98a8fff84a046642fd787d7651c1
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-02-28 11:05:27 +0000

    Fix incorrect resource type definition List<T> not <T>

--- Rewind/paramore.domain/Venues/VenueDocument.cs
+++ Rewind/paramore.domain/Venues/VenueDocument.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.Serialization;
 using Paramore.Adapters.Infrastructure.Repositories;
 using Version = Paramore.Adapters.Infrastructure.Repositories.Version;
 
 namespace Paramore.Domain.Venues
 {
+    [DataContract]
     public class VenueDocument : IAmADocument
     {
         public VenueDocument(Id id, Version version, VenueName venueName, Address address, VenueMap venueMap, VenueContact venueContact)
@@ -18,11 +20,17 @@ namespace Paramore.Domain.Venues
 
         public VenueDocument() {}
 
+        [DataMember]
         public string Address { get; set; }
+        [DataMember]
         public Guid Id { get; set; }
+        [DataMember]
         public string VenueName { get; set; }
+        [DataMember]
         public string VenueContact { get; set; }
+        [DataMember]
         public string VenueMap { get; set; }
+        [DataMember]
         public int Version { get; set; }
     }
 }
