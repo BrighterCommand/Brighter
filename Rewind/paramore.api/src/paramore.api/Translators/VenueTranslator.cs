d557cf894aaebb9c342ede0a71c4e9ba302a23d5
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-03-07 11:31:40 +0000

    Links

--- Rewind/paramore.api/src/paramore.api/Translators/VenueTranslator.cs
+++ Rewind/paramore.api/src/paramore.api/Translators/VenueTranslator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Paramore.Adapters.Presentation.API.Handlers;
+using Paramore.Adapters.Presentation.API.Resources;
 using Paramore.Domain.Venues;
 
 namespace Paramore.Adapters.Presentation.API.Translators
