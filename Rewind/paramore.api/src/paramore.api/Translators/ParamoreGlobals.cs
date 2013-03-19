20fb7e558b5f3676fc0bd6fbadc6f7bcf90fa1fe
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-03-19 11:05:47 +0000

    Merged in missing files

---
+++ Rewind/paramore.api/src/paramore.api/Translators/ParamoreGlobals.cs
@@ -0,0 +1,17 @@
+﻿namespace Paramore.Adapters.Presentation.API.Translators
+{
+    //TODO: Move declarations that can change to a configuration file
+    internal static class ParamoreGlobals
+    {
+        static ParamoreGlobals()
+        {
+            HostName = "localhost: xxxxx";
+            Self = "self";
+            Map = "map";
+        }
+
+        public static object HostName { get; private set; }
+        public static string Map { get; private set; }
+        public static string Self { get; private set; }
+    }
+}
