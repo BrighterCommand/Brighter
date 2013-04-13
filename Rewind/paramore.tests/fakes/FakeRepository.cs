9df6d211f51c1c8a4f7d8774a9357a8fa824e296
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-04-13 14:58:58 +0100

    Creating fakes

---
+++ Rewind/paramore.tests/fakes/FakeRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Paramore.Adapters.Infrastructure.Repositories;
+using Raven.Client.Linq;
+
+namespace Paramore.Adapters.Tests.UnitTests.fakes
+{
+    internal class FakeRepository<T, TDocument> : IRepository<T, TDocument>
+        where T : IAmAnAggregateRoot<TDocument>, new()
+        where TDocument : IAmADocument
+    {
+        readonly List<T> members = new List<T>();
+
+        #region IRepository<T,TDocument> Members
+
+        public void Add(T aggregate)
+        {
+            members.Add(aggregate);
+        }
+
+        public T this[System.Guid id]
+        {
+            get
+            {
+                foreach (var member in members.Where(member => member.Id == id))
+                {
+                    return member;
+                }
+            }
+        }
+
+        public IUnitOfWork UnitOfWork
+        {
+            set { ; }
+        }
+
+        public void Delete(T aggregate)
+        {
+        }
+
+        #endregion
+    }
+}
