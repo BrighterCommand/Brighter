d03df9607b003917d65a75269eb4dbe584a40739
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-04-14 22:13:38 +0100

    Working with revealing module pattern and dummy data

--- Rewind/paramore.web/js/venues.js
+++ Rewind/paramore.web/js/venues.js
@@ -1,16 +1,43 @@
 ﻿$(function () {
  
-    //The viewmodel
-    paramore.ViewModel = function () {
-        this.address = ko.observable(paramore.model.address);
-        this.contact = ko.observable(paramore.model.contact);
-        this.self = ko.observable(paramore.model.links[0]);
-        this.map = ko.observable(paramore.model.links[1]);
+    //for creating venues
+    paramore.Venue = function () {
         this.name = ko.observable(paramore.model.name);
-        this.version = ko.observable(paramore.model.version);
+        this.address = ko.observable();
+        this.contact = ko.observable();
+        this.map = ko.observable();
+        this.self = ko.observable();
+        this.version = ko.observable();
 
     };
 
+    //The viewmodel
+    paramore.ViewModel = function () {
+        var
+            venues = ko.observableArray([]),
+            loadVenues = function() {
+                $.each(paramore.model.data.Venues, function(i, v) {
+                    venues.push(new paramore.Venue()
+                        .name(v.name)
+                        .address(v.address)
+                        .contact(v.contact)
+                        .map(v.links[1].HRef)
+                        .self(v.links[0].HRef)
+                        .version(v.version)
+                    );
+                });
+            }
+        ;
+
+        return {
+            venues: venues,
+            loadVenues : loadVenues
+        };
+
+    }();
+    
+
     //Note: Data bind the values between the source and the targets using Knockout
-    ko.applyBindings(new paramore.ViewModel());
+    paramore.ViewModel.loadVenues();
+    ko.applyBindings(paramore.ViewModel);
 });
