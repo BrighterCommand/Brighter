3f49cedb3f51ab3d52e93836b4b0dc1eb23d2ccb
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-04-24 00:16:11 +0100

    Update

--- Rewind/paramore.web/js/data.js
+++ Rewind/paramore.web/js/data.js
@@ -1,12 +1,15 @@
 ﻿var paramore = paramore || {}; //paramore namespace
 
 // MOCK: Get the real data from a REST endpoint - this is test data captured from the enpoint by Fiddler
-//[{"address":"Street : StreetNumber: 1, Street: MyStreet, City : London, PostCode : N1 3GA",
-//"contact":"ContactName: Ian, EmailAddress: ian@huddle.com, PhoneNumber: 123454678",
-//"links":[{"HRef":"\/\/localhost:59280\/venue\/5557e160-0f5a-472a-8dab-56f4e70ed15f","Rel":"self"},
-//{"HRef":"http:\/\/www.mysite.com\/maps\/12345","Rel":"map"}],
-//"name":"Test Venue",
-//"version":1}]
+//[{
+//    "address": { "city": "", "postCode": "", "street": "", "streetnumber": "" },
+//    "contact": { "emailAddress": "ian@huddle.com", "name": "Ian", "phoneNumber": "123454678" },
+//    "links": [{ "HRef": "\/\/localhost:59280\/venue\/8b8c66fc-d541-4051-94ed-1699209d69b0", "Rel": "self" },
+//      { "HRef": "http:\/\/www.mysite.com\/maps\/12345", "Rel": "map" }],
+//    "name": "Test Venue",
+//    "version": 1
+//}]
+
 
 //The model
 paramore.model = function () {
