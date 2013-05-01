c1c074a5ae14e793a4f2bbfd184bd694ec9b8571
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-05-02 00:37:53 +0100

    Getting durandal running

---
+++ Rewind/paramore.web/App/data.js
@@ -0,0 +1,41 @@
+﻿// MOCK: Get the real data from a REST endpoint - this is test data captured from the enpoint by Fiddler
+//[{
+//    "address": { "city": "", "postCode": "", "street": "", "streetnumber": "" },
+//    "contact": { "emailAddress": "ian@huddle.com", "name": "Ian", "phoneNumber": "123454678" },
+//    "links": [{ "HRef": "\/\/localhost:59280\/venue\/8b8c66fc-d541-4051-94ed-1699209d69b0", "Rel": "self" },
+//      { "HRef": "http:\/\/www.mysite.com\/maps\/12345", "Rel": "map" }],
+//    "name": "Test Venue",
+//    "version": 1
+//}]
+
+
+//The model
+define([],  function () {
+    var venues = {
+        rows: [
+            {
+                "address": { "streetNumber": "100", "street": "City Road", "city" : "London", "postCode" : "EC1Y 2BP" },
+                "contact": { "name": "Ian", "emailAddress": "ian@huddle.com", "phoneNumber": "123454678" },
+                "links": [
+                    { "HRef": "\/\/localhost:59280\/venue\/5557e160-0f5a-472a-8dab-56f4e70ed15f", "Rel": "self" },
+                    { "HRef": "http:\/\/goo.gl\/maps\/OX8n0", "Rel": "map" }],
+                "name": "Huddle",
+                "version": "1"
+            },
+            {
+                "address": { "streetNumber": "123", "street": "Sesame Street", "city" : "New York", "postCode" : "10128" },
+                "contact": { "name": "Elmo", "emailAddress": "elmo@bigbird.com", "phoneNumber": "123454678" },
+                "links": [
+                    { "HRef": "\/\/localhost:59280\/venue\/5557e160-0f5a-472a-8dab-56f4e70ed15f", "Rel": "self" },
+                    { "HRef": "http:\/\/goo.gl\/maps\/vN5Gk", "Rel": "map" }],
+                "name": "Hooper's Store",
+                "version": "1"
+            }
+        ]   
+    };
+    
+
+    return {
+        venues: venues
+    };
+});
---
+++ Rewind/paramore.web/App/viewmodels/venues.js
@@ -0,0 +1,62 @@
+﻿define(['durandal/system', 'durandal/app', 'durandal/data', 'durandal/logger'], function(system, app, data, logger) {
+    var venueList= ko.observableArray([]);
+
+   //The viewmodel
+   var venues = {
+        venueList: venueList,
+        addVenue: addVenue,
+        loadVenues: loadVenues,
+        activate: activate
+    };
+
+   return venues;
+    
+    //for creating venues
+    function Venue() {
+        this.name = ko.observable(paramore.model.name);
+        this.streetNumber = ko.observable();
+        this.street = ko.observable();
+        this.city = ko.observable();
+        this.postcode = ko.observable();
+        this.contactName = ko.observable();
+        this.emailAddress = ko.observable();
+        this.phoneNumber = ko.observable();
+        this.map = ko.observable();
+        this.self = ko.observable();
+        this.version = ko.observable();
+
+    };
+
+    function addVenue() {
+        venues.push(new Venue());
+    };
+    function loadVenues() {
+        $.each(data.venues.rows, function(i, v) {
+            venueList.push(new Venue()
+                .name(v.name)
+                .streetNumber(v.address.streetNumber)
+                .street(v.address.street)
+                .city(v.address.city)
+                .postcode(v.address.postCode)
+                .contactName(v.contact.name)
+                .emailAddress(v.contact.emailAddress)
+                .phoneNumber(v.contact.phoneNumber)
+                .map(v.links[1].HRef)
+                .self(v.links[0].HRef)
+                .version(v.version)
+            );
+        });
+    };
+    function activate () {
+        //the router's activator calls this function and waits for it to complete before proceding
+        //Note: Data bind the values between the source and the targets using Knockout
+        logger.log('Venue view activate',
+                null,
+                system.getModuleId(venueList),
+                true);
+            loadVenues();
+        ko.applyBindings(venues);
+    };
+
+ 
+});
