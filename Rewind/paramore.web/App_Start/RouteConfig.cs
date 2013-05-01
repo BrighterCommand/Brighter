636a3dc86b3ba31ef297685229c1e8f3bdc3ff92
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-05-01 17:27:22 +0100

    Getting Durandal working

--- Rewind/paramore.web/App_Start/RouteConfig.cs
+++ Rewind/paramore.web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace paramore.web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Durandal", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
