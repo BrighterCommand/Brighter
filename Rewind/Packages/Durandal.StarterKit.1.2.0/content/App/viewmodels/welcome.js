a12b9a7df7e459d1c7f80f2158cce3bb9fe0cc86
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-05-01 15:42:24 +0100

    Adding Durandal

---
+++ Rewind/Packages/Durandal.StarterKit.1.2.0/content/App/viewmodels/welcome.js
@@ -0,0 +1,25 @@
+﻿define(function() {
+    var welcome = function () {
+        this.displayName = 'Welcome to the Durandal Starter Kit!';
+        this.description = 'Durandal is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
+        this.features = [
+            'Clean MV* Architecture',
+            'JS & HTML Modularity',
+            'Simple App Lifecycle',
+            'Eventing, Modals, Message Boxes, etc.',
+            'Navigation & Screen State Management',
+            'Consistent Async Programming w/ Promises',
+            'App Bundling and Optimization',
+            'Use any Backend Technology',
+            'Built on top of jQuery, Knockout & RequireJS',
+            'Integrates with other libraries such as SammyJS & Bootstrap',
+            'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
+        ];
+    };
+
+    welcome.prototype.viewAttached = function (view) {
+        //you can get the view after it's bound and connected to it's parent dom node if you want
+    };
+
+    return welcome;
+});
---
+++ Rewind/paramore.web/App/viewmodels/shell.js
@@ -0,0 +1,14 @@
+﻿define(['durandal/plugins/router', 'durandal/app'], function (router, app) {
+
+    return {
+        router: router,
+        search: function() {
+            //It's really easy to show a message box.
+            //You can add custom options too. Also, it returns a promise for the user's response.
+            app.showMessage('Search not yet implemented...');
+        },
+        activate: function () {
+            return router.activate('welcome');
+        }
+    };
+});
