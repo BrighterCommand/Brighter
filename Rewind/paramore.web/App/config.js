d78eee6c7fdd1c370c73b990d8041262616e1220
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-05-04 17:20:22 +0100

    Use a routing table; add moment; check we have all the images

---
+++ Rewind/paramore.web/App/config.js
@@ -0,0 +1,38 @@
+﻿define(['durandal/plugins/router', 'durandal/viewLocator'],
+    function (router, viewLocator) {
+        var routes = [
+            {
+                url: 'welcome',
+                moduleId: 'viewmodels/welcome',
+                name: 'Welcome',
+                visible: true
+            },
+            {
+                url: 'filckr',
+                moduleId: 'viewmodels/flickr',
+                name: 'flickr',
+                visible: true
+            },
+            {
+                url: 'venues',
+                moduleId: 'viewmodels/venues',
+                name: 'Venues',
+                visible: true
+            }
+        ];
+
+        var config = {
+           initialize : initialize, 
+           routes: routes,
+           startModule: 'welcome'
+        };
+        return config;
+        
+        function initialize() {
+            //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+            //Look for partial views in a 'views' folder in the root.
+            viewLocator.useConvention();
+            //configure routing
+            router.useConvention();
+        };
+    });
--- Rewind/paramore.web/App_Start/DurandalBundleConfig.cs
+++ Rewind/paramore.web/App_Start/DurandalBundleConfig.cs
@@ -13,6 +13,8 @@ namespace paramore.web {
           .Include("~/Scripts/knockout-{version}.js")
           .Include("~/Scripts/sammy-{version}.js")
           .Include("~/Scripts/bootstrap.min.js")
+          .Include("~/Scripts/moment.min.js")
+          .Include("~/Scripts/moment.datepicker.min.js")
         );
 
       bundles.Add(
