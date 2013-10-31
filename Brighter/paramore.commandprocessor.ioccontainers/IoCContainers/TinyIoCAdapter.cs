037c20af9b6e16a2d1c66a2a4e17e0de337c2420
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2013-10-31 19:10:28 +0000

    moving brighter example to OR

--- Brighter/paramore.commandprocessor.ioccontainers/IoCContainers/TinyIoCAdapter.cs
+++ Brighter/paramore.commandprocessor.ioccontainers/IoCContainers/TinyIoCAdapter.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using Nancy.TinyIoc;
+using TinyIoC;
 
 namespace paramore.commandprocessor.ioccontainers.IoCContainers
 {
