40a9380b44447e10e9d60f756a3902f51e12c329
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-01-07 13:35:04 +0000

    Add Brighter dependencies for build

---
+++ Brighter/packages/Machine.Specifications.0.6.1/tools/InstallResharperRunner.8.0.bat
@@ -0,0 +1,10 @@
+del "%APPDATA%\JetBrains\ReSharper\v8.0\vs10.0\Plugins\Machine.Specifications.*" 2> NUL
+del "%APPDATA%\JetBrains\ReSharper\v8.0\vs11.0\Plugins\Machine.Specifications.*" 2> NUL
+
+mkdir "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins" 2> NUL
+mkdir "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" 2> NUL
+copy /y Machine.Specifications.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
+copy /y Machine.Specifications.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
+copy /y Machine.Specifications.ReSharperRunner.8.0.dll "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec"
+copy /y Machine.Specifications.ReSharperRunner.8.0.pdb "%APPDATA%\JetBrains\ReSharper\v8.0\Plugins\mspec" > NUL
+pause
---
+++ Brighter/packages/Machine.Specifications.0.6.1/tools/InstallResharperRunner.8.1.bat
@@ -0,0 +1,10 @@
+del "%APPDATA%\JetBrains\ReSharper\v8.1\vs10.0\Plugins\Machine.Specifications.*" 2> NUL
+del "%APPDATA%\JetBrains\ReSharper\v8.1\vs11.0\Plugins\Machine.Specifications.*" 2> NUL
+
+mkdir "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins" 2> NUL
+mkdir "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" 2> NUL
+copy /y Machine.Specifications.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
+copy /y Machine.Specifications.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
+copy /y Machine.Specifications.ReSharperRunner.8.1.dll "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec"
+copy /y Machine.Specifications.ReSharperRunner.8.1.pdb "%APPDATA%\JetBrains\ReSharper\v8.1\Plugins\mspec" > NUL
+pause
