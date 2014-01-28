c5dcebba0638c3266d83ecbc767ea2497ee7d937
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-01-28 18:15:56 +0000

    first stab at python based acceptance tests for brighter

--- Brighter/Examples/AcceptanceTests/AcceptanceTests.py
+++ Brighter/Examples/AcceptanceTests/AcceptanceTests.py
@@ -1,1 +1,7 @@
-print('Hello World')
+import requests
+
+def test_we_can_get_all_tasks():
+    payload = {'taskName': 'test1', 'taskDescription': 'Attempting to test an api from python', 'dueDate':'2012-04-23T18:25:43.511Z'}
+    requests.post('http://localhost:49743/tasks', data=payload)
+    r = requests.get('http://localhost:49743/tasks')
+    print(r)
