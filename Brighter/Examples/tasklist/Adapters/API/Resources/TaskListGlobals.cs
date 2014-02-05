bc2a34a656a3c4bb010da712d6de0e14d5e68e89
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-02-05 10:56:57 +0000

    Working on fixing the task list

---
+++ Brighter/Examples/tasklist/Adapters/API/Resources/TaskListGlobals.cs
@@ -0,0 +1,12 @@
+namespace Tasklist.Adapters.API.Resources
+{
+    internal class TaskListGlobals
+    {
+        private const string hostName = "localhost:49743";
+
+        public static string HostName
+        {
+            get { return HostName; }
+        }
+    }
+}
