5804b1b5df2dd034f45e3fd0b4e3c8f28829569b
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-02-12 22:33:29 +0000

    Fix the source of the hostname

--- Brighter/Examples/tasklist/Adapters/Tests/TaskListRetrieveFixture.cs
+++ Brighter/Examples/tasklist/Adapters/Tests/TaskListRetrieveFixture.cs
@@ -12,10 +12,11 @@ namespace Tasklist.Adapters.Tests
     public class When_retrieving_a_list_of_tasks
     {
         static TasksDAO dao;
-        static readonly TaskListRetriever retriever = new TaskListRetriever();
+        private static TaskListRetriever retriever; 
         static Task newTask;
         private static int taskId;
         private static TaskListModel taskList;
+        private static string hostName = "http://localhost:49743";
 
         Establish context = () =>
             {
@@ -24,11 +25,13 @@ namespace Tasklist.Adapters.Tests
                 newTask = new Task(taskName: "Test Name", taskDecription: "Task Description", dueDate: DateTime.Now);
                 var addedTask = dao.Add(newTask);
                 taskId = addedTask.Id;
+
+                retriever = new TaskListRetriever(hostName);
             };
 
         Because of = () => taskList = retriever.RetrieveTasks();
 
-        It should_have_a_self_link = () => taskList.Self.ToString().ShouldEqual(string.Format("<link rel=\"self\" href=\"http://{0}/tasks\" />",TaskListGlobals.HostName));
-        It should_have_a_link_in_the_list_to_the_task = () => taskList.Links.Any(taskLink => taskLink.HRef == string.Format("<link rel=\"item\" href=\"http://{0}/task/{1}\" />", TaskListGlobals.HostName, taskId));
+        It should_have_a_self_link = () => taskList.Self.ToString().ShouldEqual(string.Format("<link rel=\"self\" href=\"http://{0}/tasks\" />", hostName));
+        It should_have_a_link_in_the_list_to_the_task = () => taskList.Links.Any(taskLink => taskLink.HRef == string.Format("<link rel=\"item\" href=\"http://{0}/task/{1}\" />", hostName, taskId));
     }
 }
