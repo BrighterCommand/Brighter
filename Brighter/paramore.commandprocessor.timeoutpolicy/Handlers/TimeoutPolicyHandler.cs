e0cc884849414454966bd25373a67f4d6a9daa91
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-03-27 18:35:43 +0000

    Adding MIT license information to source

--- Brighter/paramore.commandprocessor.timeoutpolicy/Handlers/TimeoutPolicyHandler.cs
+++ Brighter/paramore.commandprocessor.timeoutpolicy/Handlers/TimeoutPolicyHandler.cs
@@ -1,8 +1,35 @@
+#region Licence
+/* 
+ 
+TimeoutAfter method derived from work by Joe Hoag http://blogs.msdn.com/b/pfxteam/archive/2011/11/10/10235834.aspx 
+No permissions is granted to the portions of the source code that derive from Joe Hoag's which is excluded from the following licence
+ 
+The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+#endregion
+
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-//TImeoutAfter method after Joe Hoag http://blogs.msdn.com/b/pfxteam/archive/2011/11/10/10235834.aspx 
 
 namespace paramore.brighter.commandprocessor.timeoutpolicy.Handlers
 {
@@ -43,6 +70,8 @@ namespace paramore.brighter.commandprocessor.timeoutpolicy.Handlers
 
             return task.Result;
         }
+
+        #region Not covered by MIT licence
 
         private Task<TRequest> TimeoutAfter(Task<TRequest> task, int millisecondsTimeout, CancellationTokenSource cancellationTokenSource)
         {
@@ -121,6 +150,8 @@ namespace paramore.brighter.commandprocessor.timeoutpolicy.Handlers
                     break;
             }
         }
-        
+
+        #endregion
+
     }
 }
