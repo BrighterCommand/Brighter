6d6febd945f3808a985845045289cf288613872e
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-04-01 12:10:04 +0100

    Fix up the basic rabbitmq example

--- Brighter/paramore.brighter.commandprocessor.messaginggateway.rmq/MessagingGatewayConfiguration/RMQMessagingGatewayConfigurationSection.cs
+++ Brighter/paramore.brighter.commandprocessor.messaginggateway.rmq/MessagingGatewayConfiguration/RMQMessagingGatewayConfigurationSection.cs
@@ -32,18 +32,18 @@ namespace paramore.brighter.commandprocessor.messaginggateway.rmq.MessagingGatew
 
     public class AMQPUriSpecification : ConfigurationElement
     {
-        [ConfigurationProperty("Uri", IsRequired = true)]
+        [ConfigurationProperty("uri", DefaultValue = "amqp://guest:guest@localhost:5672/%2f", IsRequired = true)]
         public Uri Uri
         {
-            get { return new Uri((string)this["Uri"]); }
-            set { this["Uri"] = value.ToString(); }
+            get { return (Uri)this["uri"]; }
+            set { this["uri"] = value; }
         }
         
     }
 
     public class Exchange : ConfigurationElement
     {
-        [ConfigurationProperty("name", IsRequired = true)]
+        [ConfigurationProperty("name", DefaultValue = "", IsRequired = true)]
         public string Name
         {
             get { return this["name"] as string; }
