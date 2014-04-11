e02f794be1ec1db1ad85ff48606b5af5c5bf67dc
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-04-11 12:40:22 +0100

    First pass at creating nuget sources for Paramore.Brighter

--- Brighter/paramore.brighter.commandprocessor.timeoutpolicy/Properties/AssemblyInfo.cs
+++ Brighter/paramore.brighter.commandprocessor.timeoutpolicy/Properties/AssemblyInfo.cs
@@ -28,13 +28,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("paramore.commandprocessor.timeoutpolicy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Huddle")]
+[assembly: AssemblyDescription("The Command Dispatchter pattern is an addition to the Command design pattern that decouples the dispatcher for a service from its execution. A Command Dispatcher component maps commands to handlers. A Command Processor pattern provides a  framework for handling orthogonal concerns such as logging, timeouts, or circuit breakers")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("Ian Cooper")]
 [assembly: AssemblyProduct("paramore.commandprocessor.timeoutpolicy")]
-[assembly: AssemblyCopyright("Copyright © Huddle 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright ©  2014 Ian Cooper")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
