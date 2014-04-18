29071b962f614ce6ff8a1839e50df088419a521d
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-04-19 00:37:37 +0100

    update nuget package for common.logging dependency

---
+++ Brighter/paramore.brighter.commandprocessor.tests/MessageDispatcher/MessageDispatchFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paramore.commandprocessor.tests.MessageDispatcher
+{
+    class MessageDispatchFixture
+    {
+    }
+}
