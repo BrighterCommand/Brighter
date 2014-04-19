8e6158095bae6f08fff8d9f4fa23f969401777a5
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-04-19 01:02:00 +0100

    Add some utility logging handlers

---
+++ Brighter/paramore.brighter.commandprocessor/RequestLoggingHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Common.Logging;
+
+namespace paramore.brighter.commandprocessor
+{
+    public class RequestLoggingHandler<TRequest> : RequestHandler<TRequest> where TRequest : class, IRequest
+    {
+        private readonly ILog logger;
+
+        public RequestLoggingHandler(ILog logger)
+        {
+            this.logger = logger;
+        }
+
+        public override TRequest Handle(TRequest command)
+        {
+            LogCommand(command);
+            return base.Handle(command);
+        }
+        
+        private void LogCommand(TRequest request)
+        {
+            logger.Info(m => m("Logging request type {0} with request id {1) at {2}", request.GetType().ToString(), request.Id, DateTime.UtcNow));
+        }
+    }
+}
---
+++ Brighter/paramore.brighter.commandprocessor/RequestLoggingHandlerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace paramore.brighter.commandprocessor
+{
+    public class RequestLoggingHandlerAttribute : RequestHandlerAttribute
+    {
+        public RequestLoggingHandlerAttribute(int step)
+            : base(step)
+        {
+        }
+
+        public override Type GetHandlerType()
+        {
+            return typeof(RequestLoggingHandler<>);
+        }
+    }}
