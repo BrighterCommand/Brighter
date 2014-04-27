da7701459fe4f67c3f0732e3d41c6ab4bab1d694
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-04-27 11:20:55 +0100

    Increment nuget packages; missing file

--- Brighter/Paramore.NuGetPushAll.bat
+++ Brighter/Paramore.NuGetPushAll.bat
@@ -1,6 +1,8 @@
-NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor\Paramore.Brighter.CommandProcessor.1.0.0-alpha.nupkg
-NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.exceptionpolicy\Paramore.Brighter.CommandProcessor.ExceptionPolicy.1.0.0-alpha.nupkg
-NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.ioccontainers.tinyioc\paramore.brighter.commandprocessor.ioccontainers.tinyioc.1.0.0-alpha.nupkg
-Nuget Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.messagestore.ravendb\paramore.brighter.commandprocessor.messagestore.ravendb.1.0.0-alpha.nupkg
-NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.messaginggateway.rmq\paramore.brighter.commandprocessor.messaginggateway.rmq.1.0.0-alpha.nupkg
-NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.timeoutpolicy\paramore.brighter.commandprocessor.timeoutpolicy.1.0.0-alpha.nupkg
+NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor\Paramore.Brighter.CommandProcessor.1.0.0-alpha001.nupkg
+NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.exceptionpolicy\Paramore.Brighter.CommandProcessor.ExceptionPolicy.1.0.0-alpha001.nupkg
+NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.ioccontainers.tinyioc\paramore.brighter.commandprocessor.ioccontainers.tinyioc.1.0.0-alpha001.nupkg
+Nuget Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.messagestore.ravendb\paramore.brighter.commandprocessor.messagestore.ravendb.1.0.0-alpha001.nupkg
+NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.messaginggateway.rmq\paramore.brighter.commandprocessor.messaginggateway.rmq.1.0.0-alpha001.nupkg
+NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.commandprocessor.timeoutpolicy\paramore.brighter.commandprocessor.timeoutpolicy.1.0.0-alpha001.nupkg
+NuGet Push C:\Users\SUNDANCE\iancooper\Paramore\Brighter\paramore.brighter.serviceactivator\paramore.brighter.serviceactivator.1.0.0-alpha001.nupkg
+
