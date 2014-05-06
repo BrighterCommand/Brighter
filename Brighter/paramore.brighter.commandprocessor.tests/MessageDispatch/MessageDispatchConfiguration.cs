54df8c72ec6bd6eb1c465d0b28db70069f2ea692
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-05-07 00:03:58 +0100

    Adding configuration

---
+++ Brighter/paramore.brighter.commandprocessor.tests/MessageDispatch/MessageDispatchConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Common.Logging;
+using FakeItEasy;
+using Machine.Specifications;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.serviceactivator;
+using paramore.brighter.serviceactivator.ServiceActivatorConfiguraton;
+
+namespace paramore.commandprocessor.tests.MessageDispatch
+{
+    public class When_configuring_a_message_dispatcher
+    {
+        static ILog logger;
+        static ConnectionElements connectionElements;
+        static IEnumerable<Connection> connections;
+        static IAdaptAnInversionOfControlContainer container;
+        static Dispatcher dispatcher;
+
+        Establish configuration = () =>
+        {
+            logger = A.Fake<ILog>();
+            container = A.Fake<IAdaptAnInversionOfControlContainer>();
+
+            var configuration = ServiceActivatorConfigurationSection.GetConfiguration();
+            connectionElements = configuration.Connections;
+            connections = ConnectionFactory.Create(connectionElements);
+        };
+
+        Because of = () => dispatcher = new Dispatcher(container, connections, logger);
+    }
+}
---
+++ Brighter/paramore.brighter.serviceactivator/ServiceActivatorConfiguraton/ConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace paramore.brighter.serviceactivator.ServiceActivatorConfiguraton
+{
+    public static class ConnectionFactory
+    {
+        public static IEnumerable<Connection> Create(ConnectionElements connectionElements)
+        {
+            return null;
+        }
+    }
+}
