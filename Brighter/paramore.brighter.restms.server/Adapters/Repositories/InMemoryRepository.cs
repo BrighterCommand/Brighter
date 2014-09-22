86ee25672dad36ee5b8bede74f72fdf340862a87
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-09-22 21:37:59 +0100

    missing files

---
+++ Brighter/paramore.brighter.restms.server/Adapters/Repositories/InMemoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using paramore.brighter.restms.server.Ports.Common;
+
+namespace paramore.brighter.restms.server.Adapters.Repositories
+{
+    public class InMemoryRepository<T> : IAmARepository<T> where T: class, IAmAnAggregate
+    {
+        readonly Dictionary<Identity, T> domains = new  Dictionary<Identity, T>();
+        public void Add(T aggregate)
+        {
+            domains.Add(aggregate.Id, aggregate);
+        }
+
+        public T this[Identity index]
+        {
+            get
+            {
+                if (domains.ContainsKey(index))
+                    return domains[index];
+                else
+                    return null;
+            }
+        }
+    }
+}
