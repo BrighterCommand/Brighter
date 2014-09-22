c99d254967c7170453bc0b65ea60a118510434f5
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-09-22 21:37:59 +0100

    missing files

---
+++ Brighter/paramore.brighter.restms.server/Adapters/Repositories/InMemoryFeedRepository.cs
@@ -0,0 +1,8 @@
+﻿using paramore.brighter.restms.server.Model;
+
+namespace paramore.brighter.restms.server.Adapters.Repositories
+{
+    public class InMemoryFeedRepository : InMemoryRepository<Feed>
+    {
+    }
+}
