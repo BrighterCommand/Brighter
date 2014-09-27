0680bea67fec67d7cfc0fba05cef7e8a2df42f89
iancooper <ian_hammond_cooper@yahoo.co.uk>
2014-09-27 10:54:50 +0100

    Split restms between testable core and adapter layer which we don't want to unit test. Prevents issues where we pull TinyIoC into the unit tests which already has a TinyIoC implementation for testing purposes

---
+++ Brighter/paramore.brighter.restms.core/Ports/Repositories/InMemoryFeedRepository.cs
@@ -0,0 +1,8 @@
+﻿using paramore.brighter.restms.core.Model;
+
+namespace paramore.brighter.restms.core.Ports.Repositories
+{
+    public class InMemoryFeedRepository : InMemoryRepository<Feed>
+    {
+    }
+}
