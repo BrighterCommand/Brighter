f41726884f06414ea004846493b52f6ba400ab80
iancooper <ian_hammond_cooper@yahoo.co.uk>
2014-10-01 13:30:23 +0100

    Include suppression files for Code Analysis output

--- Brighter/paramore.brighter.restms.server/Adapters/Service/UnityResolver.cs
+++ Brighter/paramore.brighter.restms.server/Adapters/Service/UnityResolver.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Web.Http.Dependencies;
 using Microsoft.Practices.Unity;
@@ -7,13 +30,10 @@ namespace paramore.brighter.restms.server.Adapters.Service
 {
 public class UnityResolver : IDependencyResolver
 {
-    readonly IUnityContainer container;
+    static readonly UnityContainer container = new UnityContainer();
     bool disposed;
 
-    public UnityResolver(IUnityContainer container)
-    {
-        this.container = container;
-    }
+    public static UnityContainer Instance {get { return container; }}
 
     public object GetService(Type serviceType)
     {
@@ -42,7 +62,7 @@ public class UnityResolver : IDependencyResolver
     public IDependencyScope BeginScope()
     {
         var child = container.CreateChildContainer();
-        return new UnityResolver(child);
+        return new UnityResolver();
     }
 
     public void Dispose()
