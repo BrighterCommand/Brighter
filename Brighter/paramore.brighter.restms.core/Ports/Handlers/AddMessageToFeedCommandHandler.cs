55f89d221c8d8ff4e48deb345fb4c58067eb5b76
iancooper <ian_hammond_cooper@yahoo.co.uk>
2014-10-17 13:28:58 +0100

    Work on restms server

---
+++ Brighter/paramore.brighter.restms.core/Ports/Handlers/AddMessageToFeedCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Common.Logging;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.restms.core.Ports.Commands;
+
+namespace paramore.brighter.restms.core.Ports.Handlers
+{
+    public class AddMessageToFeedCommandHandler : RequestHandler<AddMessageToFeedCommand>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestHandler{TRequest}"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public AddMessageToFeedCommandHandler(ILog logger) : base(logger)
+        {
+        }
+    }
+}
