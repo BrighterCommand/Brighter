29ef712ca6539ce103ae4a766f4de8c57d2e12d8
iancooper <ian_hammond_cooper@yahoo.co.uk>
2014-11-03 18:23:17 +0000

    Working on restms server details, deleting a message and older messages

---
+++ Brighter/paramore.brighter.restms.core/Ports/Commands/DeleteMessageCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using paramore.brighter.commandprocessor;
+
+namespace paramore.brighter.restms.core.Ports.Commands
+{
+    public class DeleteMessageCommand : Command
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public DeleteMessageCommand() : base(Guid.NewGuid())
+        {
+        }
+    }
+}
---
+++ Brighter/paramore.brighter.restms.core/Ports/Handlers/DeleteMessageCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Common.Logging;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.restms.core.Ports.Commands;
+
+namespace paramore.brighter.restms.core.Ports.Handlers
+{
+    public class DeleteMessageCommandHandler : RequestHandler<DeleteMessageCommand>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestHandler{TRequest}"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public DeleteMessageCommandHandler(ILog logger) : base(logger)
+        {
+        }
+    }
+}
