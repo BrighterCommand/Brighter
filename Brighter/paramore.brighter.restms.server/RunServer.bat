1d5680d0e2616cd8ecacf303963ff1cb781847d9
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2014-12-31 17:40:57 +0000

    reset config to localhost; need to get tokenzed configs

--- Brighter/paramore.brighter.restms.server/RunServer.bat
+++ Brighter/paramore.brighter.restms.server/RunServer.bat
@@ -1,7 +1,5 @@
 REM You need to run at an elevated command prompt if you need to reserve the url you are using
 REM netsh http add urlacl url=http://+:8080/ user=machine\username
-netsh http add urlacl url=http://10.50.4.18:3416/ user=HUDDLE\ian
 .\bin\Debug\paramore.brighter.restms.server.exe
 REM netsh http delete urlacl url=http://+:8080/
-netsh http delete urlaclurl=http://10.50.4.18:3416/
 
