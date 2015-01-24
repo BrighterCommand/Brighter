1898695cd809319d6acc0d2ee1a863c494b897ec
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-01-24 16:48:47 +0000

    Add client side pipelining and caching; fix issues with stale caches on server (WiP)

---
+++ Brighter/paramore.brighter.restms.core/Ports/Handlers/CacheCleaningHandler.cs
@@ -0,0 +1,34 @@
+﻿using Common.Logging;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.restms.core.Ports.Cache;
+using paramore.brighter.restms.core.Ports.Commands;
+
+namespace paramore.brighter.restms.core.Ports.Handlers
+{
+    public class CacheCleaningHandler: RequestHandler<InvalidateCacheCommand>
+    {
+        readonly IAmACache cache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestHandler{TRequest}"/> class.
+        /// </summary>
+        /// <param name="cache">The cache we should clear; implementors will need to wrap their cache in this</param>
+        /// <param name="logger">The logger.</param>
+        public CacheCleaningHandler(IAmACache cache, ILog logger) : base(logger)
+        {
+            this.cache = cache;
+        }
+
+        /// <summary>
+        /// Handles the specified command.
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <returns>TRequest.</returns>
+        public override InvalidateCacheCommand Handle(InvalidateCacheCommand command)
+        {
+            cache.InvalidateResource(command.ResourceToInvalidate);
+            return base.Handle(command);
+        }
+
+    }
+}
---
+++ Brighter/paramore.brighter.restms.server/Adapters/Cache/CacheHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using CacheCow.Server;
+using paramore.brighter.restms.core.Ports.Cache;
+
+namespace paramore.brighter.restms.server.Adapters.Cache
+{
+    internal class CacheHandler : IAmACache
+    {
+        readonly ICachingHandler cachingHandler;
+
+        public CacheHandler(ICachingHandler cachingHandler)
+        {
+            this.cachingHandler = cachingHandler;
+        }
+
+        public void InvalidateResource(Uri resourceToInvalidate)
+        {
+            cachingHandler.InvalidateResource(new HttpRequestMessage(HttpMethod.Get, resourceToInvalidate));
+        }
+
+    }
+}
