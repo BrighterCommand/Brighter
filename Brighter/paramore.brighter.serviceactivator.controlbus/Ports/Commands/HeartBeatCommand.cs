3d5ba1d84c004362413f7d9136c184b1f6d8d170
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-02-11 18:33:57 +0000

    Added a Greeting example to show reading messages from a queue. Intended to be used with the RMQ web portal

---
+++ Brighter/paramore.brighter.serviceactivator.controlbus/Ports/Commands/HeartBeatCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using paramore.brighter.commandprocessor;
+
+namespace paramore.brighter.serviceactivator.controlbus.Ports.Commands
+{
+    public class HeartBeatCommand : Command
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Command"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        public HeartBeatCommand() : base(Guid.NewGuid())
+        {
+        }
+    }
+}
