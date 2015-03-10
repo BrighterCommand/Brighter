78dd04d186f4b755a88add881d98c8c50a874f2f
Ian Pender <ian.pender@huddle.com>
2015-03-10 13:16:39 +0000

    TaskList UI - initial commit - added MVC (jquery, bootstrap) - added CORS cheating dummy Controller - added intiial app.js that calls.

---
+++ Brighter/Examples/TaskListUI/Controllers/TaskListController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace TaskListUI.Controllers
+{
+    public class TaskListController : Controller
+    {
+        // GET: Tasks
+        public async Task<string> Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:49743/");
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage response = await client.GetAsync("tasks");
+            if (response.IsSuccessStatusCode)
+            {
+                var model = response.Content.ReadAsStringAsync().Result;
+
+                return model;
+           }
+
+            return await Task.FromResult<string>(null);
+        }
+    }
+}
