664db8f7e4738d658d9abdbf48f598399794df80
Ian Pender <ian.pender@huddle.com>
2015-03-11 17:00:50 +0000

    TaskListUI - wired in send UI

--- Brighter/Examples/TaskListUI/app/app.js
+++ Brighter/Examples/TaskListUI/app/app.js
@@ -1,6 +1,7 @@
-﻿var listVm = function () {
+﻿var taskVm = function () {
     var baseUri = 'http://localhost:49743/tasks';
-    var getTasks = function(getCallback) {
+    var dueDateFixed = '01-Jan-2014';
+    var getTasksInternal = function(getCallback) {
         $.ajax({
             url: baseUri,
             dataType: 'json',
@@ -9,14 +10,14 @@
         });
     };
     var addTaskInternal = function(taskText, addCallback) {
-        //TODO: sort the date format.
+        //TODO: change FIXED due date, and desc==name
         $.ajax({
             url: baseUri,
             dataType: 'text', //to process location, not json
             type: 'POST',
             success: function(data) { addCallback(data); },
             contentType: "application/json",
-            data: '{"dueDate": "01-Jan-2014", "taskDescription": "' + taskText + '", "taskName": "' + taskText + '"}'
+            data: '{"dueDate": "' + dueDateFixed + '", "taskDescription": "' + taskText + '", "taskName": "' + taskText + '"}'
         });
     };
     var completeTaskInternal = function(taskId, completeCb) {
@@ -27,23 +28,81 @@
             success: function(data) { completeCb(data); }
         });
     };
+    var sendReminderInternal = function (dueDate, taskName, recipient, reminderCb) {
+        $.ajax({
+            url: baseUri + '/reminders',
+            dataType: 'text', //to process location, not json
+            type: 'POST',
+            success: function (data) { reminderCb(data); },
+            contentType: "application/json",
+            data: '{"dueDate": "' + dueDate + '", "recipient": "' + recipient + '", "copyTo": "' + recipient + '", "taskName": "' + taskName + '"}'
+        });
+    }
     return {
-        init: getTasks,
+        getTasks: getTasksInternal,
         addTask: addTaskInternal,
-        completeTask: completeTaskInternal
+        completeTask: completeTaskInternal,
+        sendReminder: sendReminderInternal
     };
 }();
 var refreshTaskList = function() {
-    listVm.init(onTaskLoad);
+    taskVm.getTasks(onTaskLoad);
 };
 var onTaskCompleteClick = function() {
     var taskUri = $(this).parent().find(".taskHref").text();
     var taskId = taskUri.substring(taskUri.lastIndexOf('/') + 1);
-    listVm.completeTask(taskId, onTaskCompletedCb);
+    taskVm.completeTask(taskId, onTaskCompletedCb);
 };
 var onTaskCompletedCb = function(data) {
     refreshTaskList();
 };
+var onReminderSentCb = function(data) {
+    alert('sent it!');
+}
+var onReminderSendClick = function () {
+    var divForm = $(this).parent();
+    var dueDate = divForm.find("input[name=dueDate]").val();
+    var taskName = divForm.find("input[name=taskName]").val();
+    var recipient = divForm.find("input[name=recipient]").val();
+
+    taskVm.sendReminder(dueDate, taskName, recipient, onReminderSentCb);
+}
+var onTaskLoad = function(tl) {
+    tl.items.sort(taskSorter);
+    var content = Mustache.to_html($("#viewTemplate").html(), tl);
+    $("#taskContainer").html(content);
+    bindTaskEvents();
+};
+var onTaskCreatedCb = function(newHref) {
+    refreshTaskList();
+    $("#taskAddName").val("");
+};
+var onTaskAddClick = function() {
+    var parent = $("#taskAddName").parent();
+    var taskText = parent.children().first().val();
+    taskVm.addTask(taskText, onTaskCreatedCb);
+};
+$(document).ready(function() {
+    refreshTaskList();
+    $("#taskAddName").bind('blur keyup', function (e) {
+        if (e.type === 'keyup' && e.keyCode !== 10 && e.keyCode !== 13) return;
+        onTaskAddClick();
+    });
+});
+var onReminderClick = function () {
+    $("#taskList button.mail").show(); 
+    $(this).hide();
+    $("#taskList li div.mailForm").css({ 'display': 'none' }); //hide all
+    var thisForm = $(this).parents("li").find("div.mailForm");
+    thisForm.css({ 'display': 'block' });
+    thisForm.find("button").click(onReminderSendClick);
+}
+var bindTaskEvents = function () {
+    $("#taskContainer")
+        .find(".complete")
+        .click(onTaskCompleteClick);
+    $("#taskList button.mail").bind('click', onReminderClick);
+}
 function taskSorter(a, b) {
     //completed last, then by id
     if (a.isComplete && b.isComplete) {
@@ -63,25 +122,3 @@ function taskSorter(a, b) {
     }
     return -1;
 }
-var onTaskLoad = function(tl) {
-    tl.items.sort(taskSorter);
-    var content = Mustache.to_html($("#viewTemplate").html(), tl);
-    $("#taskContainer").html(content);
-    $("#taskContainer").find(".complete").click(onTaskCompleteClick);
-};
-var onTaskCreated = function(newHref) {
-    refreshTaskList();
-    $("#taskAddName").val("");
-};
-var onTaskAddClick = function() {
-    var parent = $("#taskAddName").parent();
-    var taskText = parent.children().first().val();
-    listVm.addTask(taskText, onTaskCreated);
-};
-$(document).ready(function() {
-    refreshTaskList();
-    $("#taskAddName").bind('blur keyup', function (e) {
-        if (e.type === 'keyup' && e.keyCode !== 10 && e.keyCode !== 13) return;
-        onTaskAddClick();
-    });
-});
