00b26a278b1d354c5c112cbd38e0131084caca83
Ian Pender <ian.pender@huddle.com>
2015-03-11 12:26:47 +0000

    TaskListUI - Deleted all the cruft.

--- Brighter/Examples/TaskListUI/Global.asax.cs
+++ Brighter/Examples/TaskListUI/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace TaskListUI
@@ -13,9 +8,7 @@ namespace TaskListUI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
