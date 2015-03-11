3bf18ddf1982f30b191c265856de5f0abf4a7e97
Ian Pender <ian.pender@huddle.com>
2015-03-11 12:30:48 +0000

    TaskListUI - reformat code.

--- Brighter/Examples/TaskListUI/App_Start/RouteConfig.cs
+++ Brighter/Examples/TaskListUI/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace TaskListUI
@@ -14,10 +10,8 @@ namespace TaskListUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                "Default", "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });
         }
     }
 }
--- Brighter/Examples/TaskListUI/Scripts/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="jquery-2.1.3.js" />
-/// <autosync enabled="true" />
-/// <reference path="bootstrap.js" />
-/// <reference path="respond.js" />
