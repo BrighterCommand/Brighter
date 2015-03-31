3bb63e034b46d32f2c4df355c86e02a6a0867e10
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-03-31 18:39:43 +0100

    Remove RESTMS server from Brighter code; now in own solution

--- Brighter/paramore.brighter.restms.server/RunServer.bat
+++
@@ -1,5 +0,0 @@
-REM You need to run at an elevated command prompt if you need to reserve the url you are using
-REM netsh http add urlacl url=http://+:8080/ user=machine\username
-.\bin\Debug\paramore.brighter.restms.server.exe
-REM netsh http delete urlacl url=http://+:8080/
-
