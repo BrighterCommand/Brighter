5ed0807c586846a2595aa268a3eef61cae59b055
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-05-18 10:35:40 +0100

    Missing Files

---
+++ Brighter/Examples/ManagementAndMonitoring/Adapters/ServiceHost/MeetingAndManagementService.cs
@@ -0,0 +1,134 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+using ManagementAndMonitoring.Adapters.ServiceHost;
+using ManagementAndMonitoring.Ports.CommandHandlers;
+using ManagementAndMonitoring.Ports.Commands;
+using ManagementAndMonitoring.Ports.Mappers;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.commandprocessor.Logging;
+using paramore.brighter.commandprocessor.messaginggateway.rmq;
+using paramore.brighter.serviceactivator;
+using paramore.brighter.serviceactivator.controlbus;
+using Polly;
+using TinyIoC;
+using Topshelf;
+
+namespace Greetings.Adapters.ServiceHost
+{
+    internal class MeetingAndManagementService : ServiceControl
+    {
+        private Dispatcher _dispatcher;
+        private Dispatcher _controlDispatcher;
+
+        public MeetingAndManagementService()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            //Create a logger
+            var logger = LogProvider.For<MeetingAndManagementService>();
+
+            var container = new TinyIoCContainer();
+            container.Register<ILog>(logger);
+
+            var handlerFactory = new TinyIocHandlerFactory(container);
+            var messageMapperFactory = new TinyIoCMessageMapperFactory(container);
+            container.Register<IHandleRequests<GreetingCommand>, GreetingCommandHandler>();
+
+            var subscriberRegistry = new SubscriberRegistry();
+            subscriberRegistry.Register<GreetingCommand, GreetingCommandHandler>();
+
+            //create policies
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetry(new[]
+                    {
+                        TimeSpan.FromMilliseconds(50),
+                        TimeSpan.FromMilliseconds(100),
+                        TimeSpan.FromMilliseconds(150)
+                    });
+
+            var circuitBreakerPolicy = Policy
+                .Handle<Exception>()
+                .CircuitBreaker(1, TimeSpan.FromMilliseconds(500));
+
+            var policyRegistry = new PolicyRegistry()
+            {
+                {CommandProcessor.RETRYPOLICY, retryPolicy},
+                {CommandProcessor.CIRCUITBREAKER, circuitBreakerPolicy}
+            };
+
+            //create message mappers
+            var messageMapperRegistry = new MessageMapperRegistry(messageMapperFactory)
+            {
+                {typeof(GreetingCommand), typeof(GreetingCommandMessageMapper)}
+            };
+
+            //create the gateway
+            var rmqMessageConsumerFactory = new RmqMessageConsumerFactory(logger);
+            var builder = DispatchBuilder
+                .With()
+                .Logger(logger)
+                .CommandProcessor(CommandProcessorBuilder.With()
+                    .Handlers(new HandlerConfiguration(subscriberRegistry, handlerFactory))
+                    .Policies(policyRegistry)
+                    .Logger(logger)
+                    .NoTaskQueues()
+                    .RequestContextFactory(new InMemoryRequestContextFactory())
+                    .Build()
+                 )
+                 .MessageMappers(messageMapperRegistry)
+                 .ChannelFactory(new InputChannelFactory(rmqMessageConsumerFactory))
+                 .ConnectionsFromConfiguration();
+            _dispatcher = builder.Build();
+
+            var controlBusBuilder = ControlBusBuilder
+                .With()
+                .Logger(logger)
+                .Dispatcher(_dispatcher)
+                .ChannelFactory(new InputChannelFactory(rmqMessageConsumerFactory)) as ControlBusBuilder;
+            _controlDispatcher = builder.Build();
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            _dispatcher.Receive();
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _dispatcher.End().Wait();
+            _dispatcher = null;
+            return false;
+        }
+
+        public void Shutdown(HostControl hostcontrol)
+        {
+            if (_dispatcher != null)
+                _dispatcher.End();
+            return;
+        }
+    }
+}
