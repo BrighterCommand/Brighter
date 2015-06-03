1f1410052a472037d734d85f9b12d87f59865ad5
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-06-03 12:34:56 +0100

    Issue #86 support for event soucing (command sourcing approach) within Brighter. In-memory only,  no documentation as yet

---
+++ Brighter/paramore.brighter.commandprocessor.tests/EventSourcing/CommandStoreTests.cs
@@ -0,0 +1,67 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2015 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using FakeItEasy;
+using Machine.Specifications;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.commandprocessor.Logging;
+using paramore.commandprocessor.tests.CommandProcessors.TestDoubles;
+using paramore.commandprocessor.tests.EventSourcing.TestDoubles;
+using TinyIoC;
+
+namespace paramore.commandprocessor.tests.EventSourcing
+{
+    public class When_handling_a_command_with_a_command_store_enabled
+    {
+        private static MyCommand s_command;
+        private static IAmACommandStore s_commandstore;
+        private static IAmACommandProcessor s_commandProcessor;
+
+        private Establish context = () =>
+        {
+            var logger = A.Fake<ILog>();
+
+            s_commandstore = new InMemoryCommandStore();
+
+            var registry = new SubscriberRegistry();
+            registry.Register<MyCommand, MyStoredCommandHandler>();
+
+            var container = new TinyIoCContainer();
+            var handlerFactory = new TinyIocHandlerFactory(container);
+            container.Register<IHandleRequests<MyCommand>, MyStoredCommandHandler>();
+            container.Register<IAmACommandStore>(s_commandstore);
+            container.Register<ILog>(logger);
+
+            s_command = new MyCommand();
+            s_command.Value = "My Test String";
+
+            s_commandProcessor = new CommandProcessor(registry, handlerFactory, new InMemoryRequestContextFactory(), new PolicyRegistry(), logger);
+
+        };
+
+        private Because of = () => s_commandProcessor.Send(s_command);
+
+        private It should_store_the_command_to_the_command_store = () => s_commandstore.Find<MyCommand>(s_command.Id).Value.ShouldEqual(s_command.Value);
+    }
+}
---
+++ Brighter/paramore.brighter.commandprocessor/InMemoryCommandStore.cs
@@ -0,0 +1,117 @@
+﻿// ***********************************************************************
+// Assembly         : paramore.brighter.commandprocessor
+// Author           : ian
+// Created          : 06-03-2015
+//
+// Last Modified By : ian
+// Last Modified On : 06-03-2015
+// ***********************************************************************
+// <copyright file="InMemoryCommandStore.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+#region Licence
+/* The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace paramore.brighter.commandprocessor
+{
+    /// <summary>
+    /// Class InMemoryCommandStore.
+    /// This is mainly intended to support developer tests where a persistent command store is not needed
+    /// </summary>
+    public class InMemoryCommandStore : IAmACommandStore
+    {
+        private readonly Dictionary<Guid, CommandStoreItem> _commands = new Dictionary<Guid, CommandStoreItem>();
+
+        public void Add<T>(Guid id, T command) where T : class, IRequest
+        {
+            if (!_commands.ContainsKey(id))
+            {
+                _commands.Add(id, new CommandStoreItem(typeof(T), string.Empty));
+            }
+
+            _commands[id].CommandBody = JsonConvert.SerializeObject(command);
+        }
+
+        /// <summary>
+        /// Finds the command with the specified identifier.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>ICommand.</returns>
+        public T Find<T>(Guid id) where T:class, IRequest
+        {
+            if (!_commands.ContainsKey(id))
+                return null;
+
+            var commandStoreItem = _commands[id];
+            if (commandStoreItem.CommandType != typeof(T))
+                throw new TypeLoadException(string.Format("The type of item {0) is {1} not{2}", id, commandStoreItem.CommandType.Name, typeof(T).Name));
+
+            return JsonConvert.DeserializeObject<T>(commandStoreItem.CommandBody);
+        }
+
+                
+        /// <summary>
+        /// Class CommandStoreItem.
+        /// </summary>
+        class CommandStoreItem
+        {
+            /// <summary>
+            /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+            /// </summary>
+            public CommandStoreItem(Type commandType, string commandBody, DateTime commandWhen)
+            {
+                CommandType = commandType;
+                CommandBody = commandBody;
+                CommandWhen = commandWhen;
+            }
+
+            public CommandStoreItem(Type commandType, string commandBody) : this(commandType, commandBody, DateTime.UtcNow){}
+
+            /// <summary>
+            /// Gets or sets the type of the command.
+            /// </summary>
+            /// <value>The type of the command.</value>
+            public Type CommandType { get; set; }            
+            /// <summary>
+            /// Gets or sets the command body.
+            /// </summary>
+            /// <value>The command body.</value>
+            public string CommandBody { get; set; }
+
+            /// <summary>
+            /// Gets or sets the command when.
+            /// </summary>
+            /// <value>The command when.</value>
+            public DateTime CommandWhen { get; set; }
+        }
+
+    }
+}
