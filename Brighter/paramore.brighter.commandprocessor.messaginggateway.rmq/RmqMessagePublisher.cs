b21d9132077759e132a208d9cad1171198878986
yiannis <yiannis@huddle.net>
2015-07-01 14:47:43 +0100

    #101 Updated Requeue method to send a message to a specific queue as opposed to a topic

--- Brighter/paramore.brighter.commandprocessor.messaginggateway.rmq/RmqMessagePublisher.cs
+++ Brighter/paramore.brighter.commandprocessor.messaginggateway.rmq/RmqMessagePublisher.cs
@@ -78,13 +78,10 @@ namespace paramore.brighter.commandprocessor.messaginggateway.rmq
         /// <param name="message">The message.</param>
         /// <param name="headers">User specified message headers.</param>
         /// <param name="regenerate">Generate new unique message identifier.</param>
-        public void PublishMessage(Message message, int delayMilliseconds, bool regenerate = false)
+        public void PublishMessage(Message message, int delayMilliseconds)
         {
-            var messageId = regenerate ? Guid.NewGuid() : message.Id;
-            var deliveryTag = regenerate ? "1" : message.Header.Bag.ContainsKey(HeaderNames.DELIVERY_TAG) ? message.GetDeliveryTag().ToString() : null;
-
-            if (_logger != null && regenerate)
-                _logger.InfoFormat("RmqMessagePublisher: Regenerating message {0} with DeliveryTag of {1} to {2} with DeliveryTag of {3}", message.Id, deliveryTag ?? "(none)", messageId, 1);
+            var messageId = message.Id;
+            var deliveryTag = message.Header.Bag.ContainsKey(HeaderNames.DELIVERY_TAG) ? message.GetDeliveryTag().ToString() : null;
 
             var headers = new Dictionary<string, object>
                                       {
@@ -104,12 +101,51 @@ namespace paramore.brighter.commandprocessor.messaginggateway.rmq
             if (delayMilliseconds > 0)
                 headers.Add(HeaderNames.DELAY_MILLISECONDS, delayMilliseconds);
 
-            if (regenerate && !message.Header.Bag.Any(h => h.Key.Equals(HeaderNames.ORIGINAL_MESSAGE_ID, StringComparison.CurrentCultureIgnoreCase)))
+            _channel.BasicPublish(
+                _exchangeName,
+                message.Header.Topic,
+                false,
+                false,
+                CreateBasicProperties(messageId, message.Header.TimeStamp, headers),
+                Encoding.UTF8.GetBytes(message.Body.Value));
+        }
+
+        /// <summary>
+        /// Requeues the message.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="delayMilliseconds">Delay in ms.</param>
+        public void RequeueMessage(Message message, string queueName, int delayMilliseconds)
+        {
+            var messageId = Guid.NewGuid() ;
+            const string DeliveryTag = "1";
+
+            if (_logger != null)
+                _logger.InfoFormat("RmqMessagePublisher: Regenerating message {0} with DeliveryTag of {1} to {2} with DeliveryTag of {3}", message.Id, DeliveryTag, messageId, 1);
+
+            var headers = new Dictionary<string, object>
+                                      {
+                                          {HeaderNames.MESSAGE_TYPE, message.Header.MessageType.ToString()},
+                                          {HeaderNames.TOPIC, message.Header.Topic},
+                                          {HeaderNames.HANDLED_COUNT, message.Header.HandledCount.ToString(CultureInfo.InvariantCulture)},
+                                      };
+
+            message.Header.Bag.Each((header) =>
+            {
+                if (!HeadersToReset.Any(htr => htr.Equals(header.Key))) headers.Add(header.Key, header.Value);
+            });
+
+            headers.Add(HeaderNames.DELIVERY_TAG, DeliveryTag);
+
+            if (delayMilliseconds > 0)
+                headers.Add(HeaderNames.DELAY_MILLISECONDS, delayMilliseconds);
+
+            if (!message.Header.Bag.Any(h => h.Key.Equals(HeaderNames.ORIGINAL_MESSAGE_ID, StringComparison.CurrentCultureIgnoreCase)))
                 headers.Add(HeaderNames.ORIGINAL_MESSAGE_ID, message.Id.ToString());
 
             _channel.BasicPublish(
                 _exchangeName,
-                message.Header.Topic,
+                queueName,
                 false,
                 false,
                 CreateBasicProperties(messageId, message.Header.TimeStamp, headers),
