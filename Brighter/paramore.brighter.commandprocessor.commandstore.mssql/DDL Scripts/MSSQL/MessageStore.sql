1d07cf530e48a8ef8b58cadd6a07274b076d9409
iancooper <ian_hammond_cooper@yahoo.co.uk>
2015-07-03 12:18:00 +0100

    Add DDL for command and message stores

---
+++ Brighter/paramore.brighter.commandprocessor.commandstore.mssql/DDL Scripts/MSSQL/MessageStore.sql
@@ -0,0 +1,13 @@
+﻿-- User Table information:
+-- Number of tables: 1
+-- Commands: 0 row(s)
+
+CREATE TABLE [Commands] (
+  [CommandId] uniqueidentifier NOT NULL
+, [CommandType] nvarchar(256) NULL
+, [CommandBody] ntext NULL
+, [Timestamp] datetime NULL
+);
+GO
+ALTER TABLE [Commands] ADD CONSTRAINT [PK_MessageId] PRIMARY KEY ([CommandId]);
+GO
