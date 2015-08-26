e8a22cbb13b223b70c96b5850bc7352e1727e0e2
'ian' <'ian@huddle.com'>
2015-08-26 17:28:47 +0100

    Add  missing files.

---
+++ Brighter/paramore.brighter.commandprocessor/IAmAControlBusSenderFactory.cs
@@ -0,0 +1,59 @@
+// ***********************************************************************
+// Assembly         : paramore.brighter.commandprocessor
+// Author           : ian
+// Created          : 08-26-2015
+//
+// Last Modified By : ian
+// Last Modified On : 08-26-2015
+// ***********************************************************************
+// <copyright file="IAmAControlBusSenderFactory.cs" company="Ian Cooper">
+//     Copyright \u00A9  2014 Ian Cooper
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+#region Licence
+
+/* The MIT License (MIT)
+Copyright © 2015 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using paramore.brighter.commandprocessor.Logging;
+
+namespace paramore.brighter.commandprocessor
+{
+    /// <summary>
+    /// Interface IAmAControlBusSenderFactory. Helper for creating a control bus sender, which only requires
+    /// messaging configuration because it wraps the command processor and only supports the Post method, 
+    /// not Send and Publish and as such does not have handlers to register
+    /// </summary>
+    public interface IAmAControlBusSenderFactory {
+        /// <summary>
+        /// Creates the specified configuration.
+        /// </summary>
+        /// <param name="gateway">The gateway to the control bus</param>
+        /// <param name="logger">The logger to use</param>
+        /// <param name="messageStore">The message store to record outbound messages on the control bus</param>
+        /// <returns>IAmAControlBusSender.</returns>
+        IAmAControlBusSender Create(IAmAMessageStore<Message> messageStore, IAmAMessageProducer gateway, ILog logger);
+    }
+}
