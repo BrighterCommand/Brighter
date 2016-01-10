413e7ed645022297b5104034e98530bf0cc4ad1d
Fred Hoogduin <dev@hoogduin.net>
2016-01-10 21:58:07 +0100

    Implement PostAsync; Introducing IAmAnAsyncMessageStore and IAmAnAsyncMessageProducer; Introducing IMessageStore as a shared base for IAmAMessageStore and IAmAnAsyncMessageStore; Introducing IMessageProducer as a shared base for IAmAMessageProducer and IAmAnAsyncMessageProducer; re-factored CommandProcessor constructors to accept IMessageStore and IMessageProducers with guards that the passed implementations implement one or both of the sync and async interfaces;

--- Brighter/paramore.brighter.commandprocessor/IAmAnAsyncMessageStore.cs
+++ Brighter/paramore.brighter.commandprocessor/IAmAnAsyncMessageStore.cs
@@ -1,10 +1,11 @@
-// ***********************************************************************
+Ôªø// ***********************************************************************
 // Assembly         : paramore.brighter.commandprocessor
-// Author           : ian
-// Created          : 07-01-2014
+// Author           : Fred
+// Created          : 2016-01-10
+//                    Based on IAmAMessageStore.cs
 //
-// Last Modified By : ian
-// Last Modified On : 07-01-2014
+// Last Modified By : Fred
+// Last Modified On : 2016-01-10
 // ***********************************************************************
 //     Copyright (c) . All rights reserved.
 // </copyright>
@@ -13,10 +14,10 @@
 
 #region Licence
 /* The MIT License (MIT)
-Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+Copyright ¬© 2016 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the ìSoftwareî), to deal
+of this software and associated documentation files (the ‚ÄúSoftware‚Äù), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
@@ -25,7 +26,7 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED ìAS ISî, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED ‚ÄúAS IS‚Äù, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -36,33 +37,34 @@ THE SOFTWARE. */
 #endregion
 
 using System;
+using System.Threading.Tasks;
 
 namespace paramore.brighter.commandprocessor
 {
     /// <summary>
-    /// Interface IAmAMessageStore
+    /// Interface IAmAnAsyncMessageStore
     /// In order to provide reliability for messages sent over a <a href="http://parlab.eecs.berkeley.edu/wiki/_media/patterns/taskqueue.pdf">Task Queue</a> we
     /// store the message into a Message Store to allow later replay of those messages in the event of failure. We automatically copy any posted message into the store
-    /// We provide implementations of <see cref="IAmAMessageStore{T}"/> for Event Store and SQL. Clients using other message stores should consider a Pull
+    /// We provide implementations of <see cref="paramore.brighter.commandprocessor.IAmAnAsyncMessageStore{T}"/> for Event Store and SQL. Clients using other message stores should consider a Pull
     /// request
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public interface IAmAMessageStore<in T> : IMessageStore<T> where T : Message
+    public interface IAmAnAsyncMessageStore<in T> : IMessageStore<T> where T : Message
     {
         /// <summary>
-        /// Adds the specified message.
+        /// Awaitable add the specified message.
         /// </summary>
         /// <param name="message">The message.</param>
         /// <param name="messageStoreTimeout">The time allowed for the write in milliseconds; on a -1 default</param>
-        /// <returns>Task.</returns>
-        void Add(T message, int messageStoreTimeout = -1);
+        /// <returns><see cref="Task"/>.</returns>
+        Task AddAsync(T message, int messageStoreTimeout = -1);
 
         /// <summary>
-        /// Gets the specified message identifier.
+        /// Awaitable Get the specified message identifier.
         /// </summary>
         /// <param name="messageId">The message identifier.</param>
         /// <param name="messageStoreTimeout">The time allowed for the read in milliseconds; on  a -2 default</param>
-        /// <returns>Task&lt;Message&gt;.</returns>
-        Message Get(Guid messageId, int messageStoreTimeout = -1);
+        /// <returns><see cref="Task{Message}"/>.</returns>
+        Task<Message> GetAsync(Guid messageId, int messageStoreTimeout = -1);
     }
 }
