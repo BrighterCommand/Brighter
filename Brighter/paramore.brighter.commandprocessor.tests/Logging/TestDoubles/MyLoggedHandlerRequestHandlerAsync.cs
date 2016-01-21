8d99efe09c195fb0910d4d8efa825f18db318e5d
'ian' <'ian@huddle.com'>
2016-01-21 11:58:23 +0000

    Async Request Logging

---
+++ Brighter/paramore.brighter.commandprocessor.tests/Logging/TestDoubles/MyLoggedHandlerRequestHandlerAsync.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using paramore.brighter.commandprocessor;
+using paramore.brighter.commandprocessor.logging.Attributes;
+using paramore.brighter.commandprocessor.Logging;
+using paramore.commandprocessor.tests.CommandProcessors.TestDoubles;
+
+namespace paramore.commandprocessor.tests.Logging.TestDoubles
+{
+    class MyLoggedHandlerRequestHandlerAsync : RequestHandlerAsync<MyCommand>
+    {
+        public MyLoggedHandlerRequestHandlerAsync(ILog logger)
+            : base(logger)
+        {}
+
+        [RequestLoggingAsync(step:0, timing: HandlerTiming.Before)]
+        public override Task<MyCommand> HandleAsync(MyCommand command)
+        {
+            return base.HandleAsync(command);
+        }
+    }
+}
