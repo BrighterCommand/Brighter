d0e0939102b302fd5cf53033984f4c3ef8a40827
ian <ian@huddle.com>
2016-02-24 10:10:35 +0000

    Remove Rewind and Renegade; need to BFG at some point

--- Rewind/paramore.rewind.core/Domain/Meetings/IIssueTickets.cs
+++
@@ -1,9 +0,0 @@
-using Paramore.Rewind.Core.Domain.Venues;
-
-namespace Paramore.Rewind.Core.Domain.Meetings
-{
-    public interface IIssueTickets
-    {
-        Tickets Issue(Capacity capacity);
-    }
-}
