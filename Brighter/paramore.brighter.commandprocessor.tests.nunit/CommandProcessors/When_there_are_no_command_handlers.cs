c37ab4b40f32f6aaa3dc2b8ea24d0ceedbfe5665
ian.pender <ian.pender@huddle.com>
2016-10-12 11:38:45 +0100

    Test fixes for monitoring and dispatcher

--- Brighter/paramore.brighter.commandprocessor.tests.nunit/CommandProcessors/When_there_are_no_command_handlers.cs
+++ Brighter/paramore.brighter.commandprocessor.tests.nunit/CommandProcessors/When_there_are_no_command_handlers.cs
@@ -48,6 +48,6 @@ namespace paramore.brighter.commandprocessor.tests.nunit.CommandProcessors
         private Because _of = () => s_exception = Catch.Exception(() => s_commandProcessor.Send(s_myCommand));
 
         private It _should_fail_because_multiple_receivers_found = () => s_exception.ShouldBeAssignableTo(typeof(ArgumentException));
-        private It _should_have_an_error_message_that_tells_you_why = () => s_exception.ShouldContainErrorMessage("No command handler was found for the typeof command paramore.commandprocessor.tests.CommandProcessors.TestDoubles.MyCommand - a command should have exactly one handler.");
+        private It _should_have_an_error_message_that_tells_you_why = () => s_exception.ShouldContainErrorMessage("No command handler was found for the typeof command paramore.brighter.commandprocessor.tests.CommandProcessors.TestDoubles.MyCommand - a command should have exactly one handler.");
     }
 }
