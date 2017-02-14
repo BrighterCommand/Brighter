f2da7b26fde9b8463ab5e81e62ea34b06e06355b
Skullalive <thynquest@gmail.com>
2017-02-14 03:08:31 +0100

    character formating

--- paramore.brighter.commandprocessor/Interpreter.cs
+++ paramore.brighter.commandprocessor/Interpreter.cs
@@ -16,7 +16,7 @@
 Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the �Software�), to deal
+of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
