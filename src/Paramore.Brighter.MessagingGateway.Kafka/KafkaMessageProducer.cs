c59b550e2be4e8f040e5c9eb1a3274967c48a3e1
Wayne Hunsley <whunsley@gmail.com>
2017-08-17 19:05:35 +0100

    Updating license and project properties

--- src/Paramore.Brighter.MessagingGateway.Kafka/KafkaMessageProducer.cs
+++ src/Paramore.Brighter.MessagingGateway.Kafka/KafkaMessageProducer.cs
@@ -1,6 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2015 Wayne Hunsley <whunsley@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+#endregion
+
+using System;
 using System.Threading.Tasks;
 using Confluent.Kafka;
 using Paramore.Brighter.MessagingGateway.Kafka.Logging;
@@ -10,6 +31,7 @@ namespace Paramore.Brighter.MessagingGateway.Kafka
     class KafkaMessageProducer : IAmAMessageProducerSupportingDelay, IAmAMessageProducerAsync
     {
         private static readonly Lazy<ILog> _logger = new Lazy<ILog>(LogProvider.For<KafkaMessageProducer>);
+        //private readonly Producer<string, string> _producer = new Producer<string, string>(null, null, null);
 
         public bool DelaySupported => throw new NotImplementedException();
 
