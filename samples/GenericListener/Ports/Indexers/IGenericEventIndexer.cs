ecaba00eaa861ddd5df3e3eae682804a16939f9c
ian <ian@huddle.com>
2017-08-18 18:32:28 +0100

    Move non 'hello' projects to Contrib

--- samples/GenericListener/Ports/Indexers/IGenericEventIndexer.cs
+++
@@ -1,9 +0,0 @@
-using GenericListener.Ports.Events;
-
-namespace GenericListener.Ports.Indexers
-{
-    public interface IGenericFeedEventIndexer<in T> where T : EventStoredEvent
-    {
-        void Index(T @event);
-    }
-}
