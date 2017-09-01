83dc6ee8af8f09524ed26b191376ee01fd5c8c3f
toby.henderson <toby.henderson@huddle.com>
2017-09-01 16:09:30 +0100

    Added appveyor mysql support and docker settings

--- tests/Paramore.Brighter.Tests/MySqlTestHelper.cs
+++ tests/Paramore.Brighter.Tests/MySqlTestHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 using Paramore.Brighter.MessageStore.MySql;
 
@@ -6,17 +7,22 @@ namespace Paramore.Brighter.Tests
 {
     public class MySqlTestHelper
     {
-        public static string ConnectionString = "Server=localhost;Uid=root;Pwd=root;Database=BrighterTests";
-        
         private string _tableName;
+        private readonly MySqlSettings _mysqlSettings;
+
         public MySqlTestHelper()
         {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            var configuration = builder.Build();
+
+            _mysqlSettings = new MySqlSettings();
+            configuration.GetSection("MySql").Bind(_mysqlSettings);
             _tableName = $"test_{Guid.NewGuid()}";
         }
 
        public void CreateDatabase()
         {
-            using (var connection = new MySqlConnection(ConnectionString))
+            using (var connection = new MySqlConnection(_mysqlSettings.TestsMasterConnectionString))
             {
                 connection.Open();
                 using (var command = connection.CreateCommand())
@@ -39,11 +45,11 @@ namespace Paramore.Brighter.Tests
             CreateMessageStoreTable();
         }
         
-        public MySqlMessageStoreConfiguration MessageStoreConfiguration => new MySqlMessageStoreConfiguration(ConnectionString, _tableName);
+        public MySqlMessageStoreConfiguration MessageStoreConfiguration => new MySqlMessageStoreConfiguration(_mysqlSettings.TestsBrighterConnectionString, _tableName);
 
         public void CleanUpDb()
         {
-            using (var connection = new MySqlConnection(ConnectionString))
+            using (var connection = new MySqlConnection(_mysqlSettings.TestsBrighterConnectionString))
             {
                 connection.Open();
                 using (var command = connection.CreateCommand())
@@ -56,7 +62,7 @@ namespace Paramore.Brighter.Tests
 
         public void CreateMessageStoreTable()
         {
-            using (var connection = new MySqlConnection(ConnectionString))
+            using (var connection = new MySqlConnection(_mysqlSettings.TestsBrighterConnectionString))
             {
                 _tableName = $"`message_{_tableName}`";
                 var createTableSql = MySqlMessageStoreBuilder.GetDDL(_tableName);
@@ -69,5 +75,12 @@ namespace Paramore.Brighter.Tests
                 }
             }
         }
+    }
+
+    public class MySqlSettings
+    {
+        public string TestsBrighterConnectionString { get; set; } = "Server=localhost;Uid=root;Pwd=root;Database=BrighterTests";
+
+        public string TestsMasterConnectionString { get; set; } = "Server=localhost;Uid=root;Pwd=root;";
     }
 }
