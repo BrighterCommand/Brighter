9b233fece3a665a22f5fa8d878ea1391e45add55
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-02-20 11:33:36 +0000

    Pull in all the configuration options

--- src/Paramore.Brighter.MessagingGateway.Redis/RedisMessageGateway.cs
+++ src/Paramore.Brighter.MessagingGateway.Redis/RedisMessageGateway.cs
@@ -8,42 +8,25 @@ namespace Paramore.Brighter.MessagingGateway.Redis
         private readonly TimeSpan _messageTimeToLive;
         protected static Lazy<RedisManagerPool> Pool;
         protected string Topic;
+        private readonly RedisMessagingGatewayConfiguration _gatewayConfiguration;
 
         protected RedisMessageGateway(RedisMessagingGatewayConfiguration redisMessagingGatewayConfiguration)
         {
             _messageTimeToLive = redisMessagingGatewayConfiguration.MessageTimeToLive ?? TimeSpan.FromMinutes(10);
+            _gatewayConfiguration = redisMessagingGatewayConfiguration;
             
             Pool = new Lazy<RedisManagerPool>(() =>
             {
-                RedisConfig.DefaultConnectTimeout = 1 * 1000;
-                RedisConfig.DefaultSendTimeout = 1 * 1000;
-                RedisConfig.DefaultReceiveTimeout = 1 * 1000;
-                if (redisMessagingGatewayConfiguration.DefaultRetryTimeout.HasValue)
-                {
-                    RedisConfig.DefaultRetryTimeout = redisMessagingGatewayConfiguration.DefaultRetryTimeout.Value;
-                }
-                RedisConfig.DefaultIdleTimeOutSecs = 240;
-                if (redisMessagingGatewayConfiguration.BackoffMultiplier.HasValue)
-                {
-                    RedisConfig.BackOffMultiplier = redisMessagingGatewayConfiguration.BackoffMultiplier.Value;
-                }
-                RedisConfig.BufferLength = 1450;
-                if (redisMessagingGatewayConfiguration.MaxPoolSize.HasValue)
-                {
-                    RedisConfig.DefaultMaxPoolSize = redisMessagingGatewayConfiguration.MaxPoolSize;
-                }
-                RedisConfig.VerifyMasterConnections = true;
-                RedisConfig.HostLookupTimeoutMs = 1000;
-                RedisConfig.DeactivatedClientsExpiry = TimeSpan.FromSeconds(15);
-                RedisConfig.DisableVerboseLogging = false;
-                    
-                 return new RedisManagerPool(
-                    redisMessagingGatewayConfiguration.RedisConnectionString,
+                OverrideRedisClientDefaults();
+
+                return new RedisManagerPool(
+                    _gatewayConfiguration.RedisConnectionString,
                     new RedisPoolConfig()
                 );
             });
  
         }
+
 
         protected void DisposePool()
         {
@@ -65,6 +48,67 @@ namespace Paramore.Brighter.MessagingGateway.Redis
                 redisMessage = redisMessageFactory.Create(message);
             }
             return redisMessage;
+        }
+        
+        /// <summary>
+        /// Service Stack Redis provides global (static) configuration settings for how Redis behaves.
+        /// We want to be able to override the defaults (or leave them if we think they are appropriate
+        /// We run this as part of lazy initialization, as these are static and so we want to enforce the idea
+        /// that you are globally setting them for any worker that runs in the same process
+        /// A client could also set RedisConfig values directly in their own code, if preferred.
+        /// Our preference is to hide the global nature of that config inside this Lazy<T>
+        /// </summary>
+        private void OverrideRedisClientDefaults()
+        {
+            RedisMessagingGatewayConfiguration redisMessagingGatewayConfiguration;
+            if (_gatewayConfiguration.DefaultConnectTimeout.HasValue)
+            {
+                RedisConfig.DefaultConnectTimeout = _gatewayConfiguration.DefaultConnectTimeout.Value;
+            }
+
+            if (_gatewayConfiguration.DefaultSendTimeout.HasValue)
+            {
+                RedisConfig.DefaultSendTimeout = _gatewayConfiguration.DefaultSendTimeout.Value;
+            }
+
+            if (_gatewayConfiguration.DefaultReceiveTimeout.HasValue)
+            {
+                RedisConfig.DefaultReceiveTimeout = _gatewayConfiguration.DefaultReceiveTimeout.Value;
+            }
+
+            if (_gatewayConfiguration.DefaultRetryTimeout.HasValue)
+            {
+                RedisConfig.DefaultRetryTimeout = _gatewayConfiguration.DefaultRetryTimeout.Value;
+            }
+
+            if (_gatewayConfiguration.DefaultIdleTimeOutSecs.HasValue)
+            {
+                RedisConfig.DefaultIdleTimeOutSecs = _gatewayConfiguration.DefaultIdleTimeOutSecs.Value;
+            }
+
+            if (_gatewayConfiguration.BackoffMultiplier.HasValue)
+            {
+                RedisConfig.BackOffMultiplier = _gatewayConfiguration.BackoffMultiplier.Value;
+            }
+
+            if (_gatewayConfiguration.BufferLength.HasValue)
+            {
+                RedisConfig.BufferLength = _gatewayConfiguration.BufferLength.Value;
+            }
+
+            if (_gatewayConfiguration.MaxPoolSize.HasValue)
+            {
+                RedisConfig.DefaultMaxPoolSize = _gatewayConfiguration.MaxPoolSize;
+            }
+
+            if (_gatewayConfiguration.VerifyMasterConnections.HasValue)
+            {
+                RedisConfig.VerifyMasterConnections = true;
+            }
+
+            RedisConfig.HostLookupTimeoutMs = 1000;
+            RedisConfig.DeactivatedClientsExpiry = TimeSpan.FromSeconds(15);
+            RedisConfig.DisableVerboseLogging = false;
         }
         
         /// <summary>
--- src/Paramore.Brighter.MessagingGateway.Redis/RedisMessagingGatewayConfiguration.cs
+++ src/Paramore.Brighter.MessagingGateway.Redis/RedisMessagingGatewayConfiguration.cs
@@ -29,41 +29,41 @@ namespace Paramore.Brighter.MessagingGateway.Redis
         /// </summary>
         public int? DefaultRetryTimeout { get; set; }
 
-      /// <summary>
+        /// <summary>
         /// The Byte Buffer Size to combine Redis Operations within (default 1450 bytes)
         /// </summary>
-        public static int BufferLength = 1450;
+        public int? BufferLength { get; set; }
 
         /// <summary>
         /// The Byte Buffer Size for Operations to use a byte buffer pool (default 500kb)
         /// </summary>
-        public static int BufferPoolMaxSize = 500000;
+        public int? BufferPoolMaxSize { get; set; }
 
         /// <summary>
         /// Whether Connections to Master hosts should be verified they're still master instances (default true)
         /// </summary>
-        public static bool VerifyMasterConnections = true;
+        public bool? VerifyMasterConnections { get; set; }
 
         /// <summary>
         /// The ConnectTimeout on clients used to find the next available host (default 200ms)
         /// </summary>
-        public static int HostLookupTimeoutMs = 200;
+        public int? HostLookupTimeoutMs { get; set; }
 
         /// <summary>
         /// Skip ServerVersion Checks by specifying Min Version number, e.g: 2.8.12 => 2812, 2.9.1 => 2910
         /// </summary>
-        public static int? AssumeServerVersion;
+        public static int? AssumeServerVersion { get; set; }
 
         /// <summary>
         /// How long to hold deactivated clients for before disposing their connection (default 1 min)
         /// Dispose of deactivated Clients immediately with TimeSpan.Zero
         /// </summary>
-        public static TimeSpan DeactivatedClientsExpiry = TimeSpan.FromMinutes(1);
+        public TimeSpan? DeactivatedClientsExpiry { get; set; }
 
         /// <summary>
         /// Whether Debug Logging should log detailed Redis operations (default false)
         /// </summary>
-        public static bool DisableVerboseLogging = false;
+        public bool? DisableVerboseLogging { get; set; }
         
         /// <summary>
         /// Exponential backoff interval for retrying connections on socker failure, default 10ms
