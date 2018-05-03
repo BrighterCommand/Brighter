bff5dff1025536f36b3016841b24946410e72a18
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-05-03 18:57:40 +0100

    Add support for testing older versions of RMQ that don't support policy based queue mirroring

---
+++ samples/RMQPartitionTesting/expose_ports_to_toolbox_28.ps1
@@ -0,0 +1,3 @@
+VBoxManage controlvm "default" natpf1 "tcp-port55672,tcp,127.0.0.1,55672,,55672";
+VBoxManage controlvm "default" natpf1 "tcp-port55673,tcp,127.0.0.1,55673,,55673";
+VBoxManage controlvm "default" natpf1 "tcp-port55674,tcp,127.0.0.1,55674,,55674";
