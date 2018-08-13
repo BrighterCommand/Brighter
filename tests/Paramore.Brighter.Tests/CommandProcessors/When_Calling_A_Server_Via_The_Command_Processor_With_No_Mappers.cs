763790f42e1de5abda74afc59570193c80ccb8b2
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-08-13 18:25:43 +0100

    Adding tests for paths that were not covered (exceptional) in the Call method of CommandProcessor

---
+++ tests/Paramore.Brighter.Tests/CommandProcessors/When_Calling_A_Server_Via_The_Command_Processor_With_No_Mappers.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using FluentAssertions;
+using Paramore.Brighter.ServiceActivator.TestHelpers;
+using Paramore.Brighter.Tests.CommandProcessors.TestDoubles;
+using Polly;
+using Xunit;
+
+namespace Paramore.Brighter.Tests.CommandProcessors
+{
+    public class CommandProcessorNoMappersTests 
+    {
+               private readonly CommandProcessor _commandProcessor;
+        private readonly MyRequest _myRequest = new MyRequest();
+
+        public CommandProcessorNoMappersTests()
+        {
+            _myRequest.RequestValue = "Hello World";
+
+            var messageMapperRegistry = new MessageMapperRegistry(new SimpleMessageMapperFactory((type) =>
+            {
+                throw new ConfigurationException($"No mapper found for {type.Name}");
+            }));
+            
+            messageMapperRegistry.Register<MyRequest, MyRequestMessageMapper>();
+            messageMapperRegistry.Register<MyResponse, MyResponseMessageMapper>();
+
+            var subscriberRegistry = new SubscriberRegistry();
+            subscriberRegistry.Register<MyResponse, MyResponseHandler>();
+            var handlerFactory = new TestHandlerFactory<MyResponse, MyResponseHandler>(() => new MyResponseHandler());
+
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .Retry();
+
+            var circuitBreakerPolicy = Policy
+                .Handle<Exception>()
+                .CircuitBreaker(1, TimeSpan.FromMilliseconds(1));
+
+            _commandProcessor = new CommandProcessor(
+                subscriberRegistry,
+                handlerFactory,
+                new InMemoryRequestContextFactory(),
+                new PolicyRegistry
+                {
+                    {CommandProcessor.RETRYPOLICY, retryPolicy},
+                    {CommandProcessor.CIRCUITBREAKER, circuitBreakerPolicy}
+                },
+                messageMapperRegistry,
+                (IAmAMessageStore<Message>)null,
+                (IAmAMessageProducer)new FakeMessageProducer(),
+                responseChannelFactory: new InMemoryChannelFactory());
+
+        }
+ 
+        [Fact]
+        public void When_Calling_A_Server_Via_The_Command_Processor_With_No_Mappers()
+        {
+            var exception = Catch.Exception(() => _commandProcessor.Call<MyRequest, MyResponse>(_myRequest, 0));
+            
+            //should throw an exception as we require mappers 
+            exception.Should().BeOfType<ArgumentOutOfRangeException>();
+        }
+
+
+        public void Dispose()
+        {
+            _commandProcessor.Dispose();
+        }
+     }
+}
