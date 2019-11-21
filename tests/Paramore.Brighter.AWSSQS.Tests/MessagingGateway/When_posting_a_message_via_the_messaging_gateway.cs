968cf3e2f9511916d536711be1483df7cbeb30ef
toby.henderson <toby.henderson@huddle.com>
2019-11-21 14:43:53 +0000

    Fix json compare

--- tests/Paramore.Brighter.AWSSQS.Tests/MessagingGateway/When_posting_a_message_via_the_messaging_gateway.cs
+++ tests/Paramore.Brighter.AWSSQS.Tests/MessagingGateway/When_posting_a_message_via_the_messaging_gateway.cs
@@ -63,6 +63,8 @@ namespace Paramore.Brighter.AWSSQS.Tests.MessagingGateway
             _channel.Acknowledge(message);
 
             //should_send_the_message_to_aws_sqs
+            message.Header.MessageType.Should().Be(MessageType.MT_COMMAND);
+
             message.Id.Should().Be(_myCommand.Id);
             message.Redelivered.Should().BeFalse();
             message.Header.Id.Should().Be(_myCommand.Id);
@@ -70,13 +72,12 @@ namespace Paramore.Brighter.AWSSQS.Tests.MessagingGateway
             message.Header.CorrelationId.Should().Be(_correlationId);
             message.Header.ReplyTo.Should().Be(_replyTo);
             message.Header.ContentType.Should().Be(_contentType);
-            message.Header.MessageType.Should().Be(MessageType.MT_COMMAND);
             message.Header.HandledCount.Should().Be(0);
             //allow for clock drift in the following test, more important to have a contemporary timestamp than anything
             message.Header.TimeStamp.Should().BeAfter(RoundToSeconds(DateTime.UtcNow.AddMinutes(-1)));
             message.Header.DelayedMilliseconds.Should().Be(0);
             //{"Id":"cd581ced-c066-4322-aeaf-d40944de8edd","Value":"Test","WasCancelled":false,"TaskCompleted":false}
-            message.Body.Value.Should().Be("{\"Id\":\"" + _myCommand.Id.ToString() + "\",\"Value\":\"" + _myCommand.Value + "\",\"WasCancelled\":false,\"TaskCompleted\":false}");
+            message.Body.Value.Should().Be(_message.Body.Value);
         }
 
         public void Dispose()
