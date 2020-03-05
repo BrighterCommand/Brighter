ce9b1d435bd0aff039e9228f490db0b2e9f6e646
toby.henderson <toby.henderson@huddle.com>
2020-03-05 12:52:11 +0000

    Cancel consumer correctly

--- src/Paramore.Brighter.MessagingGateway.RMQ/RmqMessageConsumer.cs
+++ src/Paramore.Brighter.MessagingGateway.RMQ/RmqMessageConsumer.cs
@@ -1,4 +1,4 @@
-#region Licence
+﻿#region Licence
 /* The MIT License (MIT)
 Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
 
@@ -52,8 +52,9 @@ namespace Paramore.Brighter.MessagingGateway.RMQ
         private readonly int _batchSize;
         private readonly RmqMessageCreator _messageCreator;
         private readonly Message _noopMessage = new Message();
+        private readonly string _consumerTag;
 
-      /// <summary>
+        /// <summary>
       /// Initializes a new instance of the <see cref="RMQMessageGateway" /> class.
       /// </summary>
       /// <param name="connection"></param>
@@ -95,6 +96,7 @@ namespace Paramore.Brighter.MessagingGateway.RMQ
             IsQueueMirroredAcrossAllNodesInTheCluster = highAvailability;
             _messageCreator = new RmqMessageCreator();
             _batchSize = batchSize;
+            _consumerTag = Connection.Name + Guid.NewGuid();
         }
 
         /// <summary>
@@ -323,9 +325,9 @@ namespace Paramore.Brighter.MessagingGateway.RMQ
             {
                 if (_consumer.IsRunning)
                 {
-                    _consumer.OnCancel();
+                    Channel.BasicCancel(_consumerTag);
                 }
-
+                
                 _consumer = null;
             }
         }
@@ -334,21 +336,11 @@ namespace Paramore.Brighter.MessagingGateway.RMQ
         {
             _consumer = new PullConsumer(Channel);
             
-            _consumer.ConsumerCancelled += (sender, args) =>
-            {
-                _logger.Value.WarnFormat(
-                    "RmqMessageConsumer: Consumer cancelled for queue {0} with routing key {1} via exchange {2} on connection {3}",
-                    _queueName,
-                    _routingKeys,
-                    Connection.Exchange.Name,
-                    Connection.AmpqUri.GetSanitizedUri());
-            };
-            
             Channel.BasicQos(0, (ushort)_batchSize, false);
 
-            Channel.BasicConsume(_queueName, false, Connection.Name, SetQueueArguments(), _consumer);
+            Channel.BasicConsume(_queueName, false, _consumerTag, SetQueueArguments(), _consumer);
             
-            _consumer.HandleBasicConsumeOk(Connection.Name);
+            _consumer.HandleBasicConsumeOk(_consumerTag);
             
             _logger.Value.InfoFormat("RmqMessageConsumer: Created consumer for queue {0} with routing key {1} via exchange {2} on connection {3}",
                 _queueName,
