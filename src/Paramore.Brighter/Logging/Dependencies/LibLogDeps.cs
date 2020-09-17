0e02e8e9235c7fe92e77ef1cc106e6e4993b9c5a
Ian Cooper <ian.cooper@just-eat.com>
2020-09-17 15:54:01 +0100

    Try to force LibLog to build successfully on GitHub

---
+++ src/Paramore.Brighter/Logging/Dependencies/LibLogDeps.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Paramore.Brighter.Logging.Dependencies
+{
+     //HACK! This is a dummy method, it is intended to allow us to force some deps that LibLog needs, so that we can build
+     //TODO: Remove LibLog, rely on .NET defined logging interface
+     [SuppressMessage("Microsoft.Performance", "CA1812: Avoid uninstantiated internal classes")]
+    internal class LibLogDeps
+    {
+    }
+}
