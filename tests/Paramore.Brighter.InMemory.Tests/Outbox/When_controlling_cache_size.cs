b24669178764f281a8a5aa3dae892a3ab93214b1
Ian Cooper <ian.cooper@just-eat.com>
2021-02-13 16:53:20 +0000

    Test that the order of eviction from the cache is correct

--- tests/Paramore.Brighter.InMemory.Tests/Outbox/When_controlling_cache_size.cs
+++ tests/Paramore.Brighter.InMemory.Tests/Outbox/When_controlling_cache_size.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Paramore.Brighter.InMemory.Tests.Builders;
 using Xunit;
@@ -32,6 +33,40 @@ namespace Paramore.Brighter.InMemory.Tests.Outbox
             //should clear compaction percentage from the outbox, and then add  the  new one
             outbox.MessageCount.Should().Be(3);
         }
-        
+
+        [Fact]
+        public void When_shrinking_evict_oldest_messages_first()
+        {
+            //Arrange
+            const int limit = 5;
+            
+            var outbox = new InMemoryOutbox()
+            {
+                MessageLimit = limit,
+                CompactionPercentage = 0.5
+            };
+
+            var messageIds = new Guid[] {Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()};
+
+            for (int i = 0; i <= limit - 1; i++)
+            {
+                outbox.Add(new MessageBuilder().WithId(messageIds[i]));
+                Task.Delay(1000);
+            }
+
+            //Act
+            outbox.MessageCount.Should().Be(5);
+            
+            outbox.Add(new MessageBuilder());
+
+            Task.Delay(500).Wait(); //Allow time for compaction to run
+            
+            //should clear compaction percentage from the outbox, and then add  the  new one
+            outbox.Get(messageIds[0]).Should().BeNull();
+            outbox.Get(messageIds[1]).Should().BeNull();
+            outbox.Get(messageIds[2]).Should().BeNull();
+            outbox.Get(messageIds[3]).Should().NotBeNull();
+            outbox.Get(messageIds[4]).Should().NotBeNull();
+        }
     }
 }
