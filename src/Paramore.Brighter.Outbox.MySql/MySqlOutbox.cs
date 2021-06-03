eeda16377593c75992cb1ea3dfb376e39216cdab
Hon Kuan Yong <honkuan.yong@ideagenplc.com>
2021-06-03 14:47:12 +0800

    Fix OutStandingMessages  query

--- src/Paramore.Brighter.Outbox.MySql/MySqlOutbox.cs
+++ src/Paramore.Brighter.Outbox.MySql/MySqlOutbox.cs
@@ -389,7 +389,7 @@ namespace Paramore.Brighter.Outbox.MySql
          
         private void CreatePagedOutstandingCommand(DbCommand command, double milliSecondsSinceAdded, int pageSize, int pageNumber)
         {
-            var pagingSqlFormat = "SELECT * FROM {0} AS TBL WHERE `CreatedID` BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) AND DISPATCHED IS NULL AND TIMESTAMP < DATE_ADD(CURRENT_DATE(), INTERVAL @OutStandingSince SECOND) ORDER BY Timestamp DESC";
+            var pagingSqlFormat = "SELECT * FROM {0} AS TBL WHERE `CreatedID` BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) AND DISPATCHED IS NULL AND TIMESTAMP < DATE_ADD(CURRENT_TIMESTAMP(), INTERVAL @OutStandingSince SECOND) ORDER BY Timestamp DESC";
             var seconds = TimeSpan.FromMilliseconds(milliSecondsSinceAdded).Seconds > 0 ? TimeSpan.FromMilliseconds(milliSecondsSinceAdded).Seconds : 1;
             var parameters = new[]
             {
