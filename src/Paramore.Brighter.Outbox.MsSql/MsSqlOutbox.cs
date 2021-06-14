23486ee2d83c62877ffc1edc6a551dd49e55be92
Paul Reardon <Paul.Reardon@Flagstoneim.com>
2021-06-14 18:24:08 +0100

    Updated Outstanding Command SQl Query

--- src/Paramore.Brighter.Outbox.MsSql/MsSqlOutbox.cs
+++ src/Paramore.Brighter.Outbox.MsSql/MsSqlOutbox.cs
@@ -386,7 +386,7 @@ namespace Paramore.Brighter.Outbox.MsSql
         
         private void CreatePagedOutstandingCommand(SqlCommand command, double milliSecondsSinceAdded, int pageSize, int pageNumber)
         {
-            var pagingSqlFormat = "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY Timestamp DESC) AS NUMBER, * FROM {0}) AS TBL WHERE DISPATCHED IS NULL AND TIMESTAMP < DATEADD(millisecond, @OutStandingSince, getdate()) AND NUMBER BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) ORDER BY Timestamp DESC";
+            var pagingSqlFormat = "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY Timestamp ASC) AS NUMBER, * FROM {0} WHERE DISPATCHED IS NULL) AS TBL WHERE TIMESTAMP < DATEADD(millisecond, -@OutStandingSince, getdate()) AND NUMBER BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) ORDER BY Timestamp ASC";
             var parameters = new[]
             {
                 CreateSqlParameter("PageNumber", pageNumber),
