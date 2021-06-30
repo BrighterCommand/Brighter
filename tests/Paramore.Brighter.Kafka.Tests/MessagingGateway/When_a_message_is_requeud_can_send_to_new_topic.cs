d2d7a8e2c35824619fc61752644c130b927bb9c4
Ian Cooper <ian.cooper@just-eat.com>
2021-06-30 14:43:26 +0100

    interim checkin befor merge of master

--- tests/Paramore.Brighter.Kafka.Tests/MessagingGateway/When_a_message_is_requeud_can_send_to_new_topic.cs
+++ tests/Paramore.Brighter.Kafka.Tests/MessagingGateway/When_a_message_is_requeud_can_send_to_new_topic.cs
@@ -11,35 +11,14 @@ namespace Paramore.Brighter.Kafka.Tests.MessagingGateway
         private readonly string _requeueName = Guid.NewGuid().ToString();
         private readonly string _topic = Guid.NewGuid().ToString();
         private readonly string _requeueTopic = Guid.NewGuid().ToString();
-        private readonly IAmAMessageProducer _producer;
         private readonly IAmAMessageConsumer _consumer;
-        private readonly string _partitionKey = Guid.NewGuid().ToString();
-        private string _deferQueueName;
         private IAmAMessageConsumer _deferConsumer;
 
         public KafkaRequeueGoesToNewTopicIfDefined()
         {
             const string groupId = "Kafka Message Producer Send Test";
-            _deferQueueName = $"{_topic}_Defer_{5}";
+            string deferQueueName = $"{_topic}_Defer_{5}";
             
-            _producer = new KafkaMessageProducerFactory(
-                new KafkaMessagingGatewayConfiguration
-                {
-                    Name = "Kafka Producer Send Test",
-                    BootStrapServers = new[] {"localhost:9092"}
-                },
-                new KafkaPublication()
-                {
-                    Topic = new RoutingKey(_topic),
-                    NumPartitions = 1,
-                    ReplicationFactor = 1,
-                    //These timeouts support running on a container using the same host as the tests, 
-                    //your production values ought to be lower
-                    MessageTimeoutMs = 2000,
-                    RequestTimeoutMs = 2000,
-                    MakeChannels = OnMissingChannel.Create
-                }).Create();
-
             var kafkaConfiguration = new KafkaMessagingGatewayConfiguration {Name = "Kafka Consumer Test", BootStrapServers = new[] {"localhost:9092"}}; 
             _consumer = new KafkaMessageConsumerFactory(kafkaConfiguration)
                 .Create(new KafkaSubscription<MyCommand>(
@@ -49,7 +28,7 @@ namespace Paramore.Brighter.Kafka.Tests.MessagingGateway
                         numOfPartitions: 1,
                         replicationFactor: 1,
                         requeueDelayInMs:5,
-                        deferRoutingKey: new RoutingKey(_deferQueueName),
+                        deferRoutingKey: new RoutingKey(deferQueueName),
                         makeChannels: OnMissingChannel.Create
                     )
                 );
@@ -72,6 +51,16 @@ namespace Paramore.Brighter.Kafka.Tests.MessagingGateway
         [Fact]
         public void When_a_message_is_requeued_send_to_new_topic()
         {
+            var message = new Message(
+                new MessageHeader(Guid.NewGuid(), _topic, MessageType.MT_COMMAND) {PartitionKey = Guid.NewGuid().ToString()},
+                new MessageBody($"test content [{_queueName}]")
+                );
+            
+            //reject from the first consumer
+            _consumer.Reject(message);
+            
+            //should now be on the second consumer
+            var rejectedMessage = _consumer.Receive(5000);
         }
     }
 }
