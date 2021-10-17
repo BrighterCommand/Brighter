4436f96b82b791cd13333ce8dc53a28d2ef4bbd7
Paul Reardon <paul.reardon@flagstoneim.com>
2021-10-17 11:18:01 +0100

    Updated Timed outbox sweeper Options to be configuratble via action.

--- src/Paramore.Brighter.Extensions.Hosting/HostedServiceCollectionExtensions.cs
+++ src/Paramore.Brighter.Extensions.Hosting/HostedServiceCollectionExtensions.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 using Paramore.Brighter.Extensions.DependencyInjection;
 
 namespace Paramore.Brighter.Extensions.Hosting
 {
     public static class HostedServiceCollectionExtensions
     {
+        /// <summary>
+        /// Use a timer based outbox sweeper as a Hosted Service.
+        /// </summary>
+        /// <param name="brighterBuilder">The Brighter Builder</param>
+        /// <param name="timedOutboxSweeperOptionsAction">Configuration actions for the Timed outbox Sweeper <see cref="TimedOutboxSweeper"/></param>
+        /// <returns>The Brighter Builder</returns>
         public static IBrighterBuilder UseOutboxSweeper(this IBrighterBuilder brighterBuilder,
-            TimedOutboxSweeperOptions options = default)
+            Action<TimedOutboxSweeperOptions> timedOutboxSweeperOptionsAction = null)
         {
+            var options = new TimedOutboxSweeperOptions();
+            timedOutboxSweeperOptionsAction?.Invoke(options);
+            
             brighterBuilder.Services.AddSingleton<TimedOutboxSweeperOptions>(options);
             brighterBuilder.Services.AddHostedService<TimedOutboxSweeper>();
             return brighterBuilder;
