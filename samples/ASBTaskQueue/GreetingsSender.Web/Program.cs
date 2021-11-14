216e2aa1351100367ec1ae0786fa193648f07409
Paul Reardon <Paul.Reardon@Flagstoneim.com>
2021-11-14 13:54:47 +0000

    Updated EF to target 6.0 and 5.0
    EF Core LIbrary set to Multi Target # 1819
    
    Updated Some Examples to dotnet 6

--- samples/ASBTaskQueue/GreetingsSender.Web/Program.cs
+++ samples/ASBTaskQueue/GreetingsSender.Web/Program.cs
@@ -1,26 +1,84 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using GreetingsSender.Web.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Paramore.Brighter.MessagingGateway.AzureServiceBus;
+using Paramore.Brighter.MsSql;
+using Paramore.Brighter.MsSql.EntityFrameworkCore;
+using Paramore.Brighter.Outbox.MsSql;
+using Paramore.Brighter.Extensions.DependencyInjection;
+using Paramore.Brighter;
+using Greetings.Ports.Events;
+using Greetings.Ports.Mappers;
 
-namespace GreetingsSender.Web
+var builder = WebApplication.CreateBuilder(args);
+
+string dbConnString = "server=(localdb)\\mssqllocaldb;database=BrighterTests;trusted_connection=yes";
+
+//EF
+builder.Services.AddDbContext<GreetingsDataContext>(o =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    o.UseSqlServer(dbConnString);
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+//Brighter
+string asbConnectionString = "Endpoint=sb://.servicebus.windows.net/;Authentication=Managed Identity";
+
+var asbConnection = new AzureServiceBusConfiguration(asbConnectionString, true);
+var producer = AzureServiceBusMessageProducerFactory.Get(asbConnection);
+
+var outboxConfig = new MsSqlConfiguration(dbConnString, "BrighterOutbox");
+
+builder.Services
+    .AddBrighter(opt =>
+    {
+        opt.PolicyRegistry = new DefaultPolicy();
+        opt.CommandProcessorLifetime = ServiceLifetime.Scoped;
+    })
+    .UseExternalBus(producer)
+    .UseMsSqlOutbox(outboxConfig, typeof(MsSqlSqlAuthConnectionProvider))
+    .UseMsSqlTransactionConnectionProvider(typeof(MsSqlEntityFrameworkCoreConnectionProvider<GreetingsDataContext>))
+    .MapperRegistry(r =>
+    {
+        r.Add(typeof(GreetingEvent), typeof(GreetingEventMessageMapper));
+        r.Add(typeof(GreetingAsyncEvent), typeof(GreetingEventAsyncMessageMapper));
+    });
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    using var serviceScope = app.Services.CreateScope();
+    var services = serviceScope.ServiceProvider;
+    var dbContext = services.GetService<GreetingsDataContext>();
+
+    dbContext.Database.EnsureCreated();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
