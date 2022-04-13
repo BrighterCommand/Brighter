643449bdd893b96587bf5a8847534c49b1b3c010
Omelian Levkovych <omelianai@gmail.com>
2022-04-13 21:02:59 +0300

    Added the SalutationAnalytics console project for consuming events

--- samples/WebAPI_Marten/SalutationPorts/EntityGateway/Interfaces/ISalutationsEntityGateway.cs
+++ samples/WebAPI_Marten/SalutationPorts/EntityGateway/Interfaces/ISalutationsEntityGateway.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SalutationPorts.EntityGateway.Interfaces
+﻿namespace SalutationPorts.EntityGateway.Interfaces
 {
     public interface ISalutationsEntityGateway
     {
