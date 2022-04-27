6c08eb8ba2c6be386f3f57f72d438204e542bf1a
Ian Cooper <ian.cooper@just-eat.com>
2022-04-27 18:21:18 +0100

    Missing files

---
+++ src/Paramore.Brighter.Dapper/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Paramore.Brighter.Dapper
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        DbTransaction BeginOrGetTransaction();
+        Task<DbTransaction> BeginOrGetTransactionAsync(CancellationToken cancellationToken);
+        void Commit();
+        DbConnection Database { get; }
+        bool HasTransaction();
+    }
+}
