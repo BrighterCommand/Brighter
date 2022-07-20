05ccea710be3c64d19f71bc70f0b4ced1ee0620f
Catalin Toma <ctoma2005@gmail.com>
2022-07-20 19:11:27 +0300

    #2194 [CT] Using UTC in MSSQL / MYSQL outbox, as this is the timezone… (#2196)
    * #2194 [CT] Using UTC in MSSQL / MYSQL outbox, as this is the timezone used when storing messages
    
    * #2194 [CT] Using built-in MySQL functions, imposing outbox dates to be stored in UTC
    
    Co-authored-by: Catalin Toma <Catalin.Toma@flagstoneim.com>

--- src/Paramore.Brighter.Outbox.MsSql/MsSqlOutbox.cs
+++ src/Paramore.Brighter.Outbox.MsSql/MsSqlOutbox.cs
@@ -373,7 +373,7 @@ namespace Paramore.Brighter.Outbox.MsSql
             var connection = await _connectionProvider.GetConnectionAsync(cancellationToken);
            
             if(connection.State!= ConnectionState.Open) await connection.OpenAsync(cancellationToken).ConfigureAwait(ContinueOnCapturedContext);
-            using (var command = InitMarkDispatchedCommand(connection, id, dispatchedAt))
+            using (var command = InitMarkDispatchedCommand(connection, id, dispatchedAt ?? DateTime.UtcNow))
             {
                 if (_connectionProvider.HasOpenTransaction) command.Transaction = _connectionProvider.GetTransaction();
                 await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(ContinueOnCapturedContext);
@@ -395,7 +395,7 @@ namespace Paramore.Brighter.Outbox.MsSql
 
             if (connection.State != ConnectionState.Open)
                 await connection.OpenAsync(cancellationToken).ConfigureAwait(ContinueOnCapturedContext);
-            using (var command = InitMarkDispatchedCommand(connection, ids, dispatchedAt))
+            using (var command = InitMarkDispatchedCommand(connection, ids, dispatchedAt ?? DateTime.UtcNow))
             {
                 await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(ContinueOnCapturedContext);
             }
@@ -414,7 +414,7 @@ namespace Paramore.Brighter.Outbox.MsSql
         {
             var connection = _connectionProvider.GetConnection();
             if(connection.State!= ConnectionState.Open) connection.Open();
-            using (var command = InitMarkDispatchedCommand(connection, id, dispatchedAt))
+            using (var command = InitMarkDispatchedCommand(connection, id, dispatchedAt ?? DateTime.UtcNow))
             {
                 if (_connectionProvider.HasOpenTransaction) command.Transaction = _connectionProvider.GetTransaction();
                 command.ExecuteNonQuery();
@@ -499,7 +499,7 @@ namespace Paramore.Brighter.Outbox.MsSql
         
         private void CreatePagedDispatchedCommand(SqlCommand command, double millisecondsDispatchedSince, int pageSize, int pageNumber)
         {
-            var pagingSqlFormat = "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY Timestamp DESC) AS NUMBER, * FROM {0}) AS TBL WHERE DISPATCHED IS NOT NULL AND DISPATCHED < DATEADD(millisecond, @OutStandingSince, getdate()) AND NUMBER BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) ORDER BY Timestamp DESC";
+            var pagingSqlFormat = "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY Timestamp DESC) AS NUMBER, * FROM {0}) AS TBL WHERE DISPATCHED IS NOT NULL AND DISPATCHED < DATEADD(millisecond, @OutStandingSince, getutcdate()) AND NUMBER BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) ORDER BY Timestamp DESC";
             var parameters = new[]
             {
                 CreateSqlParameter("PageNumber", pageNumber),
@@ -530,7 +530,7 @@ namespace Paramore.Brighter.Outbox.MsSql
         
         private void CreatePagedOutstandingCommand(SqlCommand command, double milliSecondsSinceAdded, int pageSize, int pageNumber)
         {
-            var pagingSqlFormat = "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY Timestamp ASC) AS NUMBER, * FROM {0} WHERE DISPATCHED IS NULL) AS TBL WHERE TIMESTAMP < DATEADD(millisecond, -@OutStandingSince, getdate()) AND NUMBER BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) ORDER BY Timestamp ASC";
+            var pagingSqlFormat = "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY Timestamp ASC) AS NUMBER, * FROM {0} WHERE DISPATCHED IS NULL) AS TBL WHERE TIMESTAMP < DATEADD(millisecond, -@OutStandingSince, getutcdate()) AND NUMBER BETWEEN ((@PageNumber-1)*@PageSize+1) AND (@PageNumber*@PageSize) ORDER BY Timestamp ASC";
             var parameters = new[]
             {
                 CreateSqlParameter("PageNumber", pageNumber),
@@ -620,7 +620,7 @@ namespace Paramore.Brighter.Outbox.MsSql
                 CreateSqlParameter("MessageId", message.Id),
                 CreateSqlParameter("MessageType", message.Header.MessageType.ToString()),
                 CreateSqlParameter("Topic", message.Header.Topic),
-                CreateSqlParameter("Timestamp", message.Header.TimeStamp),
+                CreateSqlParameter("Timestamp", message.Header.TimeStamp.ToUniversalTime()), //always store in UTC, as this is how we query messages
                 CreateSqlParameter("CorrelationId", message.Header.CorrelationId),
                 CreateSqlParameter("ReplyTo", message.Header.ReplyTo),
                 CreateSqlParameter("ContentType", message.Header.ContentType),
@@ -636,7 +636,7 @@ namespace Paramore.Brighter.Outbox.MsSql
             var sql = $"UPDATE {_configuration.OutBoxTableName} SET Dispatched = @DispatchedAt WHERE MessageId = @MessageId";
             command.CommandText = sql;
             command.Parameters.Add(CreateSqlParameter("MessageId", messageId));
-            command.Parameters.Add(CreateSqlParameter("DispatchedAt", dispatchedAt));
+            command.Parameters.Add(CreateSqlParameter("DispatchedAt", dispatchedAt?.ToUniversalTime())); //always store in UTC, as this is how we query messages
             return command;
          }
         private SqlCommand InitMarkDispatchedCommand(SqlConnection connection, IEnumerable<Guid> messageIds, DateTime? dispatchedAt)
@@ -646,7 +646,7 @@ namespace Paramore.Brighter.Outbox.MsSql
             var sql = $"UPDATE {_configuration.OutBoxTableName} SET Dispatched = @DispatchedAt WHERE MessageId in ( {inClause} )";
 
             command.CommandText = sql;
-            command.Parameters.Add(CreateSqlParameter("DispatchedAt", dispatchedAt));
+            command.Parameters.Add(CreateSqlParameter("DispatchedAt", dispatchedAt?.ToUniversalTime())); //always store in UTC, as this is how we query messages
 
             return command;
         }
