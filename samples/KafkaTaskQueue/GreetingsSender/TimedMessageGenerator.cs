e0d35dd84d6a3b51708f34e5c38074c6a1f0cb29
Ian Cooper <ian.cooper@just-eat.com>
2022-09-29 11:02:05 +0200

    Use modern hostbuilder approach in sample

--- samples/KafkaTaskQueue/GreetingsSender/TimedMessageGenerator.cs
+++ samples/KafkaTaskQueue/GreetingsSender/TimedMessageGenerator.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Paramore.Brighter;
 
-namespace GreetingsSender.Adapters
+namespace GreetingsSender
 {
     public class TimedMessageGenerator : IHostedService, IDisposable
     {
