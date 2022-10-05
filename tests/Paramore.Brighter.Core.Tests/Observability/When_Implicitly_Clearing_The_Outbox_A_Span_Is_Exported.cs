24ad0be8b0015d05c8210bb559b915cf5c875e9c
Paul Reardon <Paul.Reardon@flagstoneim.com>
2022-10-05 17:05:13 +0100

    Split Tests
    # Conflicts:
    #	tests/Paramore.Brighter.Core.Tests/Observability/When_Implicitly_Clearing_The_Outbox_A_Span_Is_Exported.cs

---
+++ tests/Paramore.Brighter.Core.Tests/Observability/When_Implicitly_Clearing_The_Outbox_A_Span_Is_Exported.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Trace;
+using Paramore.Brighter.Core.Tests.Observability.TestDoubles;
+using Paramore.Brighter.Extensions.DependencyInjection;
+using Polly;
+using Polly.Registry;
+using Xunit;
+
+namespace Paramore.Brighter.Core.Tests.Observability;
+[Collection("Observability")]
+public class ImplicitClearingObservabilityTests
+{
+    private readonly CommandProcessor _commandProcessor;
+    private readonly IAmAnOutboxSync<Message> _outbox;
+    private readonly MyEvent _event;
+    private readonly TracerProvider _traceProvider;
+    private readonly List<Activity> _exportedActivities;
+
+    public ImplicitClearingObservabilityTests()
+    {
+        _outbox = new InMemoryOutbox();
+        _event = new MyEvent("TestEvent");
+
+        var registry = new SubscriberRegistry();
+        registry.Register<MyEvent, MyEventHandler>();
+
+        var messageMapperRegistry = new MessageMapperRegistry(new SimpleMessageMapperFactory((_) => new MyEventMessageMapper()));
+        messageMapperRegistry.Register<MyEvent, MyEventMessageMapper>();
+        
+        var container = new ServiceCollection();
+        container.AddTransient<MyEvent>();
+        container.AddSingleton<IBrighterOptions>(new BrighterOptions() {HandlerLifetime = ServiceLifetime.Transient});
+
+        var builder = Sdk.CreateTracerProviderBuilder();
+        _exportedActivities = new List<Activity>();
+        
+        _traceProvider = builder.AddSource("Paramore.Brighter")
+            .AddInMemoryExporter(_exportedActivities)
+            .Build();
+
+        var handlerFactory = new ServiceProviderHandlerFactory(container.BuildServiceProvider());
+        
+        var retryPolicy = Policy
+            .Handle<Exception>()
+            .Retry();
+
+        var policyRegistry = new PolicyRegistry() {{CommandProcessor.RETRYPOLICY, retryPolicy}};
+        var producerRegistry = new ProducerRegistry(new Dictionary<string, IAmAMessageProducer>()
+        {
+            {MyEvent.Topic, new FakeMessageProducer()}
+        });
+        producerRegistry.GetDefaultProducer().MaxOutStandingMessages = -1;
+        
+        _commandProcessor = new CommandProcessor(registry, handlerFactory, new InMemoryRequestContextFactory(), 
+            policyRegistry, messageMapperRegistry,_outbox,producerRegistry);
+    }
+
+    [Fact]
+    public void When_Clearing_Implicitly()
+    {
+        _commandProcessor.DepositPost(_event);
+        _commandProcessor.ClearOutbox(10, 0);
+        
+        //wait for Background Process
+        Task.Delay(100).Wait();
+
+        _traceProvider.ForceFlush();
+        
+        Assert.NotEmpty(_exportedActivities);
+
+        var act = _exportedActivities.First(a => a.Source.Name == "Paramore.Brighter");
+
+        Assert.NotNull(act);
+        Assert.Equal(false,act.TagObjects.First(a => a.Key == "bulk").Value);
+        Assert.Equal(false,act.TagObjects.First(a => a.Key == "async").Value);
+    }
+}
