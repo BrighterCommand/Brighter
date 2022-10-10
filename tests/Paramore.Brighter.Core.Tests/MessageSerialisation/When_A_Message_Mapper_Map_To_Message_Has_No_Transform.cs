c79ba33083b931f3d8f7405aee25f44ab3b36b5e
Ian Cooper <ian.cooper@just-eat.com>
2022-10-10 16:09:59 +1100

    Vanilla mappers should be added to a wrap

---
+++ tests/Paramore.Brighter.Core.Tests/MessageSerialisation/When_A_Message_Mapper_Map_To_Message_Has_No_Transform.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Paramore.Brighter.Core.Tests.MessageSerialisation.Test_Doubles;
+using Xunit;
+
+namespace Paramore.Brighter.Core.Tests.MessageSerialisation;
+
+public class MessageUnwrapPathNoTransformPipelineTests
+{
+    private UnwrapPipeline<MyTransformableCommand> _transformPipeline;
+    private readonly TransformPipelineBuilder _pipelineBuilder;
+    private readonly MyTransformableCommand _myCommand;
+
+    public MessageUnwrapPathNoTransformPipelineTests()
+    {
+        //arrange
+        var mapperRegistry = new MessageMapperRegistry(new SimpleMessageMapperFactory(_ => new MyVanillaCommandMessageMapper()))
+            { { typeof(MyTransformableCommand), typeof(MyVanillaCommandMessageMapper) } };
+
+        _myCommand = new MyTransformableCommand();
+        
+        var messageTransformerFactory = new SimpleMessageTransformerFactory((_ => null));
+
+        _pipelineBuilder = new TransformPipelineBuilder(mapperRegistry, messageTransformerFactory);
+        
+    }
+    
+    [Fact]
+    public void When_A_Message_Mapper_Map_To_Message_Has_No_Transform()
+    {
+        //act
+        _transformPipeline = _pipelineBuilder.BuildUnwrapPipeline(_myCommand);
+        
+        //assert
+        TraceFilters().ToString().Should().Be("MyVanillaCommandMessageMapper");
+    }
+    
+    private TransformPipelineTracer TraceFilters()
+    {
+        var pipelineTracer = new TransformPipelineTracer();
+        _transformPipeline.DescribePath(pipelineTracer);
+        return pipelineTracer;
+    }
+}
