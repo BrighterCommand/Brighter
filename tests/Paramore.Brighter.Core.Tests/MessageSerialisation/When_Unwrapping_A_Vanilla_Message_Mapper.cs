28ff8d94308f4e2f52142dd015fba128e3631a55
Ian Cooper <ian.cooper@just-eat.com>
2022-10-10 16:17:21 +1100

    vanilla message mapper wrap and unwrap

---
+++ tests/Paramore.Brighter.Core.Tests/MessageSerialisation/When_Unwrapping_A_Vanilla_Message_Mapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.Json;
+using Paramore.Brighter.Core.Tests.MessageSerialisation.Test_Doubles;
+using Xunit;
+
+namespace Paramore.Brighter.Core.Tests.MessageSerialisation;
+
+public class VanillaMessageUnwrapRequestTests
+{
+    private UnwrapPipeline<MyTransformableCommand> _transformPipeline;
+    private readonly TransformPipelineBuilder _pipelineBuilder;
+    private readonly MyTransformableCommand _myCommand;
+    private readonly Message _message;
+
+    public VanillaMessageUnwrapRequestTests()
+    {
+        //arrange
+        var mapperRegistry = new MessageMapperRegistry(new SimpleMessageMapperFactory(_ => new MyVanillaCommandMessageMapper()))
+            { { typeof(MyTransformableCommand), typeof(MyVanillaCommandMessageMapper) } };
+
+        _myCommand = new MyTransformableCommand();
+        
+        var messageTransformerFactory = new SimpleMessageTransformerFactory((_ => null));
+
+        _pipelineBuilder = new TransformPipelineBuilder(mapperRegistry, messageTransformerFactory);
+
+        _message = new Message(
+            new MessageHeader(_myCommand.Id, "transform.event", MessageType.MT_COMMAND, DateTime.UtcNow),
+            new MessageBody(JsonSerializer.Serialize(_myCommand, new JsonSerializerOptions(JsonSerializerDefaults.General)))
+        );
+
+        _message.Header.Bag[MySimpleTransformAsync.HEADER_KEY] = MySimpleTransformAsync.TRANSFORM_VALUE;
+    }
+    
+    [Fact]
+    public void When_Unwrapping_A_Vanilla_Message_Mapper()
+    {
+        //act
+        _transformPipeline = _pipelineBuilder.BuildUnwrapPipeline(_myCommand);
+        var request = _transformPipeline.Unwrap(_message).Result;
+        
+        //assert
+        request.Value = _myCommand.Value;
+    }
+}
---
+++ tests/Paramore.Brighter.Core.Tests/MessageSerialisation/When_Wrapping_A_Vanilla_Message_Mapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Paramore.Brighter.Core.Tests.MessageSerialisation.Test_Doubles;
+using Xunit;
+
+namespace Paramore.Brighter.Core.Tests.MessageSerialisation;
+
+public class VanillaMessageWrapRequestTests
+{
+    private WrapPipeline<MyTransformableCommand> _transformPipeline;
+    private readonly TransformPipelineBuilder _pipelineBuilder;
+    private readonly MyTransformableCommand _myCommand;
+
+    public VanillaMessageWrapRequestTests()
+    {
+        //arrange
+        var mapperRegistry = new MessageMapperRegistry(new SimpleMessageMapperFactory(_ => new MyVanillaCommandMessageMapper()))
+            { { typeof(MyTransformableCommand), typeof(MyVanillaCommandMessageMapper) } };
+
+        _myCommand = new MyTransformableCommand();
+        
+        var messageTransformerFactory = new SimpleMessageTransformerFactory((_ => null));
+
+        _pipelineBuilder = new TransformPipelineBuilder(mapperRegistry, messageTransformerFactory);
+    }
+    
+    [Fact]
+    public void When_Wrapping_A_Vanilla_Message_Mapper()
+    {
+        //act
+        _transformPipeline = _pipelineBuilder.BuildWrapPipeline(_myCommand);
+        var message = _transformPipeline.Wrap(_myCommand).Result;
+        
+        //assert
+        message.Body.Value.Should().Be(JsonSerializer.Serialize(_myCommand, new JsonSerializerOptions(JsonSerializerDefaults.General)).ToString());
+    }
+}
