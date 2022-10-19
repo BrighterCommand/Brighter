52abcb92de952c871fbe8da7b2a935c46d1150f8
Ian Cooper <ian.cooper@just-eat.com>
2022-10-19 20:50:44 +0100

    Fix sample issues for WebAPI_Dapper

--- samples/WebAPI_Dapper/GreetingsPorts/Handlers/DeletePersonHandlerAsync.cs
+++ samples/WebAPI_Dapper/GreetingsPorts/Handlers/DeletePersonHandlerAsync.cs
@@ -22,7 +22,7 @@ namespace GreetingsPorts.Handlers
             _uow = uow;
         }
         
-        [RequestLogging(0, HandlerTiming.Before)]
+        [RequestLoggingAsync(0, HandlerTiming.Before)]
         [UsePolicyAsync(step:1, policy: Policies.Retry.EXPONENTIAL_RETRYPOLICYASYNC)]
         public async override Task<DeletePerson> HandleAsync(DeletePerson deletePerson, CancellationToken cancellationToken = default(CancellationToken))
         {
