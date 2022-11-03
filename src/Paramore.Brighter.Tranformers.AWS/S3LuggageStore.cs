4d3222734a825481c63893034dd5a37b43a19ce2
Ian Cooper <ian.cooper@just-eat.com>
2022-11-03 08:18:38 +0000

    Add an AWS project to hold transforms

---
+++ src/Paramore.Brighter.Tranformers.AWS/S3LuggageStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Paramore.Brighter.Transforms.Storage;
+
+namespace Paramore.Brighter.Tranformers.AWS
+{
+
+    public class S3LuggageStore : IAmAStorageProviderAsync
+    {
+        public async Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Stream> DownloadAsync(Guid claimCheck)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> HasClaim(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Guid> UploadAsync(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
