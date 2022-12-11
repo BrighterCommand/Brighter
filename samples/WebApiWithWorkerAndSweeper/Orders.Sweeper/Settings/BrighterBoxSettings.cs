b8eea7099ea62d0e025f53716b6ff4cc02efe56f
Paul Reardon <Paul@ReardonTech.UK>
2022-12-11 12:45:19 +0000

    Add Sample (#2341)

---
+++ samples/WebApiWithWorkerAndSweeper/Orders.Sweeper/Settings/BrighterBoxSettings.cs
@@ -0,0 +1,27 @@
+namespace Orders.Sweeper.Settings;
+
+public class BrighterBoxSettings
+{
+    public const string SettingsKey = "BrighterBox";
+
+    public string OutboxTableName { get; set; } = "BrighterOutbox";
+
+    public int OutboxSweeperInterval { get; set; } = 5;
+
+    public bool UseMsi { get; set; } = true;
+    public string ConnectionString { get; set; }
+
+    public int MinimumMessageAge { get; set; } = 5000;
+    /// <summary>
+    /// The number of messages to take each run of the sweeper
+    /// </summary>
+    public int BatchSize { get; set; }
+    /// <summary>
+    /// Send using bulk
+    /// </summary>
+    public bool UseBulk { get; set; }
+    /// <summary>
+    /// The chunk size when bulk sending
+    /// </summary>
+    public int BatchChunkSize { get; set; }
+}
