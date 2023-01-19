b2d07d5b806fde8b44221a77008499566d27f8ba
Hon Kuan Yong <honkuan.yong@ideagenplc.com>
2023-01-19 15:23:05 +0800

    Kafka Message Header Builder Implementation

--- src/Paramore.Brighter.MessagingGateway.Kafka/KafkaMessagePublisher.cs
+++ src/Paramore.Brighter.MessagingGateway.Kafka/KafkaMessagePublisher.cs
@@ -1,30 +1,20 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Confluent.Kafka;
-using Paramore.Brighter.Extensions;
 
 namespace Paramore.Brighter.MessagingGateway.Kafka
 {
     internal class KafkaMessagePublisher
     {
         private readonly IProducer<string, byte[]> _producer;
-        
-        private static readonly string[] s_headersToReset;
-        
-        public KafkaMessagePublisher(IProducer<string, byte[]> producer)
+
+        private readonly IKafkaMessageHeaderBuilder _headerBuilder;
+
+        public KafkaMessagePublisher(IProducer<string, byte[]> producer,
+            IKafkaMessageHeaderBuilder headerBuilder)
         {
             _producer = producer;
-        }
-
-        static KafkaMessagePublisher()
-        {
-            s_headersToReset = new[] {
-                HeaderNames.MESSAGE_TYPE,
-                HeaderNames.TOPIC,
-                HeaderNames.CORRELATION_ID,
-                HeaderNames.TIMESTAMP
-            };
+            _headerBuilder = headerBuilder;
         }
 
         public void PublishMessage(Message message, Action<DeliveryReport<string, byte[]>> deliveryReport)
@@ -42,46 +32,9 @@ namespace Paramore.Brighter.MessagingGateway.Kafka
             deliveryReport(deliveryResult);
         }
 
-        private static Message<string, byte[]> BuildMessage(Message message)
+        private Message<string, byte[]> BuildMessage(Message message)
         {
-            var headers = new Headers()
-            {
-                new Header(HeaderNames.MESSAGE_TYPE, message.Header.MessageType.ToString().ToByteArray()),
-                new Header(HeaderNames.TOPIC, message.Header.Topic.ToByteArray()),
-                new Header(HeaderNames.MESSAGE_ID, message.Header.Id.ToString().ToByteArray()),
-                new Header(HeaderNames.TIMESTAMP, BitConverter.GetBytes(UnixTimestamp.GetCurrentUnixTimestampSeconds()))
-            };
-
-            if (message.Header.CorrelationId != Guid.Empty)
-                headers.Add(HeaderNames.CORRELATION_ID, message.Header.CorrelationId.ToString().ToByteArray());
-
-            if (!string.IsNullOrEmpty(message.Header.PartitionKey))
-                headers.Add(HeaderNames.PARTITIONKEY, message.Header.PartitionKey.ToByteArray());
-            
-            if (!string.IsNullOrEmpty(message.Header.ContentType))
-                headers.Add(HeaderNames.CONTENT_TYPE, message.Header.ContentType.ToByteArray());
-
-            if (!string.IsNullOrEmpty(message.Header.ReplyTo))
-                headers.Add(HeaderNames.REPLY_TO, message.Header.ReplyTo.ToByteArray());
-
-            message.Header.Bag.Each((header) =>
-            {
-                if (!s_headersToReset.Any(htr => htr.Equals(header.Key)))
-                {
-                    switch (header.Value)
-                    {
-                        case string stringValue:
-                            headers.Add(header.Key, stringValue.ToByteArray());
-                            break;
-                        case int intValue:
-                            headers.Add(header.Key, BitConverter.GetBytes(intValue));
-                            break;
-                        default:
-                            headers.Add(header.Key, header.Value.ToString().ToByteArray());
-                            break;
-                    }
-                }
-            });
+            var headers = _headerBuilder.Build(message);
 
             var kafkaMessage = new Message<string, byte[]>()
             {
@@ -89,6 +42,7 @@ namespace Paramore.Brighter.MessagingGateway.Kafka
                 Key = message.Header.PartitionKey,
                 Value = message.Body.Bytes
             };
+
             return kafkaMessage;
         }
     }
