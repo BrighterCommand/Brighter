df44ffafaa919d3fc44c52e7a2e34dac1f0e20d0
Paul Reardon <paul.reardon@flagstoneim.com>
2023-02-06 22:42:35 +0000

    Refactor MsSql Outbox to use abstract class

---
+++ src/Paramore.Brighter/IRelationDatabaseOutboxQueries.cs
@@ -0,0 +1,16 @@
+﻿namespace Paramore.Brighter
+{
+    public interface IRelationDatabaseOutboxQueries
+    {
+        string PagedDispatchedCommand { get; }
+        string PagedReadCommand { get; }
+        string PagedOutstandingCommand { get; }
+        string AddCommand { get; }
+        string BulkAddCommand { get; }
+        string MarkDispatchedCommand { get; }
+        string MarkMultipleDispatchedCommand { get; }
+        string GetMessageCommand { get; }
+        string GetMessagesCommand { get; }
+        string DeleteMessagesCommand { get; }
+    }
+}
