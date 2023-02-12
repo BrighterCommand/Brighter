396fe647d97b094455539e37a387eed099e532c6
Paul Reardon <paul@reardontech.uk>
2023-02-12 22:14:48 +0000

    Initial push for Azure Blob Transform

---
+++ src/Paramore.Brighter.Transformers.Azure/AzureBlobLuggageStore.cs
@@ -0,0 +1,72 @@
+﻿using Azure.Core;
+using Azure.Storage.Blobs;
+using Paramore.Brighter.Transforms.Storage;
+
+namespace Paramore.Brighter.Transformers.Azure
+{
+    public class AzureBlobLuggageStore : IAmAStorageProviderAsync
+    {
+        private readonly BlobContainerClient _blockBlobClient;
+        public AzureBlobLuggageStore(Uri containterUrl, TokenCredential tokenCredential)
+        {
+            _blockBlobClient = new BlobContainerClient(containterUrl, tokenCredential);
+        }
+
+        /// <summary>
+        /// Delete the luggage identified by the claim check
+        /// Used to clean up after luggage is retrieved
+        /// </summary>
+        /// <param name="claimCheck">The claim check for the luggage</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        public Task DeleteAsync(string claimCheck, CancellationToken cancellationToken)
+        {
+            var blobClient = _blockBlobClient.GetBlobClient(claimCheck);
+
+            return blobClient.DeleteAsync(cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// Downloads the luggage associated with the claim check
+        /// </summary>
+        /// <param name="claimCheck">The claim check for the luggage</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <returns>The luggage as a stream</returns>
+        public async Task<bool> HasClaimAsync(string claimCheck, CancellationToken cancellationToken)
+        {
+            var blobClient = _blockBlobClient.GetBlobClient(claimCheck);
+
+            var response = await blobClient.ExistsAsync(cancellationToken);
+
+            return response.Value;
+        }
+
+        /// <summary>
+        /// Do we have luggage for this claim check - in case of error or deletion
+        /// </summary>
+        /// <param name="claimCheck"></param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        public Task<Stream> RetrieveAsync(string claimCheck, CancellationToken cancellationToken)
+        {
+            var blobClient = _blockBlobClient.GetBlobClient(claimCheck);
+
+            return blobClient.OpenReadAsync(cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// Puts luggage into the store and provides a claim check for that luggage
+        /// </summary>
+        /// <param name="stream">A stream representing the luggage to check</param>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <returns>A claim check for the luggage stored</returns>
+        public async Task<string> StoreAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            var claimId = Guid.NewGuid().ToString();
+
+            var blobClient = _blockBlobClient.GetBlobClient(claimId);
+
+            await blobClient.UploadAsync(stream, cancellationToken);
+
+            return claimId;
+        }
+    }
+}
---
+++ tests/Paramore.Brighter.Azure.Tests/Transformers/When_uploading_luggage_to_Blob.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Paramore.Brighter.AWS.Tests.Helpers;
+using Paramore.Brighter.Tranformers.AWS;
+using Polly;
+using Polly.Contrib.WaitAndRetry;
+using Polly.Retry;
+using Xunit;
+using Policy = Polly.Policy;
+
+namespace Paramore.Brighter.Azure.Tests.Transformers;
+
+public class S3LuggageUploadTests : IDisposable
+{
+    private readonly BlobContainerClient _client;
+    private readonly AmazonSecurityTokenServiceClient _stsClient;
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly string _bucketName;
+
+    public S3LuggageUploadTests()
+    {
+        //arrange
+        _client = new AmazonS3Client(credentials, region);
+        _stsClient = new AmazonSecurityTokenServiceClient(credentials, region);
+
+        var services = new ServiceCollection();
+        services.AddHttpClient();
+        var provider = services.BuildServiceProvider();
+        _httpClientFactory = provider.GetService<IHttpClientFactory>();
+        _bucketName = $"brightertestbucket-{Guid.NewGuid()}";
+    }
+    
+    [Fact]
+    public async Task When_uploading_luggage_to_Blob()
+    {
+        //arrange
+        var luggageStore = await S3LuggageStore.CreateAsync(
+            client: _client,
+            bucketName: _bucketName,
+            storeCreation: S3LuggageStoreCreation.CreateIfMissing,
+            httpClientFactory: _httpClientFactory,       
+            stsClient: _stsClient,
+#pragma warning disable CS0618 // although obsolete, the region string on the replacement is wrong for our purpose
+            bucketRegion: S3Region.EUW1,
+#pragma warning restore CS0618
+            tags: new List<Tag>(){new Tag{Key = "BrighterTests", Value = "S3LuggageUploadTests"}},
+            acl: S3CannedACL.Private,
+            policy: GetSimpleHandlerRetryPolicy(), 
+            abortFailedUploadsAfterDays: 1, 
+            deleteGoodUploadsAfterDays: 1);    
+        
+        //act
+        //Upload the test stream to S3
+        var testContent = "Well, always know that you shine Brighter";
+        var stream = new MemoryStream();
+        var streamWriter = new StreamWriter(stream);
+        await streamWriter.WriteAsync(testContent);
+        await streamWriter.FlushAsync();
+        stream.Position = 0;
+
+        var claim = await luggageStore.StoreAsync(stream);
+
+        //assert
+        //do we have a claim?
+        (await luggageStore.HasClaimAsync(claim)).Should().BeTrue();
+        
+        //check for the contents indicated by the claim id on S3
+        var result = await luggageStore.RetrieveAsync(claim);
+        var resultAsString = await new StreamReader(result).ReadToEndAsync();
+        resultAsString.Should().Be(testContent);
+
+        await luggageStore.DeleteAsync(claim);
+
+    }
+
+    public void Dispose()
+    {
+        _client.DeleteBucketAsync(_bucketName).GetAwaiter().GetResult();
+    }
+}
