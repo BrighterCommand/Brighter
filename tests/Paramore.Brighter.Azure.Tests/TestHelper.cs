f33c7eb63ad0120e6643c703e3ba3838738e8443
Paul Reardon <paul.reardon@flagstoneim.com>
2023-02-19 21:50:40 +0000

    Add Azure Transforms tests

---
+++ tests/Paramore.Brighter.Azure.Tests/TestHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace Paramore.Brighter.Azure.TestDoubles.Tests;
+
+public class TestHelper
+{
+    public const string BlobLocation = "https://brighterarchivertest.blob.core.windows.net/";
+}
--- tests/Paramore.Brighter.Azure.Tests/Transformers/When_uploading_luggage_to_Blob.cs
+++ tests/Paramore.Brighter.Azure.Tests/Transformers/When_uploading_luggage_to_Blob.cs
@@ -1,63 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using Azure.Identity;
 using Azure.Storage.Blobs;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
-using Paramore.Brighter.AWS.Tests.Helpers;
-using Paramore.Brighter.Tranformers.AWS;
-using Polly;
-using Polly.Contrib.WaitAndRetry;
-using Polly.Retry;
-using Xunit;
+using Paramore.Brighter.Azure.TestDoubles.Tests;
+using Paramore.Brighter.Transformers.Azure;
 using Policy = Polly.Policy;
 
 namespace Paramore.Brighter.Azure.Tests.Transformers;
 
-public class S3LuggageUploadTests : IDisposable
+public class AzureBlobUploadTests : IDisposable
 {
     private readonly BlobContainerClient _client;
-    private readonly AmazonSecurityTokenServiceClient _stsClient;
-    private readonly IHttpClientFactory _httpClientFactory;
     private readonly string _bucketName;
+    private Uri _bucketUrl;
 
-    public S3LuggageUploadTests()
+    public AzureBlobUploadTests()
     {
         //arrange
-        _client = new AmazonS3Client(credentials, region);
-        _stsClient = new AmazonSecurityTokenServiceClient(credentials, region);
-
-        var services = new ServiceCollection();
-        services.AddHttpClient();
-        var provider = services.BuildServiceProvider();
-        _httpClientFactory = provider.GetService<IHttpClientFactory>();
         _bucketName = $"brightertestbucket-{Guid.NewGuid()}";
+        _bucketUrl = new Uri($"{TestHelper.BlobLocation}{_bucketName}");
+
+        _client = new BlobContainerClient(_bucketUrl, new AzureCliCredential());
     }
     
-    [Fact]
+    [Test]
     public async Task When_uploading_luggage_to_Blob()
     {
         //arrange
-        var luggageStore = await S3LuggageStore.CreateAsync(
-            client: _client,
-            bucketName: _bucketName,
-            storeCreation: S3LuggageStoreCreation.CreateIfMissing,
-            httpClientFactory: _httpClientFactory,       
-            stsClient: _stsClient,
-#pragma warning disable CS0618 // although obsolete, the region string on the replacement is wrong for our purpose
-            bucketRegion: S3Region.EUW1,
-#pragma warning restore CS0618
-            tags: new List<Tag>(){new Tag{Key = "BrighterTests", Value = "S3LuggageUploadTests"}},
-            acl: S3CannedACL.Private,
-            policy: GetSimpleHandlerRetryPolicy(), 
-            abortFailedUploadsAfterDays: 1, 
-            deleteGoodUploadsAfterDays: 1);    
+        await _client.CreateIfNotExistsAsync();
+        var luggageStore = new AzureBlobLuggageStore(_bucketUrl, new AzureCliCredential());
         
         //act
-        //Upload the test stream to S3
+        //Upload the test stream to Azure
         var testContent = "Well, always know that you shine Brighter";
         var stream = new MemoryStream();
         var streamWriter = new StreamWriter(stream);
@@ -65,23 +39,23 @@ public class S3LuggageUploadTests : IDisposable
         await streamWriter.FlushAsync();
         stream.Position = 0;
 
-        var claim = await luggageStore.StoreAsync(stream);
+        var claim = await luggageStore.StoreAsync(stream, CancellationToken.None);
 
         //assert
         //do we have a claim?
-        (await luggageStore.HasClaimAsync(claim)).Should().BeTrue();
+        (await luggageStore.HasClaimAsync(claim, CancellationToken.None)).Should().BeTrue();
         
         //check for the contents indicated by the claim id on S3
-        var result = await luggageStore.RetrieveAsync(claim);
+        var result = await luggageStore.RetrieveAsync(claim, CancellationToken.None);
         var resultAsString = await new StreamReader(result).ReadToEndAsync();
         resultAsString.Should().Be(testContent);
 
-        await luggageStore.DeleteAsync(claim);
+        await luggageStore.DeleteAsync(claim, CancellationToken.None);
 
     }
 
     public void Dispose()
     {
-        _client.DeleteBucketAsync(_bucketName).GetAwaiter().GetResult();
+        _client.Delete();
     }
 }
