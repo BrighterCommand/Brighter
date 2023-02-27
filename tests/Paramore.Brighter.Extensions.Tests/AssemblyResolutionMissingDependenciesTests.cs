1e70c067cba579513f52e8c5ff296fe9d3208efb
Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
2023-02-27 09:30:22 +0000

    Missing Dependencies for Built In Types break with validation (#2516)
    * Add a failing test, that breaks if we are missing a dependency
    
    * Fix by providing a null store that throws when you don't register

---
+++ tests/Paramore.Brighter.Extensions.Tests/AssemblyResolutionMissingDependenciesTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Paramore.Brighter.ServiceActivator.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Paramore.Brighter.Extensions.Tests;
+
+public class AssemblyResolutionMissingDependenciesTests
+{
+    [Fact]
+    public void When_we_auto_register_handlers_from_assemblies_with_missing_dependencies()
+    {
+        //arrange
+        var factory =
+            new DefaultServiceProviderFactory(new ServiceProviderOptions
+            {
+                ValidateOnBuild = true, ValidateScopes = true
+            });
+
+        var services = new ServiceCollection();
+
+        services.AddServiceActivator().AutoFromAssemblies();
+        
+        //act
+        var provider = factory.CreateServiceProvider(services);
+        
+        //assert
+        //we will not get here, if we have a missing dependency
+        Assert.True(true);
+        
+    }
+}
