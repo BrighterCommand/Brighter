85780d8f483c89985473ef42eb8b41c2bfb29126
Paul Reardon <Paul.Reardon@flagstoneim.com>
2023-04-04 08:48:44 +0100

    Add Options and Tagging to the Archive provider

--- src/Paramore.Brighter.Archive.Azure/AzureBlobArchiveProviderOptions.cs
+++ src/Paramore.Brighter.Archive.Azure/AzureBlobArchiveProviderOptions.cs
@@ -1,10 +1,27 @@
 using Azure.Core;
+using Azure.Storage.Blobs.Models;
 
 namespace Paramore.Brighter.Storage.Azure;
 
 public class AzureBlobArchiveProviderOptions
 {
+    /// <summary>
+    /// The URI of the blob container
+    /// </summary>
     public Uri BlobContainerUri;
 
+    /// <summary>
+    /// The Credential to use when writing blobs
+    /// </summary>
     public TokenCredential TokenCredential;
+    
+    /// <summary>
+    /// The Access Tier of the blobs
+    /// </summary>
+    public AccessTier AccessTier = AccessTier.Cool;
+
+    /// <summary>
+    /// If enable write tags to the blobs
+    /// </summary>
+    public bool TagBlobs = false;
 }
