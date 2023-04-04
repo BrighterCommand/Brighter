7bba0ec8e02cdd71470e06dd7f8476ed9ee11239
Paul Reardon <Paul@ReardonTech.UK>
2023-04-04 09:05:13 +0100

    Add Options and Tagging to the Archive provider (#2593)

--- src/Paramore.Brighter.Archive.Azure/AzureBlobArchiveProvider.cs
+++ src/Paramore.Brighter.Archive.Azure/AzureBlobArchiveProvider.cs
@@ -1,16 +1,23 @@
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
 namespace Paramore.Brighter.Storage.Azure;
 
 public class AzureBlobArchiveProvider : IAmAnArchiveProvider
 {
     private BlobContainerClient _containerClient;
-    
+    private readonly AzureBlobArchiveProviderOptions _options;
+
     public AzureBlobArchiveProvider(AzureBlobArchiveProviderOptions options)
     {
         _containerClient = new BlobContainerClient(options.BlobContainerUri, options.TokenCredential);
+        _options = options;
     }
 
+    /// <summary>
+    /// Send a Message to the archive provider
+    /// </summary>
+    /// <param name="message">Message to send</param>
     public void ArchiveMessage(Message message)
     {
         var blobClient = _containerClient.GetBlobClient(message.Id.ToString());
@@ -18,22 +25,32 @@ public class AzureBlobArchiveProvider : IAmAnArchiveProvider
         blobClient.Upload(BinaryData.FromBytes(message.Body.Bytes));
     }
 
+    /// <summary>
+    /// Send a Message to the archive provider
+    /// </summary>
+    /// <param name="message">Message to send</param>
+    /// <param name="cancellationToken">The Cancellation Token</param>
     public async Task ArchiveMessageAsync(Message message, CancellationToken cancellationToken)
     {
         var blobClient = _containerClient.GetBlobClient(message.Id.ToString());
+        
+        var opts = new BlobUploadOptions()
+        {
+            AccessTier = _options.AccessTier,
+        };
 
-        // var options = new Dictionary<string, string>()
-        //     {
-        //         { "topic", message.Header.Topic },
-        //         { "correlationId", message.Header.CorrelationId.ToString() },
-        //         { "message_type", message.Header.MessageType.ToString() },
-        //         { "timestamp", message.Header.TimeStamp.ToString() },
-        //         { "content_type", message.Header.ContentType }
-        //     };
-        //
-        // await blobClient.SetTagsAsync(options, cancellationToken: cancellationToken);
-
-        // await blobClient.UploadAsync(BinaryData.FromString(message.Body.Value));
-        await blobClient.UploadAsync(BinaryData.FromBytes(message.Body.Bytes), cancellationToken);
+        if (_options.TagBlobs)
+        {
+            opts.Tags = new Dictionary<string, string>()
+            {
+                { "topic", message.Header.Topic },
+                { "correlationId", message.Header.CorrelationId.ToString() },
+                { "message_type", message.Header.MessageType.ToString() },
+                { "timestamp", message.Header.TimeStamp.ToString() },
+                { "content_type", message.Header.ContentType }
+            };
+        }
+        
+        await blobClient.UploadAsync(BinaryData.FromBytes(message.Body.Bytes), opts, cancellationToken);
     }
 }
