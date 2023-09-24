7931bec02b15f668ce0c66757fdd38be1d30004b
Tim Salva <tim.salva@justeattakeaway.com>
2023-09-24 18:15:26 +0100

    Enable DynamoDbInbox configuration

--- tests/Paramore.Brighter.DynamoDB.Tests/Inbox/When_writing_a_message_to_the_inbox.cs
+++ tests/Paramore.Brighter.DynamoDB.Tests/Inbox/When_writing_a_message_to_the_inbox.cs
@@ -40,7 +40,7 @@ namespace Paramore.Brighter.DynamoDB.Tests.Inbox
 
         public DynamoDbInboxAddMessageTests()
         {
-            _dynamoDbInbox = new DynamoDbInbox(Client);
+            _dynamoDbInbox = new DynamoDbInbox(Client, new DynamoDbInboxConfiguration());
 
             _raisedCommand = new MyCommand {Value = "Test"};
             _contextKey = "context-key";
