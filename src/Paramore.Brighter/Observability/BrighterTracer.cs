1b73ed87cfdbe74de85078d61cb63dc3bb3806b3
Ian Cooper <ian.cooper@justeattakeaway.com>
2024-05-17 19:17:55 +0100

    Add a tracer to Brighter

---
+++ src/Paramore.Brighter/Observability/BrighterTracer.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace Paramore.Brighter.Observability;
+
+public static class BrighterTracer
+{
+    private static readonly AssemblyName AssemblyName = typeof(BrighterTracer).Assembly.GetName();
+    public static readonly string SourceName = AssemblyName.Name;
+    public static readonly string Version = AssemblyName.Version.ToString();
+    public static readonly ActivitySource ActivitySource = new (SourceName, Version); 
+}
---
+++ tests/Paramore.Brighter.Core.Tests/Observability/Common/When_creating_an_activity_source_use_lib_name_and_version.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Paramore.Brighter.Observability;
+using Xunit;
+
+namespace Paramore.Brighter.Core.Tests.Observability.Common;
+
+public class BrighterActivitySourceTests 
+{
+    [Fact]
+    public void When_creating_an_activity_source_use_lib_name_and_version()
+    {
+        //act
+        var activitySource = BrighterTracer.ActivitySource;
+        
+        //assert
+        BrighterTracer.SourceName.Should().Be("Paramore.Brighter");
+        BrighterTracer.Version.Should().NotBeNull();
+        activitySource.Name.Should().Be(BrighterTracer.SourceName);
+        activitySource.Version.Should().Be(BrighterTracer.Version);
+    }
+}
