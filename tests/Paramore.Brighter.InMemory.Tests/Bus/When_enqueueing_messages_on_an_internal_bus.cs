60cbbcc6debcd4727e6efc938bf872df8ac1d3a3
Ian Cooper <ian.cooper@justeattakeaway.com>
2024-05-31 21:35:53 +0200

    Add an internal bus for tests

---
+++ tests/Paramore.Brighter.InMemory.Tests/Bus/When_enqueueing_messages_on_an_internal_bus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Paramore.Brighter.InMemory.Tests.Bus;
+
+public class InternalBusTests 
+{
+    [Fact]
+    public void When_enqueueing_messages_on_an_internal_bus()
+    {
+        //arrange
+        var internalBus = new InternalBus();
+        var messageId = Guid.NewGuid().ToString();
+        const string topic = "test";
+         
+        //act
+       internalBus.Enqueue(new Message(new MessageHeader(messageId, topic, MessageType.MT_COMMAND), new MessageBody("test_content")));
+        
+        //assert
+        var message = internalBus.Stream(new RoutingKey(topic)).Single();
+        message.Should().NotBeNull();
+        message.Header.MessageType.Should().Be(MessageType.MT_COMMAND);
+        message.Body.Value.Should().Be("test_content");
+        message.Header.Topic.Should().Be(topic);
+        message.Header.Id.Should().Be(messageId);
+    }
+}
