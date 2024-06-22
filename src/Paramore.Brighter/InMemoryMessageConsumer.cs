3e73a0b986a848d70c89d02e901d702c39414ba2
Ian Cooper <ian.cooper@justeattakeaway.com>
2024-06-22 21:06:48 +0100

    feature: add an inmemory consumer for use with the bus

---
+++ src/Paramore.Brighter/InMemoryMessageConsumer.cs
@@ -0,0 +1,133 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+using System.Collections.Concurrent;
+
+namespace Paramore.Brighter;
+
+/// <summary>
+/// An in memory consumer that reads from the Internal Bus. Mostly used for testing. Can be used with <see cref="Paramore.Brighter.InMemoryProducer"/>
+/// and <see cref="Paramore.Brighter.InternalBus"/> to provide an in-memory message bus for a modular monolith.
+/// If you set an <paramref name="ackTimeoutMs"/> then the consumer will requeue the message if it is not acknowledged
+/// within the timeout. This is controlled by a background thread that checks the messages in the locked list
+/// and requeues them if they have been locked for longer than the timeout.
+/// </summary>
+/// <param name="topic">The <see cref="Paramore.Brighter.RoutingKey"/> that we want to consume from</param>
+/// <param name="bus">The <see cref="Paramore.Brighter.InternalBus"/> that we want to read the messages from</param>
+/// <param name="timeProvider">Allows us to use a timer that can be controlled from tests</param>
+/// <param name="ackTimeoutMs">The period before we requeue an unacknowledged message; defaults to -1 or infinite</param>
+public class InMemoryMessageConsumer(RoutingKey topic, InternalBus bus, TimeProvider timeProvider, int ackTimeoutMs = -1)
+    : IAmAMessageConsumer
+{
+    private readonly ConcurrentDictionary<string, LockedMessage> _lockedMessages = new();
+
+    /// <summary>
+    /// Acknowledges the specified message.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    public void Acknowledge(Message message)
+    {
+        _lockedMessages.TryRemove(message.Id, out _);
+    }
+
+    /// <summary>
+    /// Rejects the specified message.
+    /// </summary>
+    /// <param name="message">The message.</param>
+    public void Reject(Message message)
+    {
+        _lockedMessages.TryRemove(message.Id, out _);
+    }
+
+    /// <summary>
+    /// Purges the specified queue name.
+    /// </summary>
+    public void Purge()
+    {
+        Message message;
+        do {
+            message = bus.Dequeue(topic);
+        } while (message.Header.MessageType != MessageType.MT_NONE);
+    }
+
+    /// <summary>
+    /// Receives the specified queue name.
+    /// An abstraction over a third-party messaging library. Used to read messages from the broker and to acknowledge the processing of those messages or requeue them.
+    /// Used by a <see cref="Paramore.Brighter.Channel"/> to provide access to a third-party message queue.
+    /// </summary>
+    /// <param name="timeoutInMilliseconds">The timeout in milliseconds.</param>
+    /// <returns>An array of Messages from middleware</returns>
+    public Message[] Receive(int timeoutInMilliseconds = 1000)
+    {
+        //TODO: Should requeue a message that has not been processed in a time window
+        CheckForTimedOutMessages();
+        
+        var messages = new[] {bus.Dequeue(topic)};
+        _lockedMessages.TryAdd(messages[0].Id, new LockedMessage(messages[0], timeProvider.GetUtcNow()));
+        return messages;
+    }
+
+    /// <summary>
+    /// Requeues the specified message.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="delayMilliseconds">Number of milliseconds to delay delivery of the message.</param>
+    /// <returns>True if the message should be acked, false otherwise</returns>
+    public bool Requeue(Message message, int delayMilliseconds)
+    {
+        if (delayMilliseconds < 0)
+            return Requeue(message);
+
+        //we don't want to block, so we use a timer to invoke the requeue after a delay
+        timeProvider.CreateTimer(msg => Requeue((Message)msg), message, TimeSpan.FromMilliseconds(delayMilliseconds), TimeSpan.Zero);
+
+        return true;
+    }
+
+    public void Dispose() {}
+    
+    private void CheckForTimedOutMessages()
+    {
+        //start a timer using the time provider
+        
+        //on a callback check the locked messages
+        
+        //if they are older than the timeout, requeue them
+        
+    }
+    
+    private bool Requeue(Message message)
+    {
+        if (_lockedMessages.TryRemove(message.Id, out _))
+        {
+            bus.Enqueue(message);
+            return true;
+        }
+
+        return false;
+    }
+
+    private record LockedMessage(Message Message, DateTimeOffset LockedAt);
+}
---
+++ tests/Paramore.Brighter.InMemory.Tests/Consumer/When_reading_messages_via_a_consumer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Time.Testing;
+using Xunit;
+
+namespace Paramore.Brighter.InMemory.Tests.Consumer;
+
+public class InMemoryChannelTests
+{
+    [Fact]
+    public void When_reading_messages_via_a_consumer()
+    {
+        //arrange
+        const string myTopic = "my topic";
+        var routingKey = new RoutingKey(myTopic);
+        
+        var expectedMessage = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), myTopic, MessageType.MT_EVENT),
+            new MessageBody("a test body"));
+
+
+        var bus = new InternalBus();
+        bus.Enqueue(expectedMessage);
+
+        var consumer = new InMemoryMessageConsumer(routingKey, bus, new FakeTimeProvider(), 1000);
+        
+        //act
+        var receivedMessage = consumer.Receive().Single();
+
+        //assert
+        Assert.Equal(expectedMessage, receivedMessage);
+        Assert.Empty(bus.Stream(routingKey));
+    }
+
+    [Fact]
+    public void When_requeueing_a_message_it_should_be_available_again()
+    {
+        //arrange
+        const string myTopic = "my topic";
+        var routingKey = new RoutingKey(myTopic);
+
+        var expectedMessage = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), myTopic, MessageType.MT_EVENT),
+            new MessageBody("a test body"));
+        
+        var bus = new InternalBus();
+        bus.Enqueue(expectedMessage);
+
+        var consumer = new InMemoryMessageConsumer(routingKey, bus, new FakeTimeProvider(), 1000);
+        
+        //act
+        var receivedMessage = consumer.Receive().Single();
+        consumer.Requeue(receivedMessage, 0);
+        
+        //assert
+        Assert.Single(bus.Stream(routingKey));
+        
+    }
+    
+    [Fact]
+    public async Task When_requeueing_a_message_with_a_delay_it_should_not_be_available_immediately()
+    {
+        //arrange
+        const string myTopic = "my topic";
+        var routingKey = new RoutingKey(myTopic);
+
+        var expectedMessage = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), myTopic, MessageType.MT_EVENT),
+            new MessageBody("a test body"));
+        
+        var bus = new InternalBus();
+        bus.Enqueue(expectedMessage);
+
+        var timeProvider = new FakeTimeProvider();
+        var consumer = new InMemoryMessageConsumer(routingKey, bus, timeProvider, 1000);
+        
+        //act
+        var receivedMessage = consumer.Receive().Single();
+        consumer.Requeue(receivedMessage, 1000);
+        
+        //assert
+        Assert.Empty(bus.Stream(routingKey));
+        
+        timeProvider.Advance(TimeSpan.FromSeconds(2));
+        
+        Assert.Single(bus.Stream(routingKey));
+    }
+
+}
