b18cc09249a0a79a4a50e801cca3e378405ba38c
Ian Cooper <ian.cooper@justeattakeaway.com>
2024-06-23 15:28:41 +0100

    feature: add in-memory channel

---
+++ src/Paramore.Brighter/InMemoryChannelFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Paramore.Brighter;
+
+public class InMemoryChannelFactory(InternalBus internalBus, TimeProvider timeProvider) : IAmAChannelFactory
+{
+    public IAmAChannel CreateChannel(Subscription subscription)
+    {
+        return new Channel(
+            subscription.ChannelName, 
+            new InMemoryMessageConsumer(subscription.RoutingKey,internalBus, timeProvider, 1000),
+            subscription.BufferSize
+            );
+    }
+}
