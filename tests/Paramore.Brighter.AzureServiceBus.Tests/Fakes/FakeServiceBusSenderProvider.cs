8fe55a7050a18e01c40ff4016c64c22bd948af54
Paul Reardon <Paul@ReardonTech.UK>
2024-08-26 10:31:18 +0100

    Refactor ASB Transport (#3229)
    Refactor Azure Service Bus transport to better support Queues and Topics
    
    Updated the tests to use Fakes instead of Mocks

---
+++ tests/Paramore.Brighter.AzureServiceBus.Tests/Fakes/FakeServiceBusSenderProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Paramore.Brighter.MessagingGateway.AzureServiceBus.AzureServiceBusWrappers;
+
+namespace Paramore.Brighter.AzureServiceBus.Tests.Fakes;
+
+public class FakeServiceBusSenderProvider(IServiceBusSenderWrapper sender) : IServiceBusSenderProvider
+{
+    public int CreationCount { get; private set; } = 0;
+
+    public Exception SingleThrowGetException { get; set; } = null;
+    
+    public IServiceBusSenderWrapper Get(string topicOrQueueName)
+    {
+        if (SingleThrowGetException != null)
+        {
+            var ex = SingleThrowGetException;
+            SingleThrowGetException = null;
+            throw ex;
+        }
+        CreationCount++;
+        return sender;
+    }
+}
