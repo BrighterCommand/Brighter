d92cb84d3d1fd4018659f7c1cac58ca6dbe7aa0f
Ian Cooper <ian.cooper@justeattakeaway.com>
2024-10-30 21:39:29 +0000

    feat: required workflow classes to write setup of rist mediator test

---
+++ src/Paramore.Brighter.Workflow/WorkflowState.cs
@@ -0,0 +1,79 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2024 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+
+namespace Paramore.Brighter.Workflow;
+
+public enum WorkflowStep
+{
+    Start,
+    FireAndForget,
+    RequestReaction,
+    Publish,
+    Choice,
+    Wait, 
+    Failure,
+    Finish
+}
+
+/// <summary>
+/// WorkflowState represents the current state of the workflow and tracks if it’s awaiting a response.
+/// </summary>
+public class WorkflowState
+{
+    /// <summary>
+    /// The id of the workflow, used to save-retrieve it from storage
+    /// </summary>
+    public  Guid Id { get; private set; } = Guid.NewGuid();
+    
+    /// <summary>
+    /// What is the current state of the workflow
+    /// </summary>
+    public WorkflowStep CurrentStep { get; set; }
+    
+    /// <summary>
+    /// Is the workflow currently awaiting an event response
+    /// </summary>
+    public bool AwaitingResponse { get; set; } = false;
+
+    /// <summary>
+    /// Constructs a new Workflows instance in a give state
+    /// </summary>
+    /// <param name="initialStep"></param>
+    public WorkflowState(WorkflowStep initialStep)
+    {
+        CurrentStep = initialStep;
+    }
+
+    /// <summary>
+    ///  Default constructor, used for serdes
+    /// </summary>
+    public WorkflowState() { }
+}
+
+public class NullWorkflowState : WorkflowState
+{
+    public NullWorkflowState() : base(WorkflowStep.Finish) { }  
+}
---
+++ tests/Paramore.Brighter.Core.Tests/Workflows/When_running_a_single_step_workflow.cs
@@ -0,0 +1,31 @@
+﻿using Paramore.Brighter.Core.Tests.Mediator.TestDoubles;
+using Paramore.Brighter.Workflow;
+using Polly.Registry;
+using Xunit;
+
+namespace Paramore.Brighter.Core.Tests.Workflows;
+
+public class MediatorOneStepFlowTests 
+{
+    private readonly MyCommandHandler _myCommandHandler;
+
+    public MediatorOneStepFlowTests()
+    {
+        var registry = new SubscriberRegistry();
+        registry.Register<MyCommand, MyCommandHandler>();
+        _myCommandHandler = new MyCommandHandler();
+        var handlerFactory = new SimpleHandlerFactorySync(_ => _myCommandHandler);
+
+        var commandProcessor = new CommandProcessor(registry, handlerFactory, new InMemoryRequestContextFactory(), new PolicyRegistry());
+        PipelineBuilder<MyCommand>.ClearPipelineCache();    
+        
+        var stateStore = new InMemoryStateStore();
+        var mediator = new Workflow.Mediator(commandProcessor, stateStore);
+    }
+    
+    [Fact]
+    public void When_running_a_single_step_workflow()
+    {
+        
+    }
+}
