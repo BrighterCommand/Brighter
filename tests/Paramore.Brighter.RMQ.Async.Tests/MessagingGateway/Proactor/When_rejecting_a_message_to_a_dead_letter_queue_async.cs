0624dd7b4bab8f4d99fe305094ad0d714148d43c
Ian Cooper <ian.cooper@justeattakeaway.com>
2025-02-04 09:40:39 +0000

    chore: move existing rmq tests to async tests

---
+++ tests/Paramore.Brighter.RMQ.Async.Tests/MessagingGateway/Proactor/When_rejecting_a_message_to_a_dead_letter_queue_async.cs
@@ -0,0 +1,113 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Paramore.Brighter.MessagingGateway.RMQ.Async;
+using Xunit;
+
+namespace Paramore.Brighter.RMQ.Async.Tests.MessagingGateway.Proactor;
+
+[Trait("Category", "RMQ")]
+public class RmqMessageProducerDLQTestsAsync : IDisposable, IAsyncDisposable
+{
+    private readonly IAmAMessageProducerAsync _messageProducer;
+    private readonly IAmAMessageConsumerAsync _messageConsumer;
+    private readonly Message _message;
+    private readonly IAmAMessageConsumerSync _deadLetterConsumer;
+
+    public RmqMessageProducerDLQTestsAsync()
+    {
+        var routingKey = new RoutingKey(Guid.NewGuid().ToString());
+            
+        _message = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), routingKey, 
+                MessageType.MT_COMMAND), 
+            new MessageBody("test content"));
+
+        var queueName = new ChannelName(Guid.NewGuid().ToString());
+        var deadLetterQueueName = new ChannelName($"{_message.Header.Topic}.DLQ");
+        var deadLetterRoutingKey = new RoutingKey( $"{_message.Header.Topic}.DLQ");
+            
+        var rmqConnection = new RmqMessagingGatewayConnection
+        {
+            AmpqUri = new AmqpUriSpecification(new Uri("amqp://guest:guest@localhost:5672/%2f")),
+            Exchange = new Exchange("paramore.brighter.exchange"),
+            DeadLetterExchange = new Exchange("paramore.brighter.exchange.dlq")
+        };
+            
+        _messageProducer = new RmqMessageProducer(rmqConnection);
+
+        _messageConsumer = new RmqMessageConsumer(
+            connection: rmqConnection, 
+            queueName: queueName, 
+            routingKey: routingKey, 
+            isDurable: false, 
+            highAvailability: false,
+            deadLetterQueueName: deadLetterQueueName,
+            deadLetterRoutingKey: deadLetterRoutingKey,
+            makeChannels:OnMissingChannel.Create
+        );
+
+        _deadLetterConsumer = new RmqMessageConsumer(
+            connection: rmqConnection,
+            queueName: deadLetterQueueName,
+            routingKey: deadLetterRoutingKey,
+            isDurable:false,
+            makeChannels:OnMissingChannel.Assume
+        );
+    }
+
+    [Fact]
+    public async Task When_rejecting_a_message_to_a_dead_letter_queue()
+    {
+        //create the infrastructure
+        await _messageConsumer.ReceiveAsync(TimeSpan.FromMilliseconds(0)); 
+            
+        await _messageProducer.SendAsync(_message);
+
+        var message = (await _messageConsumer.ReceiveAsync(TimeSpan.FromMilliseconds(10000))).First(); 
+            
+        //This will push onto the DLQ
+        await _messageConsumer.RejectAsync(message);
+
+        var dlqMessage = _deadLetterConsumer.Receive(TimeSpan.FromMilliseconds(10000)).First();
+            
+        //assert this is our message
+        dlqMessage.Id.Should().Be(_message.Id);
+        message.Body.Value.Should().Be(dlqMessage.Body.Value);
+    }
+
+    public void Dispose()
+    {
+        ((IAmAMessageProducerSync)_messageProducer).Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _messageProducer.DisposeAsync();
+    }
+}
---
+++ tests/Paramore.Brighter.RMQ.Async.Tests/MessagingGateway/Reactor/When_retry_limits_force_a_message_onto_the_DLQ.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Paramore.Brighter.MessagingGateway.RMQ.Async;
+using Paramore.Brighter.RMQ.Async.Tests.TestDoubles;
+using Paramore.Brighter.ServiceActivator;
+using Polly.Registry;
+using Xunit;
+
+namespace Paramore.Brighter.RMQ.Async.Tests.MessagingGateway.Reactor;
+
+[Trait("Category", "RMQ")]
+[Trait("Fragile", "CI")]
+public class RMQMessageConsumerRetryDLQTests : IDisposable
+{
+    private readonly IAmAMessagePump _messagePump;
+    private readonly Message _message;
+    private readonly IAmAChannelSync _channel;
+    private readonly RmqMessageProducer _sender;
+    private readonly RmqMessageConsumer _deadLetterConsumer;
+    private readonly RmqSubscription<MyCommand> _subscription;
+
+
+    public RMQMessageConsumerRetryDLQTests()
+    {
+        string correlationId = Guid.NewGuid().ToString();
+        string contentType = "text\\plain";
+        var channelName = new ChannelName($"Requeue-Limit-Tests-{Guid.NewGuid().ToString()}");
+        var routingKey = new RoutingKey($"Requeue-Limit-Tests-{Guid.NewGuid().ToString()}");
+
+        //what do we send
+        var myCommand = new MyDeferredCommand { Value = "Hello Requeue" };
+        _message = new Message(
+            new MessageHeader(myCommand.Id, routingKey, MessageType.MT_COMMAND, correlationId: correlationId, 
+                contentType: contentType
+            ),
+            new MessageBody(JsonSerializer.Serialize((object)myCommand, JsonSerialisationOptions.Options))
+        );
+
+        var deadLetterQueueName = new ChannelName($"{Guid.NewGuid().ToString()}.DLQ");
+        var deadLetterRoutingKey = new RoutingKey( $"{_message.Header.Topic}.DLQ");
+
+        _subscription = new RmqSubscription<MyCommand>(
+            name: new SubscriptionName("DLQ Test Subscription"),
+            channelName: channelName,
+            routingKey: routingKey,
+            //after 0 retries fail and move to the DLQ
+            requeueCount: 0,
+            //delay before re-queuing
+            requeueDelay: TimeSpan.FromMilliseconds(50),
+            deadLetterChannelName: deadLetterQueueName,
+            deadLetterRoutingKey: deadLetterRoutingKey,
+            messagePumpType: MessagePumpType.Reactor,
+            makeChannels: OnMissingChannel.Create
+        );
+
+        var rmqConnection = new RmqMessagingGatewayConnection
+        {
+            AmpqUri = new AmqpUriSpecification(new Uri("amqp://guest:guest@localhost:5672/%2f")),
+            Exchange = new Exchange("paramore.brighter.exchange"),
+            DeadLetterExchange = new Exchange("paramore.brighter.exchange.dlq")
+        };
+
+        //how do we send to the queue
+        _sender = new RmqMessageProducer(rmqConnection, new RmqPublication
+        {
+            Topic = routingKey, 
+            RequestType = typeof(MyDeferredCommand)
+        });
+
+        //set up our receiver
+        ChannelFactory channelFactory = new(new RmqMessageConsumerFactory(rmqConnection));
+        _channel = channelFactory.CreateSyncChannel(_subscription);
+
+        //how do we handle a command
+        IHandleRequests<MyDeferredCommand> handler = new MyDeferredCommandHandler();
+
+        //hook up routing for the command processor
+        var subscriberRegistry = new SubscriberRegistry();
+        subscriberRegistry.Register<MyDeferredCommand, MyDeferredCommandHandler>();
+
+        //once we read, how do we dispatch to a handler. N.B. we don't use this for reading here
+        IAmACommandProcessor commandProcessor = new CommandProcessor(
+            subscriberRegistry: subscriberRegistry,
+            handlerFactory: new QuickHandlerFactory(() => handler),
+            requestContextFactory: new InMemoryRequestContextFactory(),
+            policyRegistry: new PolicyRegistry()
+        );
+
+        //pump messages from a channel to a handler - in essence we are building our own dispatcher in this test
+        var messageMapperRegistry = new MessageMapperRegistry(
+            new SimpleMessageMapperFactory(_ => new MyDeferredCommandMessageMapper()),
+            null
+        );
+            
+        messageMapperRegistry.Register<MyDeferredCommand, MyDeferredCommandMessageMapper>();
+            
+        _messagePump = new Reactor<MyDeferredCommand>(commandProcessor, messageMapperRegistry, 
+            new EmptyMessageTransformerFactory(), new InMemoryRequestContextFactory(), _channel)
+        {
+            Channel = _channel, TimeOut = TimeSpan.FromMilliseconds(5000), RequeueCount = 0
+        };
+
+        _deadLetterConsumer = new RmqMessageConsumer(
+            connection: rmqConnection,
+            queueName: deadLetterQueueName,
+            routingKey: deadLetterRoutingKey,
+            isDurable: false,
+            makeChannels: OnMissingChannel.Assume
+        );
+    }
+
+    [Fact(Skip = "Breaks due to fault in Task Scheduler running after context has closed")]
+    [SuppressMessage("Usage", "xUnit1031:Do not use blocking task operations in test method")]
+    public async Task When_retry_limits_force_a_message_onto_the_dlq()
+    {
+        //NOTE: This test is **slow** because it needs to ensure infrastructure and then wait whilst we requeue a message a number of times,
+        //then propagate to the DLQ
+            
+        //start a message pump, let it create infrastructure 
+        var task = Task.Factory.StartNew(() => _messagePump.Run(), TaskCreationOptions.LongRunning);
+        await Task.Delay(20000);
+
+        //put something on an SNS topic, which will be delivered to our SQS queue
+        _sender.Send(_message);
+
+        //Let the message be handled and deferred until it reaches the DLQ
+        await Task.Delay(20000);
+
+        //send a quit message to the pump to terminate it 
+        var quitMessage = MessageFactory.CreateQuitMessage(_subscription.RoutingKey);
+        _channel.Enqueue(quitMessage);
+
+        //wait for the pump to stop once it gets a quit message
+        await Task.WhenAll(task);
+
+        await Task.Delay(5000);
+
+        //inspect the dlq
+        var dlqMessage = _deadLetterConsumer.Receive(new TimeSpan(10000)).First();
+
+        //assert this is our message
+        dlqMessage.Body.Value.Should().Be(_message.Body.Value);
+
+        _deadLetterConsumer.Acknowledge(dlqMessage);
+
+    }
+
+    public void Dispose()
+    {
+        _channel.Dispose();
+        _deadLetterConsumer.Dispose();
+    }
+
+}
