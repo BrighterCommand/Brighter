73ee3bf7f01f5318c1107aebd74b5b0e37839ebc
Ian Cooper <ian.cooper@justeattakeaway.com>
2025-02-26 23:44:26 +0000

    chore: remove fluentvalidaor from feature switches

--- tests/Paramore.Brighter.Core.Tests/FeatureSwitch/When_No_Feature_Switch_Config.cs
+++ tests/Paramore.Brighter.Core.Tests/FeatureSwitch/When_No_Feature_Switch_Config.cs
@@ -1,30 +1,5 @@
-﻿#region Licence
-/* The MIT License (MIT)
-Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the “Software”), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE. */
-
-#endregion
-
-using System;
+﻿using System;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Paramore.Brighter.Core.Tests.CommandProcessors.TestDoubles;
 using Paramore.Brighter.Core.Tests.FeatureSwitch.TestDoubles;
 using Microsoft.Extensions.DependencyInjection;
@@ -75,7 +50,7 @@ namespace Paramore.Brighter.Core.Tests.FeatureSwitch
         {
             _commandProcessor.Send(_myCommand);
 
-            _provider.GetService<MyFeatureSwitchedConfigHandler>().DidReceive().Should().BeTrue();
+            Assert.True(_provider.GetService<MyFeatureSwitchedConfigHandler>()!.DidReceive());
         }
 
         [Fact]
@@ -83,7 +58,7 @@ namespace Paramore.Brighter.Core.Tests.FeatureSwitch
         {
             await _commandProcessor.SendAsync(_myAsyncCommand);
 
-            _provider.GetService<MyFeatureSwitchedConfigHandlerAsync>().DidReceive().Should().BeTrue();
+            Assert.True(_provider.GetService<MyFeatureSwitchedConfigHandlerAsync>()!.DidReceive());
         }
 
         public void Dispose()
