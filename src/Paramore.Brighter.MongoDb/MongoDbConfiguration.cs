1f80a3bf76e68206e35a1379d92cdf25286a3ae2
Rafael Lillo <rafael.andrade@justeattakeaway.com>
2025-02-28 10:11:13 +0000

    Add support MongoDB (#3524)
    * Initial support to MongoDB
    
    * Finish MongoDB outbox support
    
    * Add support to .NET Framework
    
    * Add support to inbox message
    
    * Add Unit of work
    
    * Add MongoDb distributed lock
    
    * Add MongoDb test
    
    * Add CI test and rename the project
    
    * Fixes TTL index
    
    * Remove unnecessary config
    
    * Increase mongodb retries
    
    * try to fix mongo startup
    
    * try to fix build
    
    * try fix build
    
    * try fix build

---
+++ src/Paramore.Brighter.MongoDb/MongoDbConfiguration.cs
@@ -0,0 +1,85 @@
+﻿using MongoDB.Driver;
+using Paramore.Brighter.Observability;
+
+namespace Paramore.Brighter.MongoDb;
+
+/// <summary>
+/// The MongoDB configuration
+/// </summary>
+public class MongoDbConfiguration
+{
+    /// <summary>
+    /// Initialize new instance of <see cref="MongoDbConfiguration"/>
+    /// </summary>
+    /// <param name="client">The Mongo client.</param>
+    /// <param name="databaseName">The database name.</param>
+    /// <param name="collectionName">The collection name.</param>
+    public MongoDbConfiguration(MongoClient client, string databaseName, string collectionName)
+    {
+        Client = client;
+        DatabaseName = databaseName;
+        CollectionName = collectionName;
+    }
+
+    /// <summary>
+    /// Initialize new instance of <see cref="MongoDbConfiguration"/>
+    /// </summary>
+    /// <param name="connectionString">The Mongo db connection string.</param>
+    /// <param name="databaseName">The database name.</param>
+    /// <param name="collectionName">The collection name.</param>
+    public MongoDbConfiguration(string connectionString, string databaseName, string collectionName)
+        : this(new MongoClient(connectionString), databaseName, collectionName)
+    {
+    }
+
+    /// <summary>
+    /// The <see cref="MongoClient"/>
+    /// </summary>
+    public MongoClient Client { get; set; }
+
+    /// <summary>
+    /// The mongodb database name
+    /// </summary>
+    public string DatabaseName { get; }
+
+    /// <summary>
+    /// The mongodb collection
+    /// </summary>
+    public string CollectionName { get; }
+
+    /// <summary>
+    /// The <see cref="System.TimeProvider"/>
+    /// </summary>
+    public TimeProvider TimeProvider { get; set; } = TimeProvider.System;
+
+    /// <summary>
+    /// Action to be performed when it's resolving a collection  
+    /// </summary>
+    public OnResolvingACollection MakeCollection { get; set; } = OnResolvingACollection.Assume;
+
+    /// <summary>
+    /// The <see cref="MongoDatabaseSettings"/> used when access the database.
+    /// </summary>
+    public MongoDatabaseSettings? DatabaseSettings { get; set; }
+
+    /// <summary>
+    /// The <see cref="MongoDatabaseSettings"/> used to get collection
+    /// </summary>
+    public MongoCollectionSettings? CollectionSettings { get; set; }
+
+    /// <summary>
+    /// The <see cref="CreateCollectionOptions"/>.
+    /// </summary>
+    public CreateCollectionOptions? CreateCollectionOptions { get; set; }
+
+    /// <summary>
+    /// The <see cref="InstrumentationOptions"/>.
+    /// </summary>
+    public InstrumentationOptions InstrumentationOptions { get; set; } = InstrumentationOptions.All;
+
+    /// <summary>
+    /// Optional time to live for the messages in the outbox
+    /// By default, messages will not expire
+    /// </summary>
+    public TimeSpan? TimeToLive { get; set; }
+}
---
+++ tests/Paramore.Brighter.MongoDb.Tests/Outbox/When_retrieving_outstanding_messages_async.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Paramore.Brighter.Outbox.MongoDb;
+using Xunit;
+
+namespace Paramore.Brighter.MongoDb.Tests.Outbox;
+
+[Trait("Category", "MongoDb")]
+public class MongoDbFetchOutStandingMessageAsyncTests : IDisposable
+{
+    private readonly string _collection;
+    private readonly Message _messageEarliest;
+    private readonly Message _messageDispatched;
+    private readonly Message _messageUnDispatched;
+    private readonly MongoDbOutbox _outbox;
+
+    public MongoDbFetchOutStandingMessageAsyncTests()
+    {
+        _collection = $"outbox-{Guid.NewGuid():N}";
+        _outbox = new MongoDbOutbox(Configuration.Create(_collection));
+        var routingKey = new RoutingKey("test_topic");
+
+        _messageEarliest = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), routingKey, MessageType.MT_DOCUMENT)
+            {
+                TimeStamp = DateTimeOffset.UtcNow.AddHours(-3)
+            },
+            new MessageBody("message body"));
+        _messageDispatched = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), routingKey, MessageType.MT_DOCUMENT),
+            new MessageBody("message body"));
+        _messageUnDispatched = new Message(
+            new MessageHeader(Guid.NewGuid().ToString(), routingKey, MessageType.MT_DOCUMENT),
+            new MessageBody("message body"));
+    }
+
+    [Fact]
+    public async Task When_Retrieving_Not_Dispatched_Messages_Async()
+    {
+        var context = new RequestContext();
+        await _outbox.AddAsync([_messageEarliest, _messageDispatched, _messageUnDispatched], context);
+        await _outbox.MarkDispatchedAsync(_messageDispatched.Id, context);
+        
+        var total = await _outbox.GetNumberOfOutstandingMessagesAsync();
+
+        var allUnDispatched = await _outbox.OutstandingMessagesAsync(TimeSpan.Zero, context);
+        var messagesOverAnHour = await _outbox.OutstandingMessagesAsync(TimeSpan.FromHours(1), context);
+        var messagesOver4Hours = await _outbox.OutstandingMessagesAsync(TimeSpan.FromHours(4), context);
+
+        //Assert
+        total.Should().Be(2);
+        allUnDispatched.Should().HaveCount(2);
+        messagesOverAnHour.Should().ContainSingle();
+        messagesOver4Hours.Should().BeEmpty();
+    }
+
+    public void Dispose()
+    {
+        Configuration.Cleanup(_collection);
+    }
+}
