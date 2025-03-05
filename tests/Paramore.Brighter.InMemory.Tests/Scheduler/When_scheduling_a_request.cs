c0d4645e0574951fbec1eb5161b8562326a4d5d8
Ian Cooper <ian.cooper@justeattakeaway.com>
2025-03-05 19:42:28 +0000

    fix: equivalence test fails

--- tests/Paramore.Brighter.InMemory.Tests/Scheduler/When_scheduling_a_request.cs
+++ tests/Paramore.Brighter.InMemory.Tests/Scheduler/When_scheduling_a_request.cs
@@ -107,7 +107,21 @@ public class InMemorySchedulerRequestTests
 
         Assert.Contains(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -125,7 +139,21 @@ public class InMemorySchedulerRequestTests
 
         Assert.Contains(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -144,7 +172,21 @@ public class InMemorySchedulerRequestTests
 
         Assert.Contains(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -162,7 +204,21 @@ public class InMemorySchedulerRequestTests
 
         Assert.Contains(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -227,7 +283,21 @@ public class InMemorySchedulerRequestTests
         _timeProvider.Advance(TimeSpan.FromHours(2));
         Assert.Contains(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Theory]
@@ -250,7 +320,21 @@ public class InMemorySchedulerRequestTests
         _timeProvider.Advance(TimeSpan.FromHours(2));
         Assert.Contains(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     #endregion
@@ -278,7 +362,21 @@ public class InMemorySchedulerRequestTests
         _timeProvider.Advance(TimeSpan.FromSeconds(2));
         Assert.DoesNotContain(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Theory]
@@ -299,7 +397,21 @@ public class InMemorySchedulerRequestTests
         _timeProvider.Advance(TimeSpan.FromSeconds(2));
         Assert.DoesNotContain(nameof(MyEventHandler), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     #endregion
--- tests/Paramore.Brighter.InMemory.Tests/Scheduler/When_scheduling_a_request_async.cs
+++ tests/Paramore.Brighter.InMemory.Tests/Scheduler/When_scheduling_a_request_async.cs
@@ -115,7 +115,21 @@ public class InMemorySchedulerRequestAsyncTests
 
         Assert.Contains(nameof(MyEventHandlerAsync), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual = await _outbox.GetAsync(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -133,7 +147,21 @@ public class InMemorySchedulerRequestAsyncTests
 
         Assert.Contains(nameof(MyEventHandlerAsync), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual = await _outbox.GetAsync(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -152,7 +180,21 @@ public class InMemorySchedulerRequestAsyncTests
 
         Assert.Contains(nameof(MyEventHandlerAsync), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual = await _outbox.GetAsync(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -170,7 +212,21 @@ public class InMemorySchedulerRequestAsyncTests
 
         Assert.Contains(nameof(MyEventHandlerAsync), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual =  _outbox.Get(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Fact]
@@ -235,7 +291,21 @@ public class InMemorySchedulerRequestAsyncTests
         _timeProvider.Advance(TimeSpan.FromHours(2));
         Assert.Contains(nameof(MyEventHandlerAsync), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual = await _outbox.GetAsync(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     [Theory]
@@ -258,7 +328,21 @@ public class InMemorySchedulerRequestAsyncTests
         _timeProvider.Advance(TimeSpan.FromHours(2));
         Assert.Contains(nameof(MyEventHandlerAsync), _receivedMessages);
 
-        Assert.Equivalent(new Message(), _outbox.Get(req.Id, new RequestContext()));
+        var expected = Message.Empty;
+        var actual = await _outbox.GetAsync(req.Id, new RequestContext());
+        
+        Assert.Equivalent(expected.Body, actual.Body);
+        Assert.Equal(expected.Id, actual.Id);
+        Assert.Equal(expected.Persist, actual.Persist);
+        Assert.Equal(expected.Redelivered, actual.Redelivered);
+        Assert.Equal(expected.DeliveryTag, actual.DeliveryTag);
+        Assert.Equal(expected.Header.MessageType, actual.Header.MessageType);
+        Assert.Equal(expected.Header.Topic, actual.Header.Topic);
+        Assert.Equal(expected.Header.TimeStamp, actual.Header.TimeStamp, TimeSpan.FromSeconds(1));
+        Assert.Equal(expected.Header.CorrelationId, actual.Header.CorrelationId);
+        Assert.Equal(expected.Header.ReplyTo, actual.Header.ReplyTo);
+        Assert.Equal(expected.Header.ContentType, actual.Header.ContentType);
+        Assert.Equal(expected.Header.HandledCount, actual.Header.HandledCount);
     }
 
     #endregion
