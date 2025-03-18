26852f199d4909a9af1bc5c0c03d689cb9f7e67d
Rafael Andrade <rafael.andrade@justeattakeaway.com>
2025-03-18 14:21:08 +0000

    Add Cloud event sample

---
+++ samples/Transforms/AWSTransfomers/CloudEvents/Greetings/Ports/Mappers/GreetingEventMessageMapper.cs
@@ -0,0 +1,58 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2014 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System.IO.Compression;
+using System.Text.Json;
+using Greetings.Ports.Commands;
+using Paramore.Brighter;
+using Paramore.Brighter.MessagingGateway.AWSSQS;
+using Paramore.Brighter.Transforms.Attributes;
+using Paramore.Brighter.Transforms.Transformers;
+
+namespace Greetings.Ports.Mappers
+{
+    public class GreetingEventMessageMapper : IAmAMessageMapper<GreetingEvent>
+    {
+        public IRequestContext Context { get; set; }
+        
+        //Although SNS allows 256K, we have to use UTF8 strings, which is not the format of a compressed message
+        //so we have to convert the bytes to base64 and then use that as the message body
+        //this inflates the byte count, see https://stackoverflow.com/questions/54224029/aws-sns-publising-compressed-payload
+        [CloudEvents(0, DataSchema = "/test", Source = "com.brighter", Subject = "cloud-events", Type = "some-type")]
+        public Message MapToMessage(GreetingEvent request, Publication publication)
+        {
+            var header = new MessageHeader(messageId: request.Id, topic: publication.Topic, messageType: MessageType.MT_EVENT);
+            var body = new MessageBody(JsonSerializer.Serialize(request, JsonSerialisationOptions.Options));
+            var message = new Message(header, body);
+            return message;
+        }
+
+        public GreetingEvent MapToRequest(Message message)
+        {
+            var greetingCommand = JsonSerializer.Deserialize<GreetingEvent>(message.Body.Value, JsonSerialisationOptions.Options);
+            
+            return greetingCommand;
+        }
+    }
+}
---
+++ samples/Transforms/AWSTransfomers/CloudEvents/GreetingsSender/Program.cs
@@ -0,0 +1,103 @@
+﻿#region Licence
+/* The MIT License (MIT)
+Copyright © 2017 Ian Cooper <ian_hammond_cooper@yahoo.co.uk>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+using System.Linq;
+using System.Transactions;
+using Amazon;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.S3;
+using Greetings.Ports.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Paramore.Brighter;
+using Paramore.Brighter.Extensions.DependencyInjection;
+using Paramore.Brighter.MessagingGateway.AWSSQS;
+using Paramore.Brighter.Tranformers.AWS;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace GreetingsSender
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<ILoggerFactory>(new SerilogLoggerFactory());
+            
+            if (new CredentialProfileStoreChain().TryGetAWSCredentials("default", out var credentials))
+            {
+                var awsConnection = new AWSMessagingGatewayConnection(credentials, RegionEndpoint.EUWest1);
+
+                var topic = new RoutingKey(typeof(GreetingEvent).FullName.ToValidSNSTopicName());
+
+                var producerRegistry = new SnsProducerRegistryFactory(
+                    awsConnection,
+                    new SnsPublication[]
+                    {
+                        new()
+                        {
+                            Topic = topic,
+                            RequestType = typeof(GreetingEvent),
+                            FindTopicBy = TopicFindBy.Convention,
+                            MakeChannels = OnMissingChannel.Create
+                        }
+                    }
+                ).Create();
+                
+                serviceCollection.AddBrighter()
+                    .UseExternalBus((configure) =>
+                    {
+                        configure.ProducerRegistry = producerRegistry;
+                    })
+                    .AutoFromAssemblies(typeof(GreetingEvent).Assembly);
+
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+
+                var commandProcessor = serviceProvider.GetService<IAmACommandProcessor>();
+                
+                Console.WriteLine($"Sending Event to SNS topic {topic} ");
+
+                //var largeString = "hello world";
+                commandProcessor.Post(new GreetingEvent("Hi Ian"));
+                
+                Console.WriteLine($"Sent Event to SNS topic {topic} ");
+            }
+        }
+        
+        public static string CreateString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[new Random().Next(s.Length)]).ToArray());
+        }    
+    }
+}
