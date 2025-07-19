f2d42d2c74252b8351c2d1800c260baf953d05f0
Toby Henderson <hendersont@gmail.com>
2025-07-19 15:31:44 +0100

    feat: add SalutationsMigrator service and update test data for consistency

--- samples/WebAPI/WebAPI_EFCore/MigrationService/Program.cs
+++ samples/WebAPI/WebAPI_EFCore/MigrationService/Program.cs
@@ -4,6 +4,7 @@ using SalutationApp.EntityGateway;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<GreetingsMigrator>();
+builder.Services.AddHostedService<SalutationsMigrator>();
 
 builder.AddServiceDefaults();
 
@@ -15,5 +16,14 @@ builder.AddMySqlDbContext<GreetingsEntityGateway>("Greetings");
 var app = builder.Build();
 
 app.Run();
+
+public class SalutationsMigrator(IServiceProvider serviceProvider,
+    IHostApplicationLifetime hostApplicationLifetime) : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 
