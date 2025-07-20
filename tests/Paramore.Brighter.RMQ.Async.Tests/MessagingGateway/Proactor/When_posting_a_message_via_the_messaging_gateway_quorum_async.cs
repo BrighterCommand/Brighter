dcac13f77baededc5b724659f9e3c7f9028fdc82
Ian Cooper <ian.cooper@justeattakeaway.com>
2025-07-20 12:39:11 +0100

    fix: seek to make RMQ tests more reliable on character decoding

--- tests/Paramore.Brighter.RMQ.Async.Tests/MessagingGateway/Proactor/When_posting_a_message_via_the_messaging_gateway_quorum_async.cs
+++ tests/Paramore.Brighter.RMQ.Async.Tests/MessagingGateway/Proactor/When_posting_a_message_via_the_messaging_gateway_quorum_async.cs
@@ -26,6 +26,7 @@ using System;
 using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
+using Paramore.Brighter.Extensions;
 using Paramore.Brighter.MessagingGateway.RMQ.Async;
 using Paramore.Brighter.Observability;
 using Xunit;
@@ -49,7 +50,7 @@ public class RmqMessageProducerSendMessageQuorumTestsAsync : IDisposable, IAsync
         var timestamp = DateTimeOffset.UtcNow;
         var correlationId = Id.Random;
         var replyTo = new RoutingKey("reply-queue");
-        var contentType = new ContentType(MediaTypeNames.Text.Plain);
+        var contentType = new ContentType(MediaTypeNames.Text.Plain){CharSet = CharacterEncoding.UTF8.FromCharacterEncoding()};
         var handledCount = 5;
         var dataSchema = new Uri("http://schema.example");
         var subject = "test-subject";
