7d1aa62f6e748d4baf3d65a4377ceedc6690e4d6
Ian Cooper <ian.cooper@justeattakeaway.com>
2025-08-28 22:01:17 +0100

    fix: rename to transactionprovider for consistency

--- src/Paramore.Brighter/CommandProcessor.cs
+++ src/Paramore.Brighter/CommandProcessor.cs
@@ -946,7 +946,7 @@ namespace Paramore.Brighter
         // type before we call it.
         private Id CallDepositPost<TRequest>(
             TRequest actualRequest, 
-            IAmABoxTransactionProvider? iamABoxTransactionProvider,
+            IAmABoxTransactionProvider? transactionProvider,
             RequestContext? requestContext, 
             Dictionary<string, object>? dictionary, 
             string? batchId,
@@ -971,7 +971,7 @@ namespace Paramore.Brighter
             }
 
             return CallMethodAndPreserveException(() =>
-                (deposit?.Invoke(this, [actualRequest, iamABoxTransactionProvider, requestContext, dictionary, batchId]) as Id)!
+                (deposit?.Invoke(this, [actualRequest, transactionProvider, requestContext, dictionary, batchId]) as Id)!
             );
         }
 
