4a5724f7d010b0c8c52ebc4280e6c00b4ad0cf72
Ian Cooper <ian.cooper@justeattakeaway.com>
2025-09-03 10:46:07 +0200

    feat: a producer needs a request type

--- samples/TaskQueue/RMQRequestReply/GreetingsClient/Program.cs
+++ samples/TaskQueue/RMQRequestReply/GreetingsClient/Program.cs
@@ -63,6 +63,7 @@ namespace GreetingsSender
                             new RmqPublication
                             {
                                 Topic = new RoutingKey("Greeting.Request"),
+                                RequestType = typeof(GreetingRequest)
                             }
                         ]).Create();
                     configure.UseRpc = true;
