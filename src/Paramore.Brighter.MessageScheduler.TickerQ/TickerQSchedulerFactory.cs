02391314c007530a842bd9bad041f066f720bab4
Aboubakr <aboubakrnasef@gmail.com>
2025-11-02 17:38:54 +0200

    add test for schedule message

--- src/Paramore.Brighter.MessageScheduler.TickerQ/TickerQSchedulerFactory.cs
+++ src/Paramore.Brighter.MessageScheduler.TickerQ/TickerQSchedulerFactory.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TickerQ.Utilities.Interfaces.Managers;
+using TickerQ.Utilities.Models.Ticker;
 
 namespace Paramore.Brighter.MessageScheduler.TickerQ
 {
-    public class TickerQSchedulerFactory
+    public class TickerQSchedulerFactory(ITimeTickerManager<TimeTicker> timeTickerManager) : IAmAMessageSchedulerFactory, IAmARequestSchedulerFactory
     {
+
+        public Func<Message, string> GetOrCreateSchedulerId { get; set; } = _ => Uuid.NewAsString();
+
+
+        public IAmAMessageScheduler Create(IAmACommandProcessor processor)
+        {
+            return new TickerQScheduler(timeTickerManager, GetOrCreateSchedulerId);
+        }
+
+        public IAmARequestSchedulerAsync CreateAsync(IAmACommandProcessor processor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAmARequestSchedulerSync CreateSync(IAmACommandProcessor processor)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
