2f55f8f2b0f7b75245ec9d433585b56a97a8155d
Doğan Çeçen <sepeth@hey.com>
2025-11-03 12:20:38 +0200

    Remove the AckOnRead flag in AzureServiceBusConfiguration (#3899)
    See https://github.com/BrighterCommand/Brighter/issues/3895

--- src/Paramore.Brighter.MessagingGateway.AzureServiceBus/AzureServiceBusConfiguration.cs
+++ src/Paramore.Brighter.MessagingGateway.AzureServiceBus/AzureServiceBusConfiguration.cs
@@ -33,12 +33,10 @@ public class AzureServiceBusConfiguration
     /// Initializes an Instance of <see cref="AzureServiceBusConfiguration"/>
     /// </summary>
     /// <param name="connectionString">The Connection String to connect to Azure Service Bus.</param>
-    /// <param name="ackOnRead">If True Messages and Read a Deleted, if False Messages are Peeked and Locked.</param>
     /// <param name="bulkSendBatchSize">When sending more than one message using the MessageProducer, the max amount to send in a single transmission.</param>
-    public AzureServiceBusConfiguration(string connectionString, bool ackOnRead = false, int bulkSendBatchSize = 10 )
+    public AzureServiceBusConfiguration(string connectionString, int bulkSendBatchSize = 10 )
     {
         ConnectionString = connectionString;
-        AckOnRead = ackOnRead;
         BulkSendBatchSize = bulkSendBatchSize;
     }
 
@@ -46,11 +44,6 @@ public class AzureServiceBusConfiguration
     /// The Connection String to connect to Azure Service Bus.
     /// </summary>
     public string ConnectionString { get; }
-
-    /// <summary>
-    /// When set to true this will set the Channel to Read and Delete, when False Peek and Lock
-    /// </summary>
-    public bool AckOnRead{ get; }
 
     /// <summary>
     /// When sending more than one message using the MessageProducer, the max amount to send in a single transmission.
