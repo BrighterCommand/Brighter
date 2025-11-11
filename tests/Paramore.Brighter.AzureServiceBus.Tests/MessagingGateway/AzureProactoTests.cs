10409ab2a5efc3f9db52f5eafa9d50a4313e56dc
Rafael Andrade <rafael.andrade@justeattakeaway.com>
2025-11-11 14:52:24 +0000

    Init Azure migration to new test

---
+++ tests/Paramore.Brighter.AzureServiceBus.Tests/MessagingGateway/AzureProactoTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Paramore.Brighter.Base.Test.MessagingGateway;
+using Paramore.Brighter.Base.Test.Requests;
+using Paramore.Brighter.MessagingGateway.AzureServiceBus;
+
+namespace Paramore.Brighter.AzureServiceBus.Tests.MessagingGateway;
+
+public class AzureProactoTests : MessagingGatewayProactorTests<AzureServiceBusPublication, AzureServiceBusSubscription>
+{
+    protected override AzureServiceBusPublication CreatePublication(RoutingKey routingKey)
+    {
+        return new AzureServiceBusPublication<MyCommand>
+        {
+            Topic = routingKey,
+            MakeChannels = OnMissingChannel.Create
+        };
+    }
+
+    protected override AzureServiceBusSubscription CreateSubscription(RoutingKey routingKey, ChannelName channelName,
+        OnMissingChannel makeChannel = OnMissingChannel.Create, bool setupDeadLetterQueue = false)
+    {
+        return new AzureServiceBusSubscription<MyCommand>(
+            subscriptionName: new SubscriptionName(Uuid.NewAsString()),
+            routingKey: routingKey,
+            channelName: channelName,
+            makeChannels: makeChannel,
+            messagePumpType: MessagePumpType.Proactor);
+    }
+
+    protected override async Task<IAmAMessageProducerAsync> CreateProducerAsync(AzureServiceBusPublication publication, CancellationToken cancellationToken = default)
+    {
+        var clientProvider = ASBCreds.ASBClientProvider;
+        var producers = await new AzureServiceBusMessageProducerFactory(
+                clientProvider, 
+                [publication],
+                1)
+            .CreateAsync();
+        
+        var producer = producers.First().Value;
+        return (IAmAMessageProducerAsync)producer;
+    }
+
+    protected override async Task<IAmAChannelAsync> CreateChannelAsync(AzureServiceBusSubscription subscription, CancellationToken cancellationToken = default)
+    {
+        var clientProvider = ASBCreds.ASBClientProvider;
+        var channel = await new AzureServiceBusChannelFactory(new AzureServiceBusConsumerFactory(clientProvider))
+            .CreateAsyncChannelAsync(subscription, cancellationToken);
+
+        if (subscription.MakeChannels == OnMissingChannel.Create)
+        {
+            // Ensuring that the queue exists before return the channel
+            await channel.ReceiveAsync(TimeSpan.FromMilliseconds(100), cancellationToken);
+        }
+
+        return channel;
+    }
+}
