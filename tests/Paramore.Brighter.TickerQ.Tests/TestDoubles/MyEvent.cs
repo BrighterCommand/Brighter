d5a0e663df2d5dc7230589c6a7bd8668bdeb6026
Aboubakr <aboubakrnasef@gmail.com>
2025-11-12 07:42:21 +0200

    use the Id instead of guid

--- tests/Paramore.Brighter.TickerQ.Tests/TestDoubles/MyEvent.cs
+++ tests/Paramore.Brighter.TickerQ.Tests/TestDoubles/MyEvent.cs
@@ -1,6 +1,6 @@
 ﻿namespace Paramore.Brighter.TickerQ.Tests.TestDoubles;
 
-public class MyEvent() : Event(Guid.NewGuid())
+public class MyEvent() : Event(Id.Random())
 {
     public string Value { get; set; }
 }
