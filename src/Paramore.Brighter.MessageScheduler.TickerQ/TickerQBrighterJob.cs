a6f79501904df03c1491e45b6fb083ee6aec5374
Aboubakr Nasef <nasefabobakr725@gmail.com>
2025-11-22 17:39:17 +0200

    Feature/add support for TickerQ (#3903)
    * add tickerq and test project
    
    * add TickerQ library
    
    * add test for schedule message
    
    * Implement the Schedule Message Functions
    
    * add request shceduler messages WIP
    
    * add documentation
    
    * refactor test fixtures
    
    * pass the cacnelation token
    
    * refactor the creation logic of the timeTicker
    
    * fix typo
    
    * remove launch settings
    
    * use the Id instead of guid
    
    * add the strong namer for the TickerQ.Utilities isn't signed with strong name
    
    * remove properites folder
    
    * update the package to not Sign the assembly
    
    * remove space  from file serviceActivator api
    
    * Remove the AckOnRead flag in AzureServiceBusConfiguration (#3899)
    
    See https://github.com/BrighterCommand/Brighter/issues/3895
    
    * Fixes found bug on Messaging Gateway (#3905)
    
    * Fixes found bug on Messaging Gateway
    
    * fixes build
    
    * Make RedisSubscription constructor as public (#3906)
    
    * add MIT License to files
    
    ---------
    
    Co-authored-by: Doğan Çeçen <sepeth@hey.com>
    Co-authored-by: Rafael Lillo <rafael.andrade@justeattakeaway.com>
    Co-authored-by: Ian Cooper <ian_hammond_cooper@yahoo.co.uk>

---
+++ src/Paramore.Brighter.MessageScheduler.TickerQ/TickerQBrighterJob.cs
@@ -0,0 +1,59 @@
+﻿#region License
+/* The MIT License (MIT)
+Copyright © 2025 Aboubakr Nasef <aboubakrnasef@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. */
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Paramore.Brighter.JsonConverters;
+using Paramore.Brighter.Scheduler.Events;
+using TickerQ.Utilities.Base;
+using TickerQ.Utilities.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Paramore.Brighter.MessageScheduler.TickerQ
+{
+    /// <summary>
+    /// The TickerQ Message scheduler Job
+    /// </summary>
+    /// <param name="processor"></param>
+    public class BrighterTickerQSchedulerJob(IAmACommandProcessor processor)
+    {
+        [TickerFunction(nameof(FireSchedulerMessageAsync))]
+        public async Task FireSchedulerMessageAsync(TickerFunctionContext<string> tickerContext, CancellationToken cancellationToken)
+        {
+            var schedulerRequest = JsonSerializer.Deserialize<FireSchedulerMessage>(tickerContext.Request, JsonSerialisationOptions.Options)!;
+            await processor.SendAsync(schedulerRequest);
+        }
+
+        [TickerFunction(nameof(FireSchedulerRequestAsync))]
+        public async Task FireSchedulerRequestAsync(TickerFunctionContext<string> tickerContext, CancellationToken cancellationToken)
+        {
+            var schedulerRequest = JsonSerializer.Deserialize<FireSchedulerRequest>(tickerContext.Request, JsonSerialisationOptions.Options)!;
+            await processor.SendAsync(schedulerRequest);
+        }
+    }
+}
---
+++ tests/Paramore.Brighter.TickerQ.Tests/TestDoubles/MyEventHandlerAsync.cs
@@ -0,0 +1,10 @@
+﻿namespace Paramore.Brighter.TickerQ.Tests.TestDoubles;
+
+public class MyEventHandlerAsync(IDictionary<string, string> receivedMessages) : RequestHandlerAsync<MyEvent>
+{
+    public override async Task<MyEvent> HandleAsync(MyEvent command, CancellationToken cancellationToken = default)
+    {
+        receivedMessages.Add(nameof(MyEventHandlerAsync), command.Id);
+        return await base.HandleAsync(command, cancellationToken).ConfigureAwait(ContinueOnCapturedContext);
+    }
+}
