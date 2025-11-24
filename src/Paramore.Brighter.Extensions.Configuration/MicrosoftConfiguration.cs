a66908dccc3f42dd4eb7c86b53b69ff62b514150
Rafael Andrade <rafael.andrade@justeattakeaway.com>
2025-11-24 16:32:15 +0000

    Add support to configuration brighter from IConfiguration

---
+++ src/Paramore.Brighter.Extensions.Configuration/MicrosoftConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Paramore.Brighter.Extensions.Configuration;
+
+public class MicrosoftConfiguration(IConfiguration configuration) : IAmAConfiguration
+{
+    public IAmAConfiguration GetSection(string sectionName)
+    {
+        return new MicrosoftConfiguration(configuration.GetSection(sectionName));
+    }
+
+    public T? Get<T>()
+    {
+        return configuration.Get<T>();
+    }
+
+    public string? GetConnectionString(string name)
+    {
+        return configuration.GetConnectionString(name);
+    }
+
+    public void Bind<T>(T obj)
+    {
+        configuration.Bind(obj);
+    }
+}
